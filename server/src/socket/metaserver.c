1f3df116cad6b78afd0319755ef20650c4c4474e
Alex Tokar <admin@atokar.net>
2009-09-23 04:04:59 +0200

    Threaded metaserver updating.
    Former-commit-id: 2a61c9a47c182ccaaf5a5bca7ffbcd20c1c2f9f5

--- server/src/socket/metaserver.c
+++ server/src/socket/metaserver.c
@@ -23,37 +23,53 @@
 * The author can be reached at admin@atrinik.org                        *
 ************************************************************************/
 
+/**
+ * @file
+ * Metaserver updating related code. */
+
 #include <global.h>
 #include <curl/curl.h>
+#include <pthread.h>
 
-/* The time in seconds for timeout upon connecting */
+/** The time in seconds for timeout upon connecting */
 #define METASERVER_TIMEOUT 3
 
-/* Init metaserver. */
+/**
+ * Init metaserver. */
 void metaserver_init()
 {
 	if (!settings.meta_on)
+	{
 		return;
+	}
 
 	/* Init global cURL */
 	curl_global_init(CURL_GLOBAL_ALL);
 }
 
-/* Function to call when receiving data from the metaserver */
+/**
+ * Function to call when receiving data from the metaserver.
+ * @param ptr Pointer to the actual data
+ * @param size Size of the data
+ * @param nmemb
+ * @param data Unused
+ * @return The real size of the data */
 static size_t metaserver_writer(void *ptr, size_t size, size_t nmemb, void *data)
 {
 	size_t realsize = size * nmemb;
 
 	(void) data;
 
-	LOG(llevDebug, "DEBUG: metaserver_writer(): Start of text:\n%s\n", (const char *)ptr);
-	LOG(llevDebug, "DEBUG: metaserver_writer(): End of text.\n");
+	LOG(llevDebug, "DEBUG: metaserver_writer(): Returned data:\n%s\n", (const char *) ptr);
 
 	return realsize;
 }
 
-/* Update the metaserver info about this server. */
-void metaserver_update()
+/**
+ * The actual function doing the metaserver updating, called by
+ * pthread_create() in metaserver_update().
+ * @return Always returns NULL. */
+void *metaserver_thread(void *dummy)
 {
 	char buf[MAX_BUF], num_players = 0;
 	player *pl;
@@ -63,16 +79,16 @@ void metaserver_update()
 	CURLcode res = 0;
 	time_t now = time(NULL);
 
-	/* If the setting is off, just return */
-	if (!settings.meta_on)
-		return;
+	(void) dummy;
 
 	/* We could use socket_info.nconns, but that is not quite as accurate,
 	 * as connections in the progress of being established, are listening
 	 * but don't have a player, etc.  This operation below should not be that
 	 * costly. */
 	for (pl = first_player; pl != NULL; pl = pl->next)
+	{
 		num_players++;
+	}
 
 	/* Hostname */
 	curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "hostname", CURLFORM_COPYCONTENTS, settings.meta_host, CURLFORM_END);
@@ -110,7 +126,9 @@ void metaserver_update()
 		res = curl_easy_perform(curl);
 
 		if (res)
+		{
 			LOG(llevDebug, "DEBUG: metaserver_update(): easy_perform got error %d (%s).\n", res, curl_easy_strerror(res));
+		}
 
 		/* Always cleanup */
 		curl_easy_cleanup(curl);
@@ -121,5 +139,31 @@ void metaserver_update()
 
 	/* Output info that the data was updated. */
 	if (!res)
+	{
 		LOG(llevInfo, "INFO: metaserver_update(): Sent data at %.16s.\n", ctime(&now));
+	}
+
+	return NULL;
+}
+
+/**
+ * Update the metaserver info about this server. */
+void metaserver_update()
+{
+	pthread_t thread_id;
+	int ret;
+
+	/* If the setting is off, just return */
+	if (!settings.meta_on)
+	{
+		return;
+	}
+
+	/* Create a thread to update the data */
+	ret = pthread_create(&thread_id, NULL, metaserver_thread, NULL);
+
+	if (ret)
+	{
+		LOG(llevBug, "BUG: metaserver_update(): Failed to create thread.\n");
+	}
 }
