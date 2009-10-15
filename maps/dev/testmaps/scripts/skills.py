dcaa73bf660e16e77d36e67067d07fba294b261d
Alex Tokar <admin@atokar.net>
2009-10-15 11:30:34 +0200

    Some testmaps cleanups.
    Former-commit-id: 96bb140ccd80c8237c6cc2c0fc9e680fa886af15

---
+++ maps/dev/testmaps/scripts/skills.py
@@ -0,0 +1,30 @@
+## @file
+## Script used by the Skillgiver NPC in Developer Testmaps.
+
+from Atrinik import *
+import string
+
+## Activator object.
+activator = WhoIsActivator()
+## Object who has the event object in their inventory.
+me = WhoAmI()
+
+msg = WhatIsMessage().strip().lower()
+text = string.split(msg)
+
+# Learn a skill.
+if text[0] == "learn":
+	## Get the skill number.
+	skill = GetSkillNr(text[1])
+
+	# Skill was not valid
+	if skill == -1:
+		me.SayTo(activator, "Unknown skill.")
+	else:
+		if activator.DoKnowSkill(skill) == 1:
+			me.SayTo(activator, "You already know that skill.")
+		else:
+			activator.AcquireSkill(skill, LEARN)
+
+else:
+	me.SayTo(activator, "\nI am the Skillgiver.\nSay ^learn <skillname>^ to learn a particular skill.")
---
+++ maps/dev/testmaps/scripts/spells.py
@@ -0,0 +1,43 @@
+## @file
+## Script used by the Spellgiver NPC in Developer Testmaps.
+
+from Atrinik import *
+import string
+
+## Activator object.
+activator = WhoIsActivator()
+## Object who has the event object in their inventory.
+me = WhoAmI()
+
+msg = WhatIsMessage().strip().lower()
+text = string.split(msg)
+
+# Learn a spell.
+if text[0] == "learn":
+	## Get the spell number.
+	spell = GetSpellNr(text[1])
+
+	# Invalid spell?
+	if spell == -1:
+		me.SayTo(activator, "Unknown spell.")
+	else:
+		if activator.DoKnowSpell(spell) == 1:
+			me.SayTo(activator, "You already know that spell.")
+		else:
+			activator.AcquireSpell(spell, LEARN)
+
+# Unlearn a spell.
+elif text[0] == "unlearn":
+	spell = GetSpellNr(text[1])
+
+	# Invalid spell?
+	if spell == -1:
+		me.SayTo(activator, "Unknown spell.")
+	else:
+		if activator.DoKnowSpell(spell) == 0:
+			me.SayTo(activator, "You don't know that spell.")
+		else:
+			activator.AcquireSpell(spell, UNLEARN)
+
+else:
+	me.SayTo(activator, "\nI am the Spellgiver.\nSay ^learn <spellname>^ or ^unlearn <spellname>^ to learn or unlearn a given spell.")
