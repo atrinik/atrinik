003d0814747b368ca6e993674097db86da417373
Alex Tokar <admin@atokar.net>
2009-11-27 00:45:26 +0100

    Remove SQLite dependencies.
    Former-commit-id: 4dc6a61669cb8215a738dec69944dc9d9e2d8760

--- server/make/win32/CodeBlocks/Setup.bat
+++ server/make/win32/CodeBlocks/Setup.bat
@@ -6,10 +6,6 @@ rem cURL
 gunzip -c curl.tar.gz > curl.tar
 tar xvf curl.tar
 
-rem SQLite3
-gunzip -c sqlite3.tar.gz > sqlite3.tar
-tar xvf sqlite3.tar
-
 rem ====================================
 rem Clean up.
 rem ====================================
