c8e0c998f12b4f42ccdcfb9130ca68b23ea45dee
Alex Tokar <admin@atokar.net>
2009-12-06 17:11:42 +0100

    Slightly rewrite ship_exit.py script; use GetSaveBed() to detect player's save bed instead of using player info object.
    Former-commit-id: 17435bd3d730838eca0bb08b398f14880da5a6d1

--- maps/shattered_islands/scripts/tutorial_island/ship_exit.py
+++ maps/shattered_islands/scripts/tutorial_island/ship_exit.py
@@ -2,23 +2,13 @@
 ## Script to run when player comes to Brynknot by the ship from Tutorial
 ## Island.
 ##
-## This will set the player's save bed to church in Brynknot.
+## This will set the player's save bed to church in Brynknot, if the
+## player's current save bed is on Tutorial Island.
 
 from Atrinik import *
 
 ## The activator.
 activator = WhoIsActivator()
 
-## Player info tag name.
-pinfo_tag = "VISITED_BRYNKNOT"
-
-## Search for the player info.
-pinfo = activator.GetPlayerInfo(pinfo_tag)
-
-# If no player info, we haven't been to Brynknot before
-if pinfo == None:
-	# Set the save bed
+if activator.GetSaveBed()["map"] == "/shattered_islands/world_0303":
 	activator.SetSaveBed(ReadyMap("/shattered_islands/world_0312", 0), 3, 7)
-
-	# Create the player info tag -- now we've been to Brynknot.
-	activator.CreatePlayerInfo(pinfo_tag)
