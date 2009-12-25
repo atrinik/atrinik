1c9be580957ce3da03e2ff4e460baa53b76ee557
Alex Tokar <admin@atokar.net>
2009-12-25 16:58:40 +0100

    Do not allow dropping unpaid items in the guild drop script if not in shop; could allow cheating by buying apartment extension, logging out and logging back in (which would put the buyer to the stairs, leaving the shop not paying for the extension).
    Former-commit-id: b21ba10aa986355d3d21b3cbaf6686ed31f86339

--- maps/python/guilds/guild_drop.py
+++ maps/python/guilds/guild_drop.py
@@ -11,6 +11,9 @@ activator = WhoIsActivator()
 ## Object who has the event object in their inventory.
 me = WhoAmI()
 
-if not activator.map.path[:8] == "/guilds/" or (activator.map.path[-7:] != "/oracle" and activator.map.path[-8:] != "/storage"):
+if me.f_unpaid:
+	activator.Write("You must pay for it first!", COLOR_RED)
+	SetReturnValue(1)
+elif not activator.map.path[:8] == "/guilds/" or (activator.map.path[-7:] != "/oracle" and activator.map.path[-8:] != "/storage"):
 	activator.Write("You cannot drop this object here.", COLOR_RED)
 	SetReturnValue(1)
