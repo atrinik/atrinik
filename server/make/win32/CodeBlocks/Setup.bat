b365fca24f866ced4f9f66d2f2c1f4581ea0b8e2
Alex Tokar <admin@atokar.net>
2010-02-03 19:35:18 +0100

    Update win32 pthread package to include needed dll.
    Former-commit-id: 586ab2938f51be2dd6d22492f2a40c61a6b3caf8

--- server/make/win32/CodeBlocks/Setup.bat
+++ server/make/win32/CodeBlocks/Setup.bat
@@ -9,6 +9,8 @@ tar xvf curl.tar
 gunzip -c pthread.tar.gz > pthread.tar
 tar xvf pthread.tar
 
+move *.dll ..\..\..\
+
 rem ====================================
 rem Clean up.
 rem ====================================
