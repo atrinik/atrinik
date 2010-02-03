0364b6d7cf3271f9844751073c15d739bea188f4
Alex Tokar <admin@atokar.net>
2010-02-03 15:12:26 +0100

    Update database structure for metaserver.
    Former-commit-id: 9bbe357eb5a97521e147134830539f5762140f2c

--- metaserver/db.sql
+++ metaserver/db.sql
@@ -1,18 +1,22 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 CREATE TABLE IF NOT EXISTS `blacklist` (
-  `id_blacklist` int(11) NOT NULL auto_increment,
-  `hostname` varchar(80) default NULL,
-  PRIMARY KEY  (`id_blacklist`)
+  `id_blacklist` int(11) NOT NULL AUTO_INCREMENT,
+  `hostname` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id_blacklist`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `servers` (
-  `id_server` int(11) NOT NULL auto_increment,
-  `ip_address` varchar(80) default NULL,
+  `id_server` int(11) NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(80) DEFAULT NULL,
   `port` int(11) NOT NULL,
-  `hostname` varchar(80) default NULL,
-  `num_players` int(11) default NULL,
-  `version` varchar(20) default NULL,
-  `text_comment` varchar(256) default NULL,
-  `last_update` int(21) default NULL,
-  PRIMARY KEY  (`id_server`),
+  `hostname` varchar(80) DEFAULT NULL,
+  `num_players` int(11) DEFAULT NULL,
+  `version` varchar(20) DEFAULT NULL,
+  `text_comment` varchar(256) DEFAULT NULL,
+  `last_update` int(21) DEFAULT NULL,
+  `players` text NOT NULL,
+  `roworder` mediumint(8) NOT NULL,
+  PRIMARY KEY (`id_server`),
   UNIQUE KEY `hostname` (`hostname`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
