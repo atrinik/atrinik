a64c5f3ebe38e5983ddb776404f8508ad89abdc2
Alex Tokar <admin@atokar.net>
2010-02-04 01:14:42 +0100

    Update metaserver database structure to put default values on some fields.
    Former-commit-id: 1c01210cc14c4704a74aa79e5e26c0a55a27cdc8

--- metaserver/db.sql
+++ metaserver/db.sql
@@ -9,14 +9,14 @@ CREATE TABLE IF NOT EXISTS `blacklist` (
 CREATE TABLE IF NOT EXISTS `servers` (
   `id_server` int(11) NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(80) DEFAULT NULL,
-  `port` int(11) NOT NULL,
+  `port` int(11) NOT NULL DEFAULT '13327',
   `hostname` varchar(80) DEFAULT NULL,
   `num_players` int(11) DEFAULT NULL,
   `version` varchar(20) DEFAULT NULL,
   `text_comment` varchar(256) DEFAULT NULL,
-  `last_update` int(21) DEFAULT NULL,
+  `last_update` int(21) NOT NULL,
   `players` text NOT NULL,
-  `roworder` mediumint(8) NOT NULL,
+  `roworder` mediumint(8) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_server`),
   UNIQUE KEY `hostname` (`hostname`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
