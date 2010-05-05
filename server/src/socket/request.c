8aeb8474f772c54799ea346f45439b3b65fd7d3d
Alex Tokar <admin@atokar.net>
2010-05-05 18:48:17 +0200

    Don't allow 1.0 client to connect to newer servers.
    Former-commit-id: 02eff4df4ed58373f8d085eaead4ecb01dba1aa4

--- server/src/socket/request.c
+++ server/src/socket/request.c
@@ -616,6 +616,13 @@ void VersionCmd(char *buf, int len, socket_struct *ns)
 		return;
 	}
 
+	if (ns->socket_version == 991017)
+	{
+		version_mismatch_msg(ns);
+		ns->status = Ns_Zombie;
+		return;
+	}
+
 	if (ns->socket_version != 991017 && ns->socket_version > SOCKET_VERSION)
 	{
 		send_socket_message(NDI_RED, ns, "This Atrinik server is outdated and incompatible with your client's version. Try another server.");
