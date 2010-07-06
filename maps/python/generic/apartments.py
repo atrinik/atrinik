f4fe63f5a4372f5b17cc6111c7058f052b49e2fe
Edwin Miltenburg <edwin.miltenburg@hotmail.com>
2010-07-06 22:40:23 +0200

    Add Treehome, apartments in Clearhaven on Eld Woods Island. wall_wood.arc: windows blocksview 0. apartments.py: add eld_woods_island. apartment_seller.py: change Darlin into 'me'.
    Former-commit-id: 3d3bc172cd6d2c00879918f9fe2d1317389d1233

--- maps/python/generic/apartments.py
+++ maps/python/generic/apartments.py
@@ -54,5 +54,45 @@ apartments_info = {
 		},
 		"tag": "strakewood_island_apartment",
 	},
+	"eld_woods_island":
+	{
+		"apartments":
+		{
+			"cheap":
+			{
+				"info": "\nThe cheap treehouse will cost you 20 silver.\nIt has only a bed and a chest.\nEvery treehouse is a kind of ^pocket dimension^.\nYou can enter it by using the teleporter there.\nSay ^sell me cheap apartment^ to buy it!\nChoose wisely!",
+				"id": 1,
+				"price": 2000,
+				"path": "/shattered_islands/eld_woods_island/apartments/apartment_cheap",
+				"x": 1,
+				"y": 2,
+			},
+			"normal":
+			{
+				"info": "\nThe normal treehouse will cost you 180 silver.\nIt has some storing devices and some furniture.\nEvery treehouse is a kind of ^pocket dimension^.\nYou can enter it by using the teleporter there.\nSay ^sell me normal apartment^ to buy it!\nChoose wisely!",
+				"id": 2,
+				"price": 18000,
+				"path": "/shattered_islands/eld_woods_island/apartments/apartment_normal",
+				"x": 1,
+				"y": 2,
+			},
+			"expensive":
+			{
+				"info": "\nThe expensive treehouse will cost you 11 gold.\nIt is large for a single treehouse and has many places to store items including a nice bed room.\nEvery apartment is a kind of ^pocket dimension^.\nYou can enter it by using the teleporter there.\nSay ^sell me expensive apartment^ to buy it!\nChoose wisely!",
+				"id": 3,
+				"price": 110000,
+				"path": "/shattered_islands/eld_woods_island/apartments/apartment_expensive",
+				"x": 1,
+				"y": 2,
+			},
+		},
+		"apartment_ids":
+		{
+			1: "cheap",
+			2: "normal",
+			3: "expensive",
+		},
+		"tag": "eld_woods_island_apartment",
+	},
 }
 
