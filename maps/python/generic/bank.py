55d7e5090dd88a7a3ea68880091dce3c0f0e0034
Edwin Miltenburg <edwin.miltenburg@hotmail.com>
2010-07-12 13:20:55 +0200

    Fix bug #68. Improve banker documentation.
    Former-commit-id: 59139b5bdc395bf829978cc42d115d992f7105e5

--- maps/python/generic/bank.py
+++ maps/python/generic/bank.py
@@ -14,19 +14,25 @@ pinfo_tag = "BANK_GENERAL"
 
 def main():
 	if msg == "bank" or msg == "hello" or msg == "hi" or msg == "hey":
-		me.SayTo(activator, "\nHello! I am {0}, the banker.\nWhat can I do for you? Do you need ^info^ about this bank?".format(me.name))
+		me.SayTo(activator, "\nHello! I am {0}, the banker.\nDo you want to ^deposit all^ or ^withdraw all^, or should I ^explain^ how we work?".format(me.name))
 
 	# Give out information about the bank
-	elif msg == 'info':
-		me.SayTo(activator, "\nWith the keyword ^balance^ I will tell how much money you have stored here on your account.\nStore money with ^deposit^ <# gold, # silver...>.\nGet money with ^withdraw^ <# gold, # silver...>.")
+	elif msg == 'explain':
+		me.SayTo(activator, "\nStoring your money in a bank account is a wise idea indeed. Just ask for the ^balance^ to see what's in your account. You can also ^deposit^ money to your account or ^withdraw^ money from it.")
 
 	elif msg == 'balance':
 		pinfo = activator.GetPlayerInfo(pinfo_tag)
 
 		if pinfo == None or pinfo.value == 0:
-			me.SayTo(activator, "\nYou have no money stored.")
+			me.SayTo(activator, "\nYou have no money stored in your bank account.\nWould you like to ^deposit all^ your money?")
 		else:
-			me.SayTo(activator, "\n{0}, your balance is {1}.".format(activator.name, activator.ShowCost(pinfo.value)))
+			me.SayTo(activator, "\nYour balance is {0}.".format(activator.ShowCost(pinfo.value)))
+
+	elif msg == 'deposit':
+		me.SayTo(activator, "\nYou can store money in your account by saying ~deposit~ followed by the amount.\nFor example ~deposit 1 mithril, 99 silver~\nYou can also ^deposit all^.")
+
+	elif msg == 'withdraw':	
+		me.SayTo(activator, "\nYou can get money from your account saying by ~withdraw~ followed by the amount.\nFor example ~withdraw 2 gold, 40 copper~\nYou can also ^withdraw all^.")
 
 	# Deposit money
 	elif text[0] == 'deposit':
@@ -41,7 +47,7 @@ def main():
 			me.SayTo(activator, "\nYou don't have that much money.")
 		elif dpose == 1:
 			if pinfo.value != 0:
-				me.SayTo(activator, "\n{0}, your new balance is {1}.".format(activator.name, activator.ShowCost(pinfo.value)))
+				me.SayTo(activator, "\nYour new balance is {0}.".format(activator.ShowCost(pinfo.value)))
 
 	# Withdraw some money
 	elif text[0] == 'withdraw':
@@ -58,6 +64,6 @@ def main():
 				if pinfo.value == 0:
 					me.SayTo(activator, "\nYou removed all your money.")
 				else:
-					me.SayTo(activator, "\n{0}, your new balance is {1}.".format(activator.name, activator.ShowCost(pinfo.value)))
+					me.SayTo(activator, "\nYour new balance is {0}.".format(activator.ShowCost(pinfo.value)))
 
 main()
