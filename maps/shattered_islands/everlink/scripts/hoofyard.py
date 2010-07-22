cddd59e2956770b776df987e5313e86513234f00
Alex Tokar <admin@atokar.net>
2010-07-22 20:43:11 +0200

    Add hint about destroying the extra walls in houses to build things.
    Former-commit-id: 2ccb11869407ed2c2a5ca7e60a492d598149ebe8

--- maps/shattered_islands/everlink/scripts/hoofyard.py
+++ maps/shattered_islands/everlink/scripts/hoofyard.py
@@ -24,7 +24,7 @@ def main():
 			me.SayTo(activator, "\nAh, a visitor! Do you have the ^construction^ skill yet?")
 
 	elif msg == "information":
-		me.SayTo(activator, "\nTo use the construction skill, you need to buy a construction builder and a construction destroyer. With the destroyer, you can destroy previously built objects or walls. Use the builder to build walls, windows, fireplaces, and so on. To use either the destroyer or the builder, you must first apply it, and then type\n~/use_skill construction~ or find the construction skill in your skill list, press enter and use CTRL + direction.\n\n^Information about materials^")
+		me.SayTo(activator, "\nTo use the construction skill, you need to buy a construction builder and a construction destroyer. With the destroyer, you can destroy previously built objects or walls. You will need a destroyer to destroy the extra walls in your house (in Greyton house, this is the eastern wall, for example). Use the builder to build walls, windows, fireplaces, and so on. To use either the destroyer or the builder, you must first apply it, and then type ~/use_skill construction~ or find the construction skill in your skill list, press enter and use CTRL + direction.\n\n^Information about materials^")
 
 	elif msg == "information about materials":
 		me.SayTo(activator, "\nTo build using the construction builder, you need to mark a material you want to build. For example, an altar material, desk material, or wall material. Sign materials exist too, but to build them, you need a book on the square where you want to build the sign with the message you want the sign to have (custom name of the book will be copied to the sign). Windows, pictures, flags and so on can only be built on top of walls.")
