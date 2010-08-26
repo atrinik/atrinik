3d1bac94f5d241115b1536fbbee0718b0269e1e7
Alex Tokar <admin@atokar.net>
2010-08-26 04:58:56 +0200

    Modify script for Warden Argoth, now he'll attack the first person who calls him a drunk for the tenth time.
    Former-commit-id: 9532b585ac984012a834d72612daee56703d251e

--- maps/shattered_islands/scripts/asteria/WardenArgoth.py
+++ maps/shattered_islands/scripts/asteria/WardenArgoth.py
@@ -2,24 +2,50 @@
 ## Implements the angry drunk Warden who drops Asteria Dungeon Key 1.
 
 from Atrinik import *
-from QuestManager import QuestManager
 
 activator = WhoIsActivator()
 me = WhoAmI()
-
 msg = WhatIsMessage().strip().lower()
 
 def main():
+	if me.enemy:
+		if me.enemy == activator:
+			me.SayTo(activator, "\nNow you die!")
+		else:
+			me.SayTo(activator, "\nCan't you see I'm busy here?!")
+
+		return
+
 	if msg == "hello" or msg == "hi" or msg == "hey":
-        me.Communicate("/yawn " + activator.name)
+		me.Communicate("/yawn")
 		me.SayTo(activator, "\nHello {0}, I am {1}.  So what ^brings^ you here?".format(activator.name, me.name))
 
 	elif msg == "brings":
 		me.SayTo(activator, "\nI don't suppose you've come about the ^key^.  Sorry, I can't give that to you.")
 
-    elif msg == "key":
+	elif msg == "key":
 		me.SayTo(activator, "\nI told you, I can't give that to you.")
 
-    elif msg == "drunk" or msg == "drunkard":
-		me.SayTo(activator, "\nWhat did you call me?")
-        ## TODO: Make him get mad when you call him this... 10 tries before he goes hostile.
+	elif msg == "drunk" or msg == "drunkard":
+		try:
+			num = int(me.ReadKey("drunk_called"))
+		except:
+			num = 0
+
+		if num == 0:
+			me.SayTo(activator, "\nWhat did you call me?")
+		elif num == 1:
+			me.SayTo(activator, "\nDon't call me that, I hate it!")
+		elif num == 8:
+			me.SayTo(activator, "\nThat's it! The next one who says I'm a drunk dies!")
+		elif num == 9:
+			me.SayTo(activator, "\nNow you've done it! You'll pay for that!")
+			me.enemy = activator
+			me.WriteKey("drunk_called")
+			return
+		else:
+			me.SayTo(activator, "\nI told you, I hate that!")
+
+		me.WriteKey("drunk_called", str(num + 1))
+
+main()
