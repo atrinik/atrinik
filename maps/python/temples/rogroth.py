702db50a6a82a1cfbd5386caa13efc49143a6b62
emperorbma <emperorbma@suddenlink.net>
2010-10-02 16:03:09 -0500

    correct temple lighting and a temple script mentioning the wrong deity.
    Former-commit-id: 8e0b1a1b74dbf433a2c44090f4bef6d6364c64bf

--- maps/python/temples/rogroth.py
+++ maps/python/temples/rogroth.py
@@ -11,7 +11,7 @@ msg = WhatIsMessage().strip().lower()
 ## Rogroth.
 class TempleRogroth(Temple.BaseTemple):
 	name = "Rogroth"
-	desc = "I am a servant of the King of the Stormy Skies and the God of Lightning, Rogroth.\nIf you would like to join our Temple, please touch the altar and Drolaxi will smile upon you."
+	desc = "I am a servant of the King of the Stormy Skies and the God of Lightning, Rogroth.\nIf you would like to join our Temple, please touch the altar and Rogroth will smile upon you."
 
 def main():
 	Temple.handle_temple(TempleRogroth, me, activator, msg)
