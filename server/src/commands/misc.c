e82bff03fbaef31bed7d1659ba4d06d87846f16e
Alex Tokar <admin@atokar.net>
2010-10-09 07:10:39 +0200

    Do not send new player command after confirming password to newer clients.
    Former-commit-id: 63cbbb3a9dc3c3c2ec76d5cc1bfacca3e1ff5010

--- server/src/commands/misc.c
+++ server/src/commands/misc.c
@@ -437,7 +437,11 @@ void receive_player_password(object *op)
 			return;
 		}
 
-		esrv_new_player(CONTR(op), 0);
+		if (CONTR(op)->socket.socket_version < 1042)
+		{
+			esrv_new_player(CONTR(op), 0);
+		}
+
 		Write_String_To_Socket(&CONTR(op)->socket, BINARY_CMD_NEW_CHAR, cmd_buf, 1);
 		LOG(llevInfo, "NewChar send for %s\n", op->name);
 		CONTR(op)->state = ST_ROLL_STAT;
