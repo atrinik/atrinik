d2e578cabb7c6931d2a01a5b38cc97ab85a7fbc2
Alex Tokar <admin@atokar.net>
2010-11-02 23:08:44 +0100

    Add the Halloween maps.
    Former-commit-id: c1fce8736f6fc61b91267d27e8d1ca03ee257c77

---
+++ maps/events/2010/halloween/scripts/map_enter.py
@@ -0,0 +1,17 @@
+## @file
+## Script ran when player enters the abyss map.
+##
+## Adjusts the map difficulty and sets magic wall levels.
+
+from Atrinik import *
+
+activator = WhoIsActivator()
+# Same level as the activator.
+activator.map.difficulty = activator.level
+
+# Adjust the magic walls.
+for (x, y) in [(29, 6), (29, 14), (37, 6), (37, 14)]:
+	for ob in activator.map.GetLayer(x, y, LAYER_SYS):
+		if ob.name == "magic wall":
+			ob.level = activator.level
+			break
