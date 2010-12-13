9631cb0d2a0e023fbf79d2d88e0cfa31dedb74d0
Alex Tokar <admin@atokar.net>
2010-12-13 23:34:38 +0100

    Remove freearr_x/freearr_y/SIZEOFFREE definitions from Common.
    Former-commit-id: 33b7fe4895aacfa55a332a5638ae2ed3e7cf4e1e

--- maps/python/Common.py
+++ maps/python/Common.py
@@ -1,8 +1,6 @@
 ## @file
 ## This file holds common code used across Atrinik Python scripts.
 
-import random
-
 ## Calculate the diagonal distance between two X and Y coordinates.
 def diagonal_distance(x1, y1, x2, y2):
 	return max(abs(x1 - x2), abs(y1 - y2))
@@ -25,13 +23,5 @@ def absdir(d):
 ## @param d The exact direction.
 ## @return The randomized direction. */
 def get_randomized_dir(d):
-	return absdir(d + random.randint(0, 2) + random.randint(0, 2) - 2);
-
-SIZEOFFREE1 = 8
-SIZEOFFREE2 = 24
-SIZEOFFREE = 49
-
-## X offset when searching around a spot.
-freearr_x = [0, 0, 1, 1, 1, 0, -1, -1, -1, 0, 1, 2, 2, 2, 2, 2, 1, 0, -1, -2, -2, -2, -2, -2, -1, 0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -3, -3, -3, -3, -2, -1]
-## Y offset when searching around a spot.
-freearr_y = [0, -1, -1, 0, 1, 1, 1, 0, -1, -2, -2, -2, -1, 0, 1, 2, 2, 2, 2, 2, 1, 0, -1, -2, -2, -3, -3, -3, -3, -2, -1, 0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 2, 1, 0, -1, -2, -3, -3, -3]
+	from random import randint
+	return absdir(d + randint(0, 2) + randint(0, 2) - 2);
