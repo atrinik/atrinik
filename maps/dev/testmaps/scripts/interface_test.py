994d806c3474fae6ac8974402f981c8e5b30487b
Alex Tokar <admin@atokar.net>
2010-12-15 02:31:33 +0100

    Remove unnecessary link characters.
    Former-commit-id: a38163d9f33a679131fd926679f1f215e8e7add6

--- maps/dev/testmaps/scripts/interface_test.py
+++ maps/dev/testmaps/scripts/interface_test.py
@@ -14,7 +14,7 @@ def main():
 		return
 
 	if msg == "hi" or msg == "hey" or msg == "hello":
-		pl.WriteToSocket(30, "X<book>{}</book><img={} -10 -15>\n\nThis is NPC interface test.\n\nOur offers:\n\n<size=14><c=#000000><u><a=:/t_tell ^buy 10^></o>Buy 10 beer</a></c></u></size>\n<size=14><c=#000000>Buy 10 cakes</c></size><y=2> -- currently out of stock<y=-2>".format(me.name, "monk.131"))
+		pl.WriteToSocket(30, "X<book>{}</book><img={} -10 -15>\n\nThis is NPC interface test.\n\nOur offers:\n\n<size=14><c=#000000><u><a=:/t_tell buy 10></o>Buy 10 beer</a></c></u></size>\n<size=14><c=#000000>Buy 10 cakes</c></size><y=2> -- currently out of stock<y=-2>".format(me.name, "monk.131"))
 	elif msg == "buy 10":
 		pl.WriteToSocket(30, "X<book>{}</book><img={} -10 -15>\n\nBeer's bad for you!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nJust kidding!\n\n<img=beer.101>\n\n\n\n\n\n\n ".format(me.name, "monk.131"))
 
