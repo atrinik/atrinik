89cb7ca60cc11574ba8a9598a04322585a616e60
Alex Tokar <admin@atokar.net>
2010-12-17 00:55:56 +0100

    Do not allow dogs in Asteria to move off the grass.
    Former-commit-id: 4faa643b81256ed3c0764d69daccc12de71abab6

---
+++ maps/python/generic/random_move_block.py
@@ -0,0 +1,23 @@
+## @file
+## Blocks object from randomly moving onto all squares which have floor,
+## except those that match regex specified in event options (matched
+## against the floor's arch name).
+
+from Atrinik import WhoAmI, GetOptions, LAYER_FLOOR
+import re
+
+me = WhoAmI()
+options = GetOptions()
+# Direction the object wants to move into
+d = GetEventParameters()[1]
+
+try:
+	# Get the floor on the square the object wants to move into.
+	floor = me.map.GetLayer(me.x + freearr_x[d], me.y + freearr_y[d], LAYER_FLOOR)[0]
+
+	if not re.match(options, floor.arch.name):
+		SetReturnValue(1)
+		# Stop showing the moving animation.
+		me.anim_moving_dir = -1
+except:
+	pass
