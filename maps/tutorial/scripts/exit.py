d7fc55f6a1e33e4db2f0fa05186fc02b8e4de59f
Alex Tokar <admin@atokar.net>
2011-03-19 01:07:04 +0100

    Remove extraneous oranges when exiting the Tutorial Island.
    Former-commit-id: 050a7f8813725a34476189b636cdc80939fbe1ab

--- maps/tutorial/scripts/exit.py
+++ maps/tutorial/scripts/exit.py
@@ -26,6 +26,36 @@ elif options == "cave-exit" or options == "island-exit":
 
 		if obj:
 			obj.Remove()
+	# Exiting the island; remove extraneous oranges.
+	elif options == "island-exit":
+		# Total number of oranges found in inventory.
+		total = 0
+		# Maximum number of oranges allowed.
+		max_oranges = 5
+		# Has at least one orange been removed?
+		removed = False
+
+		# Look for oranges inside the player.
+		for obj in activator.FindObject(mode = INVENTORY_CONTAINERS, archname = "orange", multiple = True):
+			# Already over the limit? Remove the orange.
+			if total >= max_oranges:
+				obj.Remove()
+				removed = True
+				continue
+
+			# Get the nrof.
+			obj_nrof = max(1, obj.nrof)
+
+			# Would we go over the total? Reduce from the stack.
+			if total + obj_nrof > max_oranges:
+				obj.Decrease(obj_nrof + total - max_oranges)
+				removed = True
+
+			total += obj_nrof
+
+		# If we removed any oranges, write out a message...
+		if removed:
+			activator.Write("The portal's magics have destroyed some of your oranges...", COLOR_RED)
 
 	pl.savebed_map = me.slaying
 	pl.bed_x = me.hp
