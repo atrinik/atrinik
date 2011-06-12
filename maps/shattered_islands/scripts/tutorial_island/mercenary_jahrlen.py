70ed4e2bd1a2511af601cced52e4bc7e22e3c6ce
Alex Tokar <admin@atokar.net>
2011-06-12 09:24:00 +0200

    Use markup in mercenary_jahrlen.py.
    Former-commit-id: 657bcf450f3af73710cd630ebda1288c40596b80

--- maps/shattered_islands/scripts/tutorial_island/mercenary_jahrlen.py
+++ maps/shattered_islands/scripts/tutorial_island/mercenary_jahrlen.py
@@ -36,7 +36,7 @@ def teach_wizardy():
 
 def main():
 	if msg == "hello" or msg == "hi" or msg == "hey":
-		me.SayTo(activator, "\nHello, I am Jahrlen, War ^Chronomancer^ of Thraal.\nI can teach you the art of magic and your first spells.\nI'll teach you probe for ^free^ and magic bullet for doing a ^quest^.")
+		me.SayTo(activator, "\nHello, I am Jahrlen, War <a>Chronomancer</a> of Thraal.\nI can teach you the art of magic and your first spells.\nI'll teach you probe for <a>free</a> and magic bullet for doing a <a>quest</a>.")
 
 	elif text[0] == "probe":
 		guild_force = activator.GetGuildForce()
@@ -71,9 +71,9 @@ def main():
 				me.SayTo(activator, "\nThe foul kobolds and goblins under the jail have been there long, but recently they started making trouble around here.\nIn the dead of the night, one of the goblins sneaked in here, and stole my old heavy rod of magic bullet!\nIf you can find it, I will teach you the spell magic bullet.\nI bet their chief leader, Bolubal, would have it.\nPlease be careful though. Talk to Frah'ak, the ogre in jail, if you don't know about the kobolds den yet.")
 
 				if not qm.started():
-					me.SayTo(activator, "Do you ^accept^ this quest?", 1)
+					me.SayTo(activator, "Do you <a>accept</a> this quest?", 1)
 			else:
-				me.SayTo(activator, "\nYou found the rod! Incredible! Say ^bullet^ now to learn the spell of magic bullet, as your reward.")
+				me.SayTo(activator, "\nYou found the rod! Incredible! Say <a>bullet</a> now to learn the spell of magic bullet, as your reward.")
 
 	# Accept the quest.
 	elif text[0] == "accept":
@@ -109,7 +109,7 @@ def main():
 						activator.Controller().AcquireSpell(spell)
 
 	elif text[0] == "free":
-		me.SayTo(activator, "\nI can teach you the spell |probe|.\nProbe is one of the most useful information spells you can learn.\nCast on unknown creatures it will grant you information about their powers and weaknesses.\nThe spell itself is very safe. Creatures will not notice that they were probed. They will not get angry or attack.\nSay ^probe^, then I will teach you the probe spell.\nIf you don't have the needed wizardry skill,\nI will teach it to you too.")
+		me.SayTo(activator, "\nI can teach you the spell <yellow>probe</yellow>.\nProbe is one of the most useful information spells you can learn.\nCast on unknown creatures it will grant you information about their powers and weaknesses.\nThe spell itself is very safe. Creatures will not notice that they were probed. They will not get angry or attack.\nSay <a>probe</a>, then I will teach you the probe spell.\nIf you don't have the needed wizardry skill,\nI will teach it to you too.")
 
 	elif text[0] == "chronomancer":
 		me.SayTo(activator, "\nYes, I am a master of the chronomancers of Thraal.\nWe are one of the most powerful wizard guilds.\nPerhaps, if you are higher level and stronger...\nHmm... If you ever meet Rangaron in your life... Then tell him that Jahrlen has sent you. Don't ask me more now.")
