78d6fabd8281685c157689029e805c9c26293bf5
Alex Tokar <admin@atokar.net>
2011-06-12 09:24:06 +0200

    Use markup in weapons_master.py.
    Former-commit-id: 644fa15416b4962066bfd7fd3cb602994aabdddc

--- maps/shattered_islands/scripts/tutorial_island/weapons_master.py
+++ maps/shattered_islands/scripts/tutorial_island/weapons_master.py
@@ -30,7 +30,7 @@ def learn_weapon_skill(skill, weapon):
 
 def main():
 	if msg == "hello" or msg == "hi" or msg == "hey":
-		me.SayTo(activator, "\nHello! I am {0}.\nI will give you your starting weapon skill and your first weapon. Now, tell me which weapon skill do you want.\nYou can select between slash weapons, cleave weapons, pierce weapons or impact weapons.\nAsk me about ^weapons^ to learn more.".format(me.name))
+		me.SayTo(activator, "\nHello! I am {0}.\nI will give you your starting weapon skill and your first weapon. Now, tell me which weapon skill do you want.\nYou can select between slash weapons, cleave weapons, pierce weapons or impact weapons.\nAsk me about <a>weapons</a> to learn more.".format(me.name))
 
 	elif msg == "slash":
 		learn_weapon_skill("slash weapons", "shortsword")
@@ -45,7 +45,7 @@ def main():
 		learn_weapon_skill("pierce weapons", "dagger_large")
 
 	elif msg == "weapons":
-		me.SayTo(activator, "\nWe have 4 different weapon skills.\nEach skill allows the use of a special kind of weapons.\nSlash weapons are swords.\nCleave weapons are axe-like weapons.\nPierce weapons are rapiers and daggers.\nImpact weapons are maces or hammers.\nNow select one and tell me: ^slash^, ^cleave^, ^pierce^ or ^impact^?")
+		me.SayTo(activator, "\nWe have 4 different weapon skills.\nEach skill allows the use of a special kind of weapons.\nSlash weapons are swords.\nCleave weapons are axe-like weapons.\nPierce weapons are rapiers and daggers.\nImpact weapons are maces or hammers.\nNow select one and tell me: <a>slash</a>, <a>cleave</a>, <a>pierce</a> or <a>impact</a>?")
 
 if activator.Controller().DoKnowSkill(GetSkillNr("impact weapons")) or activator.Controller().DoKnowSkill(GetSkillNr("slash weapons")) or activator.Controller().DoKnowSkill(GetSkillNr("cleave weapons")) or activator.Controller().DoKnowSkill(GetSkillNr("pierce weapons")):
 	me.SayTo(activator, "\nYou already know a weapon skill.")
