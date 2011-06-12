cdb461455033840879a8ade8cb30dba57e1e8530
Alex Tokar <admin@atokar.net>
2011-06-12 06:46:37 +0200

    Remove compatibility code from bard script.
    Former-commit-id: fcac722f71e7600e28dd3f3362851caa88f83b84

--- maps/python/generic/bard.py
+++ maps/python/generic/bard.py
@@ -22,10 +22,6 @@ try:
 except:
 	raise ValueError("Invalid area: '{0}'".format(GetOptions()))
 
-# Remove songs that 1.1 client doesn't have.
-if activator.Controller().s_socket_version < 1038:
-	areas[GetOptions()].remove(("fuego.ogg", "Fuego"))
-
 ## Adjust the background music of a single map, updating background
 ## music of all players on that map.
 ## @param m What map to update.
@@ -35,9 +31,7 @@ def adjust_map_music(m, music):
 	m.bg_music = music
 
 	for player in m.GetPlayers():
-		# 1.1 client doesn't support this, unfortunately.
-		if player.Controller().s_socket_version >= 1038:
-			player.Controller().Sound(m.bg_music and m.bg_music or "no_music", CMD_SOUND_BACKGROUND, loop = -1)
+		player.Controller().Sound(m.bg_music and m.bg_music or "no_music", CMD_SOUND_BACKGROUND, loop = -1)
 
 def main():
 	if msg == "hi" or msg == "hey" or msg == "hello":
@@ -56,17 +50,11 @@ def main():
 		# Handle 'None' specially.
 		if not l[0]:
 			if me.map.bg_music:
-				if activator.Controller().s_socket_version < 1038:
-					me.SayTo(activator, "\nOkay, I will stop playing the current song when you re-enter the map.")
-				else:
-					me.SayTo(activator, "\nOkay, I will stop playing the current song now.")
+				me.SayTo(activator, "\nOkay, I will stop playing the current song now.")
 			else:
 				me.SayTo(activator, "\nI'm not playing anything at the moment.")
 		else:
-			if activator.Controller().s_socket_version < 1038:
-				me.SayTo(activator, "\nOkay, I will play ~{0}~ then... Hold on one moment!\n...\nThere. I will start the new song when you re-enter the map.".format(l[1]))
-			else:
-				me.SayTo(activator, "\nOkay, I will play ~{0}~ then... Hold on one moment... There!".format(l[1]))
+			me.SayTo(activator, "\nOkay, I will play ~{0}~ then... Hold on one moment... There!".format(l[1]))
 
 		event_msg = WhatIsEvent().msg
 
