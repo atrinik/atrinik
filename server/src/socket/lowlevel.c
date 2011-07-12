5c5da366b2de63b5e4251b41ed3ebca1cf5ce4d6
Alex Tokar <admin@atokar.net>
2011-07-12 02:24:17 +0200

    Add utility function GetString_String() from client to server.
    Former-commit-id: 829cd8658057ca0d1c564a8efa723a075b836f91

--- server/src/socket/lowlevel.c
+++ server/src/socket/lowlevel.c
@@ -49,6 +49,30 @@ void SockList_AddString(SockList *sl, const char *data)
 }
 
 /**
+ * Construct a string from data packet.
+ * @param data Data packet.
+ * @param[out] pos Position in the data packet.
+ * @param dest Will contain the string from data packet.
+ * @param dest_size Size of 'dest'.
+ * @return 'dest'. */
+char *GetString_String(uint8 *data, int *pos, char *dest, size_t dest_size)
+{
+	size_t i = 0;
+	char c;
+
+	while ((c = (char) (data[(*pos)++])))
+	{
+		if (i < dest_size - 1)
+		{
+			dest[i++] = c;
+		}
+	}
+
+	dest[i] = '\0';
+	return dest;
+}
+
+/**
  * Reads from socket.
  * @param ns Socket to read from.
  * @param len Max length of data to read.
