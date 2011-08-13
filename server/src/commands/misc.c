ef5cfb0f6fedcba7f2d8f9d40aa4c178e0e1bea5
Alex Tokar <admin@atokar.net>
2011-08-13 14:03:35 +0200

    Remove compatibility code for /party who.
    Former-commit-id: 8f4a161bd8c9e16696b2d407048b67a8941595bd

--- server/src/commands/misc.c
+++ server/src/commands/misc.c
@@ -840,19 +840,10 @@ int command_party(object *op, char *params)
 
 			for (ol = CONTR(op)->party->members; ol; ol = ol->next)
 			{
-			if (CONTR(op)->socket.socket_version >= 1054)
-			{
 				SockList_AddString(&sl, ol->objlink.ob->name);
 				SockList_AddChar(&sl, MAX(1, MIN((double) ol->objlink.ob->stats.hp / ol->objlink.ob->stats.maxhp * 100.0f, 100)));
 				SockList_AddChar(&sl, MAX(1, MIN((double) ol->objlink.ob->stats.sp / ol->objlink.ob->stats.maxsp * 100.0f, 100)));
 				SockList_AddChar(&sl, MAX(1, MIN((double) ol->objlink.ob->stats.grace / ol->objlink.ob->stats.maxgrace * 100.0f, 100)));
-			}
-			else
-			{
-				SockList_AddString(&sl, ol->objlink.ob->name);
-				SockList_AddString(&sl, ol->objlink.ob->map->name);
-				SockList_AddChar(&sl, (char) ol->objlink.ob->level);
-			}
 			}
 		}
 		else if (!strncmp(params, "join ", 5))
