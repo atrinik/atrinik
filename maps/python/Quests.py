25df2df230df0d61621908e2230b154b27635279
Alex Tokar <admin@atokar.net>
2011-08-13 13:05:23 +0200

    Use bartender API in Gashir's script; solves #267.
    Former-commit-id: a281d0bb4e65f1ad67b8a16883050de178490955

---
+++ maps/python/Quests.py
@@ -0,0 +1,13 @@
+## @file
+## Quests used across several NPCs.
+
+from Atrinik import *
+
+## The shipment of Charob Beer quest.
+ShipmentOfCharobBeer = {
+	"quest_name": "Shipment of Charob Beer",
+	"type": QUEST_TYPE_KILL_ITEM,
+	"arch_name": "barrel2.101",
+	"item_name": "shipment of Charob Beer",
+	"message": "Deliver Charob Beer to the Asterian Arms Tavern.",
+}
