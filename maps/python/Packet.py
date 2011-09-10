c9d670dad583ed82cc022e6d5ba09d6795a0525b
Alex Tokar <admin@atokar.net>
2011-09-10 15:50:12 +0200

    Add Packet module, which currently implements Notification method for sending notification data packets to clients.
    Former-commit-id: e67d3fa39ebfd93296839226362c647a9652f82f

---
+++ maps/python/Packet.py
@@ -0,0 +1,35 @@
+## @file
+## Implements packet-specific functions, such as sending a notification
+## command to the player's client or creating a book GUI, for example.
+##
+## @author Alex Tokar
+
+## Send a notification command to the player.
+## @param pl The player.
+## @param msg Message contents of the notification.
+## @param action Optional macro or command to execute when the
+## notification is triggered.
+## @param shortcut Macro shortcut to temporarily use as trigger for the
+## notification's action, for example, "?HELP".
+## @param delay How long the delay should stay visible, in milliseconds.
+def Notification(pl, msg, action = None, shortcut = None, delay = 0):
+	fmt = "Bs"
+	data = [0, msg]
+
+	# Add the action.
+	if action:
+		fmt += "Bs"
+		data += [1, action]
+
+		# Add the shortcut, but only if there is an action.
+		if shortcut:
+			fmt += "Bs"
+			data += [2, shortcut]
+
+	# Add the delay.
+	if delay:
+		fmt += "Bi"
+		data += [3, delay]
+
+	# Send it off...
+	pl.SendPacket(40, fmt, *data)
