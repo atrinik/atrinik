b06749d49cd5daf63e7e91c5864b6a087b501a10
Alex Tokar <admin@atokar.net>
2011-09-21 04:41:47 +0200

    Update script.
    Former-commit-id: 2e0368df5edf212db01eefb839eb58bc4c556fb3

--- maps/shattered_islands/incuna/scripts/sam_goodberry.py
+++ maps/shattered_islands/incuna/scripts/sam_goodberry.py
@@ -1,4 +1,3 @@
-from Atrinik import *
 from Interface import Interface
 from QuestManager import QuestManagerMulti
 from Quests import LostMemories as quest
@@ -12,7 +11,7 @@ inf = Interface(activator, me)
 
 def main():
 	if not qm.started_part(1):
-		if msg == "hi" or msg == "hey" or msg == "hello":
+		if msg == "hello":
 			inf.add_msg("There you are! I didn't wake you up when we arrived in Incuna, because it seemed like you needed a good rest. You certainly did - you've been sleeping for quite a while now.")
 			inf.add_msg("At any rate, we did it - we managed to reach Incuna. We should resupply and head to Strakewood at last - but don't worry, I'll handle that and anything else we may need.")
 			inf.add_msg("However... there's still the question of what to do about your memory loss. Say, has it improved?")
@@ -30,7 +29,7 @@ def main():
 			qm.start(1)
 
 	elif qm.completed():
-		if msg == "hi" or msg == "hey" or msg == "hello":
+		if msg == "hello":
 			inf.add_msg("Ah, good! You seem much more confident now - almost the same person I have seen in you when you first came to me and hired me to transport you to Strakewood.")
 			inf.add_msg("Very well then. Are you packed up and ready to go?")
 			inf.add_link("I'm ready.")
@@ -40,7 +39,7 @@ def main():
 			inf.close_dialog()
 
 	else:
-		if msg == "hi" or msg == "hey" or msg == "hello":
+		if msg == "hello":
 			inf.add_msg("Hmmm... You don't seem ready yet, {}. I am sorry, but unless you regain at least part of your memory, there's no reason for us to sail to Strakewood...".format(activator.name))
 
 main()
