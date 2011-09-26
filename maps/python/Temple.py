62cad638978650518ef205281683372420b7978b
Alex Tokar <admin@atokar.net>
2011-09-26 18:24:21 +0200

    Some dialog changes in Temple class.
    Former-commit-id: c7df1b1fdc453e1fcfa759fbb50e60d8f0000df8

--- maps/python/Temple.py
+++ maps/python/Temple.py
@@ -25,47 +25,48 @@ class BaseTemple:
 		# Initialize default services.
 		#
 		# List entry meanings:
-		# - If True this is a spell and can be automatically handled,
-		#   otherwise you need a special action in handle_chat().
-		# - The amount of money required to pay for the service
+		# - The amount of money required to pay for the service.
 		# - Row order. The highest will be as the first service, while
-		# - the lowest will be the last service.
+		#   the lowest will be the last service.
+		# - Name of the service, as it appears in the links
+		# - Explanation of the service.
 		self.services = {
 			"remove depletion": [
-				True,
 				self._activator.level * 100,
 				25,
+				"Removal of depletion",
+				"Depletion occurs when you die, and it drains some of your stat attributes. In order to get them back, <green>remove depletion</green> prayer can be used. I can cast this prayer on you, if you wish.",
 			],
 			"remove curse": [
-				True,
 				3000,
 				20,
+				"Removal of curse from all cursed items",
+				"I will remove any curse from all cursed items in your inventory. Note that the curse may still come back, if the item is permanently cursed...",
 			],
 			"remove damnation": [
-				True,
 				5000,
 				15,
+				"Removal of damnation from all damned items",
+				"I will remove any damnation from all damned items in your inventory. Note that the damnation may still come back, if the item is permanently damned...",
 			],
 			"cure disease": [
-				True,
 				self._activator.level * 40,
 				10,
+				"Curing of disease",
+				"I can attempt to cure any disease that is troubling you.",
 			],
 			"cure poison": [
-				True,
 				self._activator.level * 5,
 				5,
+				"Curing of poison",
+				"I can heal your poisoning, if you wish.",
 			],
 			"food": [
-				False,
 				0,
 				0,
+				"Spare bit of food",
 			],
 		}
-
-		# Adjust prices for players under level 3.
-		if self._activator.level < 3:
-			self.services["remove depletion"][1] = self.services["cure disease"][1] = self.services["cure poison"][1] = 0
 
 	## Check whether activator is of an enemy god.
 	def is_enemy_god(self):
@@ -80,13 +81,12 @@ class BaseTemple:
 	## @return True if the chat was handled, False otherwise.
 	def handle_chat(self, msg):
 		if msg == "hello":
-			self._inf.add_msg("Welcome to the church of {}.".format(self._name))
-			self._inf.add_link("Who is {}?".format(self._name), dest = self._name)
+			self._inf.add_msg("Welcome to the church of {}. I am {}.".format(self._name, self._me.name))
 
 			# We don't like enemy gods.
 			if self.is_enemy_god():
 				self._inf.add_msg("... I see that you are a follower of {}. It would be best for you to leave immediately.".format(self._enemy_name))
-				self._inf.add_link("Who is {}?".format(self._enemy_name), dest = self._enemy_name)
+				self._inf.add_link("Tell me about {}.".format(self._enemy_name), dest = self._enemy_name)
 				return True
 
 			# Custom message to say before services.
@@ -94,13 +94,15 @@ class BaseTemple:
 				self._inf.add_msg(self.hello_msg)
 
 			# Sort the services.
-			services = sorted(self.services, key = lambda key: self.services[key][2], reverse = True)
+			services = sorted(self.services, key = lambda key: self.services[key][1], reverse = True)
 
 			if services:
 				self._inf.add_msg("I can offer you the following services.")
 
 				for service in services:
-					self._inf.add_link(service.capitalize() + ".", dest = service)
+					self._inf.add_link(self.services[service][2], dest = service)
+
+			self._inf.add_link("Tell me about {}.".format(self._name), dest = self._name)
 
 			return True
 		# Explain about the temple's god.
@@ -128,27 +130,28 @@ class BaseTemple:
 		if not service:
 			return False
 
-		if service[0]:
-			if not is_buy:
-				self._inf.add_msg("{} will cost you {}. Is that okay?".format(msg.title(), CostString(service[1])))
-				self._inf.add_link("<a=:buy {}>Yes.</a>".format(msg))
-				self._inf.add_link("<a=close:>No thanks.</a>")
+		if msg == "food":
+			if self._activator.food < 500:
+				self._activator.food = 500
+				self._inf.add_msg("Your stomach is filled again.")
 			else:
-				if self._activator.PayAmount(service[1]):
-					if service[1]:
-						self._inf.add_msg("You pay {}.".format(CostString(service[1])), COLOR_YELLOW)
+				self._inf.add_msg("You don't look very hungry to me...")
+		else:
+			self._inf.add_msg("<title>{}</title>".format(service[2]))
 
-					self._inf.add_msg("Okay, I will cast {} on you now.".format(msg))
+			if not is_buy:
+				self._inf.add_msg(service[3])
+				self._inf.add_msg("This will cost you {}.".format(CostString(service[0])))
+				self._inf.add_link("Confirm", dest = "buy " + msg)
+			else:
+				if self._activator.PayAmount(service[0]):
+					if service[0]:
+						self._inf.add_msg("You pay {}.".format(CostString(service[0])), COLOR_YELLOW)
+
+					self._inf.add_msg("Okay, I will cast <green>{}</green> on you now.".format(msg))
 					self._me.Cast(GetSpellNr(msg), self._activator)
 				else:
 					self._inf.add_msg("You do not have enough money...")
-		else:
-			if msg == "food":
-				if self._activator.food < 500:
-					self._activator.food = 500
-					self._inf.add_msg("Your stomach is filled again.")
-				else:
-					self._inf.add_msg("You don't look very hungry to me...")
 
 		return True
 
