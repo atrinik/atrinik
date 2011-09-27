084fec3c7764828b711e6ed70f5ca95ebf931f17
Alex Tokar <admin@atokar.net>
2011-09-27 12:13:52 +0200

    Add Interface support to Tavern.Bartender API.
    Former-commit-id: 4944f6a7893d7b099443ebe04527ad64b38b0ce2

--- maps/python/Tavern.py
+++ maps/python/Tavern.py
@@ -5,95 +5,137 @@ from Atrinik import *
 
 ## Implements tavern bartenders.
 class Bartender:
-	## Maximum number of provisions.
-	max_provisions = 100
-
 	## Initialize the bartender class.
 	## @param activator Script activator.
-	## @param me Bartender.
+	## @param me Bartender NPC.
 	## @param event Event object.
-	def __init__(self, activator, me, event):
-		self.activator = activator
-		self.me = me
-		self.event = event
+	## @param inf The interface to use.
+	def __init__(self, activator, me, event, inf):
+		self._activator = activator
+		self._me = me
+		self._event = event
+		self._inf = inf
 
-	## Acquires the provisions menu.
-	## @return The provisions.
-	def _get_provisions(self):
-		for x in sorted(self.event.inv, key = lambda obj: obj.name):
-			yield "<a>" + x.name + "</a> for " + CostString(x.value)
-
-	## Greeting ('hi', 'hey', 'hello')
-	def _chat_greeting(self):
-		self.me.SayTo(self.activator, "\nWelcome to my tavern, dear customer! I am {}.\nI can offer you the following <a>provisions</a>:\n\n{}".format(self.me.name, "\n".join(self._get_provisions())))
-
-	## 'provisions'
-	def _chat_provisions(self, num_max, example):
-		self.me.SayTo(self.activator, "\nYou can either buy the provisions one by one, or prefix the provision name with a number (maximum {}), for example:\n<a>{} {}</a>".format(num_max, example[0], example[1]))
-
-	## Message when buying something.
-	def _chat_buy(self, num, obj):
-		self.me.SayTo(self.activator, "\nHere you go, {} {}, just as you ordered!\nPleasure doing business with you!".format(num, obj.name))
+		# Maximum number of provisions.
+		self.max_provisions = 100
 
 	## Check if the specified provision can be bought.
-	## @param name The provision name to check.
-	def _can_buy(self, name):
+	## @param obj The provision to check.
+	def _can_buy(self, obj):
 		return True
 
-	## Triggered when one cannot buy a provision.
-	## @param name Provision that was attempted to be bought.
-	def _chat_cannot_buy(self, name):
-		pass
+	## Creates the provision links.
+	def _create_provisions(self):
+		for obj in sorted(self._event.inv, key = lambda obj: obj.name):
+			if not self._can_buy(obj):
+				continue
+
+			name = obj.GetName()
+			self._inf.add_link(name.capitalize(), dest = "buy1 " + name)
+
+	## Creates the links when NPC asks the player how many of the
+	## specified provision to buy.
+	## @param obj The provision.
+	def _create_provision_numbers(self, obj):
+		from Language import int2english
+
+		name = obj.GetName()
+
+		for x in [1, 5, 10, 25, 50, 100, 500, 1000]:
+			if x > self.max_provisions:
+				break
+
+			self._inf.add_link(int2english(x).capitalize(), dest = "buy2 " + str(x) + " " + name)
+
+	## Main dialog.
+	def _chat_greeting(self):
+		self._inf.add_msg("Welcome to my tavern, dear customer! I am {}, the bartender.".format(self._me.name))
+		self._inf.add_msg("I can offer you the following provisions.")
+		self._create_provisions()
+
+	## Dialog when the player chooses a provision to buy.
+	## @param obj The provision the player has chosen.
+	def _chat_buy1(self, obj):
+		self._inf.add_msg("Ah, excellent choice!")
+		self._inf.add_msg_icon(obj.face[0], obj.GetName())
+		self._inf.add_msg("How many do you want to purchase?")
+		self._create_provision_numbers(obj)
+
+	## Message to display when the player has confirmed the number of
+	## provisions to buy, but cannot carry them all.
+	def _chat_buy2_weight(self):
+		self._inf.add_msg("You can't carry that much weight...")
+
+	## Message to display when the player has successfully purchased
+	## provision(s).
+	## @param obj The provision(s) the player will receive.
+	def _chat_buy2_success(self, obj):
+		self._inf.add_msg("Here you go!")
+		self._inf.add_msg_icon(obj.face[0], obj.GetName())
+		self._inf.add_msg("Pleasure doing business with you!")
+
+	## Message to display when the player has confirmed the number of
+	## provisions to buy, but doesn't have enough money.
+	def _chat_buy2_fail(self):
+		self._inf.add_msg("Sorry, you don't have enough money...")
 
 	## Handle a message.
 	## @param msg Message to handle.
+	## @return True if the message was handled, False otherwise.
 	def handle_msg(self, msg):
-		if msg == "hi" or msg == "hey" or msg == "hello":
+		if msg == "hello":
 			self._chat_greeting()
+			return True
+		elif msg.startswith("buy1 "):
+			name = msg[5:]
 
-		# Display information about provisions.
-		elif msg == "provisions":
-			self._chat_provisions(self.max_provisions, (min(self.max_provisions, 10), self.event.inv.name))
+			for obj in self._event.inv:
+				if obj.GetName() == name:
+					if not self._can_buy(obj):
+						break
 
-		else:
-			# Try to parse number of provisions to buy from the message.
-			try:
-				# Split the string and get the (possible) number.
-				num_str = msg.split(" ", 1)[0]
-				# Parse the number into integer, but add limit.
-				num = max(1, min(int(num_str), self.max_provisions))
-				# Adjust the message, removing the number.
-				msg = msg[len(num_str):].strip()
-			except:
-				num = 1
+					self._chat_buy1(obj)
+					break
 
-			# Search for the provision name.
-			for obj in self.event.inv:
-				# Matched?
-				if obj.name.lower() != msg:
-					continue
+			return True
+		elif msg.startswith("buy2 "):
+			import re
 
-				# Check if the provision can be bought.
-				if not self._can_buy(obj.name.lower()):
-					self._chat_cannot_buy(obj.name.lower())
-				# Make sure the player can actually carry the provisions.
-				elif not self.activator.Controller().CanCarry(obj.weight * num + obj.carrying):
-					self.me.SayTo(self.activator, "\nYou can't carry {} {}...".format(num, obj.name))
-				# Pay the amount.
-				elif self.activator.PayAmount(obj.value * num):
-					self.activator.Write("You pay {} to {}.".format(CostString(obj.value * num), self.me.name), COLOR_YELLOW)
-					self._chat_buy(num, obj)
+			# Attempt to get the number of the provisions and the provision name.
+			match = re.match(r"(\d+) (.*)", msg[5:])
 
-					# Clone the object.
-					clone = obj.Clone()
-					# Adjust the nrof.
-					clone.nrof = num
-					# Reset value to arch default so the provisions stack properly
-					# with regular food drops.
-					clone.value = clone.arch.clone.value
-					# Insert the clone into the player.
-					clone.InsertInto(self.activator)
-				else:
-					self.me.SayTo(self.activator, "\nSorry, you don't have enough money...")
+			if not match:
+				return True
 
-				break
+			# Acquire the number and the name.
+			(num, name) = match.groups()
+			# Make sure the number does not overflow.
+			num = max(1, min(int(num), self.max_provisions))
+
+			for obj in self._event.inv:
+				if obj.GetName() == name:
+					if not self._can_buy(obj):
+						break
+
+					# See if the player can carry the provision(s).
+					if not self._activator.Controller().CanCarry(obj.weight * num + obj.carrying):
+						self._chat_buy2_weight()
+					# Pay the amount...
+					elif self._activator.PayAmount(obj.value * num):
+						self._inf.add_msg("You pay {}.".format(CostString(obj.value * num)), COLOR_YELLOW)
+						clone = obj.Clone()
+						# Adjust the nrof.
+						clone.nrof = num
+						# Reset value to arch default so the provisions stack properly
+						# with regular food drops.
+						clone.value = clone.arch.clone.value
+						self._chat_buy2_success(clone)
+						# Insert the clone into the player.
+						clone.InsertInto(self._activator)
+					# Can't afford it!
+					else:
+						self._chat_buy2_fail()
+
+			return True
+
+		return False
