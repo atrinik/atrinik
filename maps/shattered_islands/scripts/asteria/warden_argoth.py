8b169b56d2a5165a1ce50f55293fe833434209e6
Alex Tokar <admin@atokar.net>
2011-09-28 14:35:47 +0200

    Use Interface in Warden Argoth's script.
    Former-commit-id: 093356c5b37c4db8ff9fe96c7b0e8532c2176365

--- maps/shattered_islands/scripts/asteria/warden_argoth.py
+++ maps/shattered_islands/scripts/asteria/warden_argoth.py
@@ -1,53 +1,59 @@
 ## @file
 ## Implements the angry drunk Warden who drops Asteria Dungeon Key 1.
 
-from Atrinik import *
+from Interface import Interface
 
-activator = WhoIsActivator()
-me = WhoAmI()
-msg = WhatIsMessage().strip().lower()
+inf = Interface(activator, me)
 
 def main():
 	if me.enemy:
 		if me.enemy == activator:
-			me.SayTo(activator, "\nNow you die!")
-		elif msg == "drunk" or msg == "drunkard":
-			me.SayTo(activator, "\nKeep it up and you're next, buddy!")
+			me.Say("Now you die!")
 		else:
-			me.SayTo(activator, "\nCan't you see I'm busy here?!")
+			inf.dialog_close()
 
-		return
+	elif msg == "hello":
+		inf.add_msg("{} yawns sleepily.".format(me.name), COLOR_YELLOW)
+		inf.add_msg("Hello {}, I am {}. So what brings you here?".format(activator.name, me.name))
+		inf.add_link("Just exploring around...", dest = "exploring")
 
-	if msg == "hello" or msg == "hi" or msg == "hey":
-		me.Communicate("/yawn")
-		me.SayTo(activator, "\nHello {0}, I am {1}.  So what <a>brings</a> you here?".format(activator.name, me.name))
-
-	elif msg == "brings":
-		me.SayTo(activator, "\nI don't suppose you've come about the <a>key</a>.  Sorry, I can't give that to you.")
+	elif msg == "exploring":
+		inf.add_msg("*HIC* I see... I don't suppose you've come about the key. Sorry, I can't give that to you...")
+		inf.add_link("What key?", dest = "key")
+		inf.add_link("Have you been drinking?", dest = "drinking")
 
 	elif msg == "key":
-		me.SayTo(activator, "\nI told you, I can't give that to you.")
+		inf.add_msg("I told you, can't give it to you. Don't even ask about it.")
+		inf.add_link("Have you been drinking?", dest = "drinking")
 
-	elif msg == "drunk" or msg == "drunkard":
-		try:
-			num = int(me.ReadKey("drunk_called"))
-		except:
-			num = 0
+	elif msg == "drinking":
+		inf.add_msg("... of course not, I'm on duty. What makes you think that?")
+		inf.add_link("You drunkard.", dest = "drunkard1")
 
-		if num == 0:
-			me.SayTo(activator, "\nWhat did you call me?")
-		elif num == 1:
-			me.SayTo(activator, "\nDon't call me that, I hate it!")
-		elif num == 8:
-			me.SayTo(activator, "\nThat's it! The next one who says I'm a drunk dies!")
-		elif num == 9:
-			me.SayTo(activator, "\nNow you've done it! You'll pay for that!")
-			me.enemy = activator
-			me.WriteKey("drunk_called")
-			return
-		else:
-			me.SayTo(activator, "\nI told you, I hate that!")
+	elif msg == "drunkard1":
+		inf.add_msg("What did you call me? I'm on duty here.")
+		inf.add_link("A drunk.", dest = "drunkard2")
 
-		me.WriteKey("drunk_called", str(num + 1))
+	elif msg == "drunkard2":
+		inf.add_msg("I'm <b>NOT</b> a drunk!")
+		inf.add_link("Yes you are.", dest = "drunkard3")
+
+	elif msg == "drunkard3":
+		inf.add_msg("NO! I'm on DUTY here! I haven't been *HIC* drinking!")
+		inf.add_link("You're a drunkard.", dest = "drunkard4")
+
+	elif msg == "drunkard4":
+		inf.add_msg("You can't prove that I'm a drunkard!")
+		inf.add_link("There's a beer barrel right next to you.", dest = "drunkard5")
+
+	elif msg == "drunkard5":
+		inf.add_msg("That's *HIC* it! Call me a drunkard again and you'll die!")
+		inf.add_link("You're such a drunkard...", dest = "drunkard6")
+
+	elif msg == "drunkard6":
+		inf.dialog_close()
+		me.Say("Now you've done it! Prepare to die!")
+		me.enemy = activator
 
 main()
+inf.finish()
