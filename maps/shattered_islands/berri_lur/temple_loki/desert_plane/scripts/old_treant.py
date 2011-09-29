1117cb4585bfc42c3ea99385877a4fa3a8a1ac74
Alex Tokar <admin@atokar.net>
2011-09-29 13:27:15 +0200

    Replace old treant's @match dialog with Interface script.
    Former-commit-id: 034904ea6cb8a581a0e2aac736ac27c0dccc477f

---
+++ maps/shattered_islands/berri_lur/temple_loki/desert_plane/scripts/old_treant.py
@@ -0,0 +1,17 @@
+## @file
+## Script for the Old Treant in the Desert Plane of Loki's Temple.
+
+from Interface import Interface
+
+inf = Interface(activator, me)
+
+def main():
+	if msg == "hello":
+		inf.add_msg("I appear to be stood on your way out. Oh dear. I guess you'll have to find some way to move me.")
+		inf.add_link("Er... how?", dest = "how")
+
+	elif msg == "how":
+		inf.add_msg("Have you tried pushing?")
+
+main()
+inf.finish()
