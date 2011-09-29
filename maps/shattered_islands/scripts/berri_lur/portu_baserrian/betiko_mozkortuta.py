7228f567dd30468ea4b47b902e03a001b13ad3f3
Alex Tokar <admin@atokar.net>
2011-09-29 14:51:32 +0200

    Replace Betiko Mozkortuta's @match dialog with Interface script.
    Former-commit-id: 34e6803a7922f36cfb171f049acbc389339fe819

---
+++ maps/shattered_islands/scripts/berri_lur/portu_baserrian/betiko_mozkortuta.py
@@ -0,0 +1,17 @@
+## @file
+## Script for Betiko Mozkortuta in Portu Baserrian tavern.
+
+from Interface import Interface
+
+inf = Interface(activator, me)
+
+def main():
+	if msg == "hello":
+		inf.add_msg("I'll haff ano'er beer peesh.")
+		inf.add_link("What a drunkard...", dest = "drunkard")
+
+	elif msg == "drunkard":
+		inf.add_msg("I'm noh dwunk! I jus' lika dwenk. Or two.")
+
+main()
+inf.finish()
