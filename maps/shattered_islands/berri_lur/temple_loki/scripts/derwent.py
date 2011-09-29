425abc9c29b3dac92b64a1862d11ed102609ee91
Alex Tokar <admin@atokar.net>
2011-09-29 13:21:52 +0200

    Replace Derwent's @match dialog with Interface script.
    Former-commit-id: 90e11795b4205617c09d58c825a145a96198c9f5

---
+++ maps/shattered_islands/berri_lur/temple_loki/scripts/derwent.py
@@ -0,0 +1,17 @@
+## @file
+## Script for Derwent the snake priest in Loki's Temple.
+
+from Interface import Interface
+
+inf = Interface(activator, me)
+
+def main():
+	if msg == "hello":
+		inf.add_msg("Welcome worshipper! Pray with caution, for Loki can be very unpredictable!")
+		inf.add_link("Who is Loki?", dest = "aboutloki")
+
+	elif msg == "aboutloki":
+		inf.add_msg("Loki is regarded as a god of chaos, due to his playful, unpredictable nature. He is by nature however, a shapeshifter.")
+
+main()
+inf.finish()
