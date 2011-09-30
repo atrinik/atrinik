d9bf3b3c365dd12acd65f05c4becd95814e4087a
Alex Tokar <admin@atokar.net>
2011-10-01 00:58:45 +0200

    Use Interface in Marble Toe's script.
    Former-commit-id: 881c637a326b44e2497819eff3c0485cc3727e50

--- maps/shattered_islands/scripts/strakewood_island/brynknot/marble_toe.py
+++ maps/shattered_islands/scripts/strakewood_island/brynknot/marble_toe.py
@@ -1,16 +1,21 @@
 ## @file
 ## Script for NPC called Marble Toe in Brynknot tavern.
 
-from Atrinik import *
-from random import choice
+from Interface import Interface
+import random
 
-activator = WhoIsActivator()
-me = WhoAmI()
+inf = Interface(activator, me)
 
-replies = [
-	"My apologies, your utterance does not make sense to me.",
-	"I wish you actually know what you are doing or what you are saying.",
-	"I will listen to you as long as it's logical, but I doubt I will ever need to.",
-]
+def main():
+	if msg == "hello":
+		replies = [
+			"My apologies, your utterance does not make sense to me.",
+			"I wish you actually know what you are doing or what you are saying.",
+			"I will listen to you as long as it's logical, but I doubt I will ever need to.",
+		]
 
-me.SayTo(activator, "\nHello there, I'm {0}, the president of the Society of Non-Aligned Brothers based in Thraal but at the moment I'm travelling to recruit more followers to my society.\n{1}".format(me.name, choice(replies)))
+		inf.add_msg("Hello there, I'm {}, the president of the Society of Non-Aligned Brothers based in Thraal but at the moment I'm travelling to recruit more followers to my society.".format(me.name))
+		inf.add_msg(random.choice(replies))
+
+main()
+inf.finish()
