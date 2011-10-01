92250f97dc26bff727b624c424700d5eedf670fa
emperorbma <emperorbma@suddenlink.net>
2011-10-01 00:19:50 -0500

    convert 2 of the students to use new format for script
    Former-commit-id: 8b50aa374da4e3b1ed7d15c244baff88ee896aa4

---
+++ maps/shattered_islands/scripts/centennial/smug_student_witch.py
@@ -0,0 +1,27 @@
+## @file
+## Implements the smug student witch's responses.
+
+from Interface import Interface
+
+msg = msg.lower()
+
+inf = Interface(activator, me)
+
+def main():
+	options = GetOptions()
+	if options == "classTime":
+		if msg == "hello":
+			inf.add_msg("Zzzzzzzz.....")
+		elif msg == "marisa" or msg == "kirisame" or msg == "marisa kirisame":
+			inf.add_msg("{} mumbles \"Love Sign: Master Spark\".".format(me.name), COLOR_YELLOW)
+	elif options == "clubTime":
+		if msg == "hello":
+			inf.add_msg("Heh.  This sucker keeps falling for the same trick.")
+			inf.add_link("Are you cheating?")
+		elif msg == "are you cheating?":
+			inf.add_msg("It ain't cheating, I'm just playing creatively.  Haha.~")
+		elif msg == "marisa" or msg == "kirisame" or msg == "marisa kirisame":
+			inf.add_msg("Marisa's awesome isn't she?")
+
+main()
+inf.finish()
