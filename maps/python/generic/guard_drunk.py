26ade500a2abd7d9679f18082b0e21e69d549972
Alex Tokar <admin@atokar.net>
2011-10-01 17:28:36 +0200

    Add new random responses for generic drunk guard script, by Lippy.
    Former-commit-id: fdf2f54f8606c0fef503a384acd33c633a506ce8

--- maps/python/generic/guard_drunk.py
+++ maps/python/generic/guard_drunk.py
@@ -15,6 +15,8 @@ def main():
 			"Bartender, 'nother beer here!",
 			"<yellow>The " + me.name + " breaks into a song...</yellow>\n\nI am a guard aaaand I am havin' a beer. Drinky drinky guard, drinky drinky guard.",
 			"*HIC*",
+			"I'm *hic!* watchin' you! I bets ye think I'm *hic* off duty but ye be wrong! I'm here guardin' my beer an' no-one iss gonna *hic!* take it from me!",
+			"It's not fair! *hic!* I gets paid low wages! But I *hic* are super guard! We don' needs so many *hic* guards aroun' here! I can handles this whole *hic* here place meself! Noth'n can *hic* beat me! Hahahaha*hic!*!",
 		]
 
 		inf.add_msg(random.choice(replies))
