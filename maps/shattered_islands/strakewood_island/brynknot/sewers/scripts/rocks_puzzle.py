3d2c09e4d93cfd258be13081f49bdd80e6af905a
Alex Tokar <admin@atokar.net>
2011-10-02 20:57:27 +0200

    Rewrite script, better connections system in Brynknot Sewers rocks puzzle.
    Former-commit-id: 5fb79bba2fa793bcdbdfd7cee97e37167846e9ad

--- maps/shattered_islands/strakewood_island/brynknot/sewers/scripts/rocks_puzzle.py
+++ maps/shattered_islands/strakewood_island/brynknot/sewers/scripts/rocks_puzzle.py
@@ -1,38 +1,26 @@
 ## @file
 ## Script for the rocks puzzle in second level of Brynknot Sewers.
 
-from Atrinik import *
-import os
+import os.path
 
-me = WhoAmI()
+def main():
+	# The rocks.
+	rocks = [
+		["sewers_a_aaab", 13, 13, "brynknot_sewers_a_rock_1"],
+		["sewers_a_01ab", 0, 4, "brynknot_sewers_a_rock_2"],
+		["sewers_a_02ab", 1, 19, "brynknot_sewers_a_rock_3"],
+		["sewers_a_02ab", 0, 9, "brynknot_sewers_a_rock_4"],
+	]
 
-# The rocks.
-rocks = [
-	["sewers_a_aaab", 13, 13, "brynknot_sewers_a_rock_1"],
-	["sewers_a_01ab", 0, 4, "brynknot_sewers_a_rock_2"],
-	["sewers_a_02ab", 1, 19, "brynknot_sewers_a_rock_3"],
-	["sewers_a_02ab", 0, 9, "brynknot_sewers_a_rock_4"],
-]
-
-if GetEventNumber() == EVENT_TIMER:
 	for rock in rocks:
 		ReadyMap(os.path.dirname(me.map.path) + "/" + rock[0]).Insert(LocateBeacon(rock[3]).env, rock[1], rock[2])
 
-	me.f_splitting = False
-else:
-	for rock in rocks:
-		ReadyMap(os.path.dirname(me.map.path) + "/" + rock[0])
+	key = me.FindObject(archname = "key2")
 
-	key = me.FindObject(0, "key2")
-
-	if not key:
-		raise Error("Could not find key inside myself.")
-
-	activator = WhoIsActivator()
-
-	if not activator.FindObject(2, "key2", key.name):
+	if not activator.FindObject(INVENTORY_CONTAINERS, key.arch.name, key.name):
 		key.Clone().InsertInto(activator)
+		activator.Write("The rocks have moved back to their resting place and you have received a key.", COLOR_NAVY)
+	else:
+		activator.Write("The rocks have moved back to their resting place.", COLOR_NAVY)
 
-	if not me.f_splitting:
-		me.CreateTimer(1, 2)
-		me.f_splitting = True
+main()
