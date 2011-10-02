0402e8e50fbb638b4971a2e34168b5165d9c960e
Alex Tokar <admin@atokar.net>
2011-10-02 20:51:33 +0200

    Update upgrade scripts.
    Former-commit-id: 3d02e4f991b813311c1cdf471ea628a4f62d561d

--- server/tools/upgrade/upgrades/7.py
+++ server/tools/upgrade/upgrades/7.py
@@ -44,6 +44,16 @@ def player_upgrade_func(player, arches):
 		"/shattered_islands/world_0404",
 		"/shattered_islands/world_0403",
 		"/shattered_islands/world_0402",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0202",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0203",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0204",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0302",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0303",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0304",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0402",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0403",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_a_0404",
+		"/shattered_islands/strakewood_island/brynknot/sewers/sewers_b_0404",
 	]
 
 	i = Upgrader.arch_get_attr_num(arches[0], "x", None)
@@ -73,6 +83,16 @@ def player_upgrade_func(player, arches):
 
 	if s in player:
 		coords = [(10, 15), (10, 16), (10, 18), (10, 19), (10, 20), (11, 14), (11, 15), (11, 16), (11, 17), (11, 18), (11, 19), (11, 20), (12, 15), (12, 16), (12, 18), (12, 19), (12, 20), (13, 19), (14, 14), (14, 15), (14, 16), (14, 17), (14, 18), (14, 19), (14, 20), (14, 21), (14, 22), (14, 23), (15, 14), (15, 15), (15, 16), (15, 17), (15, 18), (15, 19), (15, 20), (15, 21), (15, 22), (15, 23), (16, 14), (16, 15), (16, 16), (16, 18), (16, 20), (16, 22), (17, 14), (17, 15), (17, 16), (17, 17), (17, 18), (17, 19), (17, 20), (17, 21), (17, 22), (17, 23)]
+
+		for (x, y) in coords:
+			if x == player_x and y == player_y:
+				goto_emergency(player, arches, s)
+				break
+
+	s = "map /shattered_islands/strakewood_island/brynknot/sewers/sewers_0202\n"
+
+	if s in player:
+		coords = [(17, 9), (18, 9), (17, 10), (18, 10), (18, 11)]
 
 		for (x, y) in coords:
 			if x == player_x and y == player_y:
--- server/tools/upgrade/upgrades/8.py
+++ server/tools/upgrade/upgrades/8.py
@@ -3,7 +3,30 @@ def upgrade_func(arch):
 		i = Upgrader.arch_get_attr_num(arch, "name", None)
 
 		if i != -1:
-			if arch["attrs"][i][1] in ("Shipment of Charob Beer", "Ogres near Brynknot", "Evil Trees at Brynknot", "Forgotten Graveyard at Strakewood Island", "Giant Wasps at Strakewood Island", "Wyverns at Strakewood Island", "Ice Golems in Old Outpost", "Lava Golems in Old Outpost", "Manard's Prayerbook", "Cashin's Cap", "Tutorial Island Well", "Slimes at Tutorial Island", "Frah'ak and Kobolds", "Jahrlen's Heavy Rod", "Hill Giants Stronghold", "Dark Cave Elder Wyverns"):
+			if arch["attrs"][i][1] in ("Shipment of Charob Beer", "Ogres near Brynknot", "Evil Trees at Brynknot", "Forgotten Graveyard at Strakewood Island", "Giant Wasps at Strakewood Island", "Wyverns at Strakewood Island", "Ice Golems in Old Outpost", "Lava Golems in Old Outpost", "Manard's Prayerbook", "Cashin's Cap", "Tutorial Island Well", "Slimes at Tutorial Island", "Frah'ak and Kobolds", "Jahrlen's Heavy Rod", "Hill Giants Stronghold", "Dark Cave Elder Wyverns", "Enemies beneath Brynknot", "Investigation for Maplevale"):
+				arch["archname"] = None
+
+		i = Upgrader.arch_get_attr_num(arch, "underground_city_lake_portal", None)
+
+		if i != -1:
+			arch["attrs"].pop(i)
+	elif arch["archname"] == "barrel2.101":
+		i = Upgrader.arch_get_attr_num(arch, "name", None)
+
+		if i != -1:
+			if arch["attrs"][i][1] == "shipment of Charob Beer":
+				arch["archname"] = None
+	elif arch["archname"] == "key2":
+		i = Upgrader.arch_get_attr_num(arch, "name", None)
+
+		if i != -1:
+			if arch["attrs"][i][1] in ("Maplevale's amulet", "Brynknot Maze Key"):
+				arch["archname"] = None
+	elif arch["archname"] == "note":
+		i = Upgrader.arch_get_attr_num(arch, "name", None)
+
+		if i != -1:
+			if arch["attrs"][i][1] == "Letter from Nyhelobo to oty captain":
 				arch["archname"] = None
 
 	return arch
