826b745963d49a123c657210f62bf89c3f33f18d
emperorbma <emperorbma@suddenlink.net>
2011-10-01 19:48:03 -0500

    Agnes Shipping Company :P
    Former-commit-id: 3bb21d956b933c693eae73ff11b870ae9f5d6f70

---
+++ maps/shattered_islands/scripts/strakewood_island/asteria/serge_aster.py
@@ -0,0 +1,19 @@
+## @file
+## Script for Serge Aster, the thief.
+
+from Interface import Interface
+
+inf = Interface(activator, me)
+
+def main():
+	if msg == "hello":
+		inf.add_msg("Oi.  What are you doing down here?")
+		inf.add_link("I have some questions...", dest = "questions")
+		inf.add_link("Goodbye.", action = "close")
+
+	elif msg == "questions":
+		inf.add_msg("Some questions are dangerous, friend.  Unless you have some business here, you should probably go.")
+		inf.add_link("Goodbye.", action = "close")
+
+main()
+inf.finish()
