c20930aec465c09ba67863819f3713f85707d6e0
Alex Tokar <admin@atokar.net>
2011-10-03 22:25:38 +0200

    Remove redundant scripts.
    Former-commit-id: 467359538844666f92612f452f6a8eb751d6f1c1

--- maps/shattered_islands/scripts/berri_lur/portu_baserrian/cook.py
+++
@@ -1,9 +0,0 @@
-## @file
-## Script for cook in Portu Baserrian tavern.
-
-from Atrinik import *
-
-msg = WhatIsMessage().strip().lower()
-
-if WhoIsActivator().name == "Birjina Neskamea" and msg == "that food cooked yet, bero?":
-	WhoAmI().Communicate("Yep. Here you are, ready to go.")
--- maps/shattered_islands/scripts/berri_lur/portu_baserrian/foodready.py
+++
@@ -1,6 +0,0 @@
-## @file
-## Script for barmaid in Portu Baserrian tavern.
-
-from Atrinik import *
-
-WhoIsActivator().Communicate("That food cooked yet, Bero?")
