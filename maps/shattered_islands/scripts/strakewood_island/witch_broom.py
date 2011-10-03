c2fbcb2b9f6eeb5a974c30552c9fa7c1c4d1f4b4
Alex Tokar <admin@atokar.net>
2011-10-03 02:28:37 +0200

    Use Interface in swamp witch's script.
    Former-commit-id: 7bac6815c8affe65b9c8a6c2076baa8ec6fd1172

--- maps/shattered_islands/scripts/strakewood_island/witch_broom.py
+++ maps/shattered_islands/scripts/strakewood_island/witch_broom.py
@@ -1,25 +1,32 @@
 ## @file
 ## Dialog for the witch in swamp near Asteria.
 
-from Atrinik import *
+from Interface import Interface
 
-me = WhoAmI()
-activator = WhoIsActivator()
-msg = WhatIsMessage().strip().lower()
+inf = Interface(activator, me)
 
 def main():
 	# Don't say anything if we have an enemy; she's focusing on the enemy.
 	if me.enemy:
+		inf.dialog_close()
 		return
 
-	if msg == "hi" or msg == "hey" or msg == "hello":
-		me.SayTo(activator, "\nWelcome, welcome... Heh-heh-heh...\n\n<a>Why do you have 2 brooms?</a>")
+	if msg == "hello":
+		inf.add_msg("Welcome, welcome... Heh-heh-heh...")
+		inf.add_link("Why do you have two brooms?", dest = "havetwo")
 
-	elif msg == "why do you have 2 brooms?":
-		me.SayTo(activator, "\nThat is none of your concern! Now go away before I get really mad!\n\n<a>Did you steal one?</a>")
+	elif msg == "havetwo":
+		inf.add_msg("That is none of your concern! Now go away before I get really mad!")
+		inf.add_link("Did you steal one?", dest = "stealone")
 
-	elif msg == "did you steal one?":
-		me.SayTo(activator, "\nThat's it! Now you die!")
+	elif msg == "stealone":
+		inf.add_msg("Keep it up, and you'll get more than you bargained for!")
+		inf.add_link("You stole it, didn't you.", dest = "stoleit")
+
+	elif msg == "stoleit":
+		me.Say("Now you've done it!")
+		inf.dialog_close()
+
 		# The witch gets angry...
 		me.enemy = activator
 		# Find witch's cat spawn point.
@@ -31,3 +38,4 @@ def main():
 			obj.enemy.enemy = activator
 
 main()
+inf.finish()
