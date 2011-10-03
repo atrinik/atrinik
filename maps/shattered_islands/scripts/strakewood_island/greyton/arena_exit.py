e97af2e105bea79cda24caf20fd5d516c0166cb5
Alex Tokar <admin@atokar.net>
2011-10-03 21:18:09 +0200

    Update script.
    Former-commit-id: 3526efc23562d7790f5457e12ceb66b66610a312

--- maps/shattered_islands/scripts/strakewood_island/greyton/arena_exit.py
+++ maps/shattered_islands/scripts/strakewood_island/greyton/arena_exit.py
@@ -2,8 +2,7 @@
 ## Script to teleport a player that has died to map with "local
 ## medics" randomly to one of the beds.
 
-from Atrinik import *
 from random import choice
 
 # Randomly choose the X/Y.
-WhoIsActivator().TeleportTo("/shattered_islands/world_1113", choice((7, 10, 13, 16)), choice((7, 8)), 0)
+activator.TeleportTo("/shattered_islands/world_1113", choice((7, 10, 13, 16)), choice((7, 8)), 0)
