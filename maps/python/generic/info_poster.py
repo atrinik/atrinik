74bef4ff142719a047e98e4cefca18d0d19d66b1
Alex Tokar <admin@atokar.net>
2011-10-05 22:30:51 +0200

    Update script.
    Former-commit-id: c88ca6badc032d348d346b47247846049f2fbb50

--- maps/python/generic/info_poster.py
+++ maps/python/generic/info_poster.py
@@ -1,13 +1,26 @@
 ## @file
 ## Script implementing Information Posters.
 
-from Atrinik import *
-from PostOffice import *
-from Guild import *
+from PostOffice import PostOffice
+from Guild import Guild
 
-activator = WhoIsActivator()
+post = PostOffice(activator.name)
+guild = Guild(None)
 
 def main():
+	# Messages of interest.
+	messages = []
+
+	# Bank balance.
+	balance = activator.Controller().BankBalance()
+
+	if balance:
+		messages.append("You have {} stored in bank.".format(CostString(balance)))
+
+	# Any items in post office?
+	if post.get_items():
+		messages.append("There are items waiting for you in the post office.")
+
 	# Check which guild the player is member of.
 	g = guild.pl_get_guild(activator.name)
 
@@ -15,37 +28,22 @@ def main():
 	if g and g[2]:
 		guild.set(g[0])
 
-	# Messages of interest.
-	messages = []
-	# Bank balance.
-	pinfo = activator.GetPlayerInfo("BANK_GENERAL")
+		# Are we in a guild and are we an admin of the guild?
+		if guild.member_is_admin(activator.name):
+			count = 0
 
-	if pinfo and pinfo.value:
-		messages.append("You have {} stored in bank.".format(CostString(pinfo.value)))
+			# Count members awaiting approval.
+			for member in guild.get_members():
+				if not guild.member_approved(member):
+					count += 1
 
-	# Any items in post office?
-	if post.get_items():
-		messages.append("There are items waiting for you in the post office.")
-
-	# Are we in a guild and are we an admin of the guild?
-	if g and g[2] and guild.member_is_admin(activator.name):
-		count = 0
-
-		# Count members awaiting approval.
-		for member in guild.get_members():
-			if not guild.member_approved(member):
-				count += 1
-
-		if count:
-			messages.append("There are {} member(s) awaiting approval in your guild.".format(count))
+			if count:
+				messages.append("There are {} member(s) awaiting approval in your guild.".format(count))
 
 	if messages:
 		activator.Write("\n".join(messages), COLOR_WHITE)
 	else:
 		activator.Write("There is currently nothing of interest.", COLOR_WHITE)
-
-post = PostOffice(activator.name)
-guild = Guild(None)
 
 try:
 	main()
