afe73cd9a292766cecd4cb0e8b15a86835a9a153
Alex Tokar <admin@atokar.net>
2011-10-05 22:31:53 +0200

    Update script.
    Former-commit-id: 6d27313ad2eb736b364a7829940e6f0d0c0e4dc7

--- maps/python/generic/jail_exit.py
+++ maps/python/generic/jail_exit.py
@@ -1,8 +1,11 @@
 ## @file
 ## Jail manager portal, used for the /arrest command.
 
-from Atrinik import *
 from Jail import Jail
 
-# Jail the player for 5 minutes.
-Jail(WhoAmI()).jail(WhoIsActivator(), 300, False)
+jail = Jail(me)
+
+def main():
+	jail.jail(activator, 300, False)
+
+main()
