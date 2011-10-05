6d31403692aa17bc4089cecedc783e4f26bbb79d
Alex Tokar <admin@atokar.net>
2011-10-06 01:42:22 +0200

    Update script.
    Former-commit-id: 3c7c259295ae845ec5fd318c77afbd98720b9343

--- maps/shattered_islands/everlink/scripts/destroyer.py
+++ maps/shattered_islands/everlink/scripts/destroyer.py
@@ -1,14 +1,9 @@
 ## @file
 ## Script for item destroyers in public house areas.
 
-from Atrinik import *
 import random
 
-activator = WhoIsActivator()
-
-# Whatever is triggering the destroyer must have an owner and the owner's
-# name must be 'magic wall'.
-if activator.owner and activator.owner.name == "magic wall":
+def main():
 	# Go through objects on activator's square.
 	for ob in activator.map.GetFirstObject(activator.x, activator.y):
 		if ob.f_sys_object or ob.f_no_pick or not ob.weight or ob.type == Type.PLAYER:
@@ -16,7 +11,12 @@ if activator.owner and activator.owner.name == "magic wall":
 
 		# Randomly calculate when to destroy an item.
 		if random.random() <= 0.10:
-			activator.map.Message(activator.x, activator.y, 5, "The {0} destroys the {1}!".format(activator.name, ob.GetName()), COLOR_RED)
+			activator.map.Message(activator.x, activator.y, 5, "The {} destroys the {}!".format(activator.name, ob.GetName()), COLOR_RED)
 			ob.Remove()
+
+# Whatever is triggering the destroyer must have an owner and the owner's
+# name must be 'magic wall'.
+if activator.owner and activator.owner.name == "magic wall":
+	main()
 
 SetReturnValue(1)
