2fd958e5dcfc3567fdd3d6e6abcc74970671f669
Alex Tokar <admin@atokar.net>
2011-10-09 22:27:24 +0200

    Update script.
    Former-commit-id: a1a66d12b6d85afceddbd6687cbd5afd00999b67

--- maps/shattered_islands/scripts/strakewood_island/abandoned_mine_portal.py
+++ maps/shattered_islands/scripts/strakewood_island/abandoned_mine_portal.py
@@ -4,12 +4,13 @@
 ## Makes the portal shrink, then disappear.
 
 # Shrink it.
-me.zoom -= 5
+me.zoom_x -= 5
+me.zoom_y -= 5
 # Adjust X and Y align.
 me.align += 1
 me.z += 1
 
 # Shrinked to a very small size? Then set the is_used_up flag so it'll
 # get removed the next tick.
-if me.zoom == 30:
+if me.zoom_x == 30:
 	me.f_is_used_up = True
