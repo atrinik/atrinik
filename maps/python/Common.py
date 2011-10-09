8c38557d3e81135e2ab41a4a2193a7de025a8e70
Alex Tokar <admin@atokar.net>
2011-10-09 02:05:25 +0200

    Add Common.find_obj method.
    Former-commit-id: 30e6b878db6479e815c67025fb71bb4a72df5868

--- maps/python/Common.py
+++ maps/python/Common.py
@@ -25,3 +25,32 @@ def absdir(d):
 def get_randomized_dir(d):
 	from random import randint
 	return absdir(d + randint(0, 2) + randint(0, 2) - 2)
+
+## Search squares around the activator, looking for an object identified
+## by its name/count.
+## @param activator The activator. Will search below and around this object.
+## @param limit Maximum number of squares from the activator to search.
+## @param archname Optional archname to look for.
+## @param name Optional name to look for.
+## @param count Optional object ID to look for.
+def find_obj(activator, limit = 10, archname = None, name = None, count = None):
+	if archname == None and name == None and count == None:
+		raise AttributeError("No matching conditions provided.")
+
+	for (m, x, y) in [(activator.map, activator.x, activator.y)] + activator.SquaresAround(limit):
+		first = m.GetFirstObject(x, y)
+
+		if not first:
+			continue
+
+		for tmp in first:
+			if archname != None and tmp.arch.name != archname:
+				continue
+
+			if name != None and tmp.name != name:
+				continue
+
+			if count != None and tmp.count != count:
+				continue
+
+			return tmp
