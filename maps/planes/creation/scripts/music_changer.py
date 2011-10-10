c1b5c9d6220f15d47abf48818df4884b1ced3eeb
Alex Tokar <admin@atokar.net>
2011-10-10 03:12:40 +0200

    Use Interface in music changer script.
    Former-commit-id: 962630015050447d079bc2901c8b33317532b7c9

--- maps/planes/creation/scripts/music_changer.py
+++ maps/planes/creation/scripts/music_changer.py
@@ -1,30 +1,27 @@
 ## @file
 ## Helpful NPC to change background music of a map.
 
-from Atrinik import *
+from Interface import Interface
+from Packet import MapStats
 import re
 
-activator = WhoIsActivator()
-me = WhoAmI()
-msg = WhatIsMessage().strip().lower()
+inf = Interface(activator, me)
 
 def main():
-	if msg == "hi" or msg == "hey" or msg == "hello":
-		me.SayTo(activator, "\nWelcome. I am the music changer -- just tell me which background music file to play and I will change this map to play it. You can find a list of media files <a=url:http://www.atrinik.org/page/client_media_files>here</a>. Say <a>none</a> to stop any music on the map.")
-	elif msg == "none" or re.match("^([a-zA-Z0-9_\-]+)\.(\w+)[ 0-9\-]?$", msg):
-		beacon = LocateBeacon("creation_music_changer")
-
-		if not beacon or not beacon.env:
-			return
-
-		info = beacon.env
-		info.slaying = "no_music" if msg == "none" else WhatIsMessage().strip()
+	if msg == "hello":
+		inf.add_msg("Welcome. I am the music changer -- just tell me which background music file to play and I will change this map to play it. You can find a list of media files <a=url:http://www.atrinik.org/page/client_media_files>here</a>. Use <green>none</green> to stop any music on the map.")
+		inf.set_text_input()
+	else:
+		inf.dialog_close()
+		info = LocateBeacon("creation_music_changer").env
+		info.slaying = "no_music" if msg == "none" else msg.strip()
 		# Construct new map name from old one.
 		info.race = re.sub(r"\([a-zA-Z0-9_\-\.]+\)", "({})".format(info.slaying), info.race)
 		map_name = "<b><o=0,0,0>{}</o></b>".format(info.race)
 
 		for player in me.map.GetPlayers():
 			if player.x >= info.x and player.x <= info.x + info.hp and player.y >= info.y and player.y <= info.y + info.sp:
-				player.Controller().SendPacket(17, "BsBs", 1, map_name, 2, info.slaying)
+				MapStats(player.Controller(), name = map_name, music = info.slaying)
 
 main()
+inf.finish()
