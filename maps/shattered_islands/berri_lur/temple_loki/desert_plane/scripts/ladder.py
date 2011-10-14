e48b2a79c6e324b803f2cd35d4503cef4b6ae02b
Alex Tokar <admin@atokar.net>
2011-10-14 16:29:45 +0200

    Update script.
    Former-commit-id: 89003caf2617a8aba232f32670543b9d53eb2302

--- maps/shattered_islands/berri_lur/temple_loki/desert_plane/scripts/ladder.py
+++ maps/shattered_islands/berri_lur/temple_loki/desert_plane/scripts/ladder.py
@@ -3,25 +3,22 @@
 ## jump away from the activator, so they cannot climb it down. The trick
 ## is to get it next to a wall, where it can't escape.
 
-from Atrinik import *
 from Common import get_randomized_dir
-import os
+import os.path
 
-me = WhoAmI()
-activator = WhoIsActivator()
+def main():
+	direction = get_randomized_dir(activator.direction)
+	# Calculate new X/Y
+	x = me.x + freearr_x[direction]
+	y = me.y + freearr_y[direction]
 
-direction = get_randomized_dir(activator.direction)
-# Calculate new X/Y
-x = me.x + freearr_x[direction]
-y = me.y + freearr_y[direction]
-
-# If there's no wall, continue going away from the activator.
-if not me.map.Wall(x, y):
-	(m, x, y) = me.map.GetMapFromCoord(x, y)
-	m.Insert(me, x, y)
-	SetReturnValue(1)
-# Can't go further; put it back where it came from, and allow activator
-# to climb it down.
-else:
-	m = ReadyMap(os.path.dirname(me.map.path) + "/desert_plane_0101")
-	m.Insert(me, 23, 0)
+	# If there's no wall, continue going away from the activator.
+	if not me.map.Wall(x, y):
+		(m, x, y) = me.map.GetMapFromCoord(x, y)
+		m.Insert(me, x, y)
+		SetReturnValue(1)
+	# Can't go further; put it back where it came from, and allow activator
+	# to climb it down.
+	else:
+		m = ReadyMap(os.path.dirname(me.map.path) + "/desert_plane_0101")
+		m.Insert(me, 23, 0)
