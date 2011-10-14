2c604fb675b4eb88e824eac6b2dd0761371e320e
Alex Tokar <admin@atokar.net>
2011-10-14 12:30:56 +0200

    Update upgrade script.
    Former-commit-id: f41aa1d8df9b24bae89f1c65de0770b70270b99f

--- server/tools/upgrade/upgrades/8.py
+++ server/tools/upgrade/upgrades/8.py
@@ -3,7 +3,7 @@ def upgrade_func(arch):
 		i = Upgrader.arch_get_attr_num(arch, "name", None)
 
 		if i != -1:
-			if arch["attrs"][i][1] in ("Shipment of Charob Beer", "Ogres near Brynknot", "Evil Trees at Brynknot", "Forgotten Graveyard at Strakewood Island", "Giant Wasps at Strakewood Island", "Wyverns at Strakewood Island", "Ice Golems in Old Outpost", "Lava Golems in Old Outpost", "Manard's Prayerbook", "Cashin's Cap", "Tutorial Island Well", "Slimes at Tutorial Island", "Frah'ak and Kobolds", "Jahrlen's Heavy Rod", "Hill Giants Stronghold", "Dark Cave Elder Wyverns", "Enemies beneath Brynknot", "Investigation for Maplevale", "Aris Undead Infestation", "Ants under Fort Ghzal", "Battle of Fort Ghzal", "Knowledge of Meteor Swarm", "Knowledge of Meteor", "Vielumin Ruins Altar", "Rirain's Nightmares", "Hungry Wolves", "Quidton's Mana Crystal", "Improved Asteroid"):
+			if arch["attrs"][i][1] in ("Shipment of Charob Beer", "Ogres near Brynknot", "Evil Trees at Brynknot", "Forgotten Graveyard at Strakewood Island", "Giant Wasps at Strakewood Island", "Wyverns at Strakewood Island", "Ice Golems in Old Outpost", "Lava Golems in Old Outpost", "Manard's Prayerbook", "Cashin's Cap", "Tutorial Island Well", "Slimes at Tutorial Island", "Frah'ak and Kobolds", "Jahrlen's Heavy Rod", "Hill Giants Stronghold", "Dark Cave Elder Wyverns", "Enemies beneath Brynknot", "Investigation for Maplevale", "Aris Undead Infestation", "Ants under Fort Ghzal", "Battle of Fort Ghzal", "Knowledge of Meteor Swarm", "Knowledge of Meteor", "Vielumin Ruins Altar", "Rirain's Nightmares", "Hungry Wolves", "Quidton's Mana Crystal", "Improved Asteroid", "Gandyld's Quest", "Gandyld's Quest II"):
 				arch["archname"] = None
 
 		i = Upgrader.arch_get_attr_num(arch, "underground_city_lake_portal", None)
@@ -61,6 +61,12 @@ def upgrade_func(arch):
 			if arch["attrs"][i][1] == "Silmedsen's branches":
 				arch["archname"] = "silmedsen_branches"
 				arch["attrs"] = []
+	elif arch["archname"] == "power_crystal":
+		i = Upgrader.arch_get_attr_num(arch, "name", None)
+
+		if i != -1:
+			if arch["attrs"][i][1] == "Gandyld's Mana Crystal":
+				arch["archname"] = None
 
 	return arch
 
