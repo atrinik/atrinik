a9ace450b48f6b9f2f0bdb3ef2a3184ccde025f3
Alex Tokar <admin@atokar.net>
2011-10-19 15:25:17 +0200

    Remove Nyhelobo's spawn point after he dies.
    Former-commit-id: a852765c44a498a08192cde076f438c4611fcd82

--- maps/shattered_islands/strakewood_island/brynknot/sewers/scripts/beholder_dead.py
+++ maps/shattered_islands/strakewood_island/brynknot/sewers/scripts/beholder_dead.py
@@ -1,4 +1,5 @@
 ## @file
 ## Script to execute when Nyhelobo the beholder dies.
 
+me.FindObject(type = Type.SPAWN_POINT_INFO).owner.Remove()
 me.map.Message(me.x, me.y, MAP_INFO_ALL, "As soon as {} dies, a powerful shock wave hits the whole room, and the beholder's mind controlling equipment is destroyed.".format(me.name), COLOR_GREEN)
