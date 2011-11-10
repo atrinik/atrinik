01a2d8b4a053796a96a54ceec38bf0181ff75f41
Alex Tokar <admin@atokar.net>
2011-11-10 06:19:20 +0100

    Remove the need for magic ear at Abandoned Mine main entrance.
    Former-commit-id: b31cfe78f4fd519fd6f10bf08f72fc37748a36f1

---
+++ maps/shattered_islands/scripts/strakewood_island/abandoned_mine_gate.py
@@ -0,0 +1,30 @@
+## @file
+## Script for the main entrance of the Abandoned Mine; gate that requires
+## a passphrase.
+
+from Interface import Interface
+
+inf = Interface(activator, me)
+
+def main():
+	lever = LocateBeacon("abandoned_mine_gate_lever").env
+
+	# The lever has been activated so the gate is (going) down.
+	if lever.speed:
+		SetReturnValue(1)
+		return
+
+	inf.set_icon("gate_door1.111")
+	inf.set_title("Gate")
+
+	if msg and msg.lower() == "mellon":
+		inf.dialog_close()
+		lever.Apply(lever)
+	else:
+		inf.add_msg("You notice a strange writing on the gate...", COLOR_YELLOW)
+		inf.add_msg("<font=pecita 20><o=#0083ff><center>Pedo mellon a mino!</center></o></font>")
+		inf.add_msg("It seems some kind of passphrase is needed to activate it.", COLOR_YELLOW)
+		inf.set_text_input(prepend = "/sys_tell ")
+
+main()
+inf.finish()
