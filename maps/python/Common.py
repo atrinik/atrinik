791c3a64d4a7a0321cfcc4aca7c79eddaa6c7734
Alex Tokar <admin@atokar.net>
2011-11-20 22:14:22 +0100

    Implement Common.objs_get_diff debugging method.
    Former-commit-id: 512ad4582444bc1076635e6ce5081b4b2cd849c4

--- maps/python/Common.py
+++ maps/python/Common.py
@@ -62,3 +62,27 @@ def find_obj(activator, limit = 10, archname = None, name = None, count = None):
 					continue
 
 				return tmp
+
+## Figure out the attribute differences between the specified objects.
+## @return List of attribute names that are different.
+def objs_get_diff(*args):
+	results = []
+	l = []
+
+	for arg in args:
+		l += dir(arg)
+
+	for attr in set(l):
+		if attr.startswith("_") or not attr.islower():
+			continue
+
+		# Ignore some attributes.
+		if attr == "count" or attr == "below" or attr == "above":
+			continue
+
+		for arg in args:
+			for arg2 in args:
+				if arg != arg2 and getattr(arg, attr) != getattr(arg2, attr):
+					results.append(attr)
+
+	return list(set(results))
