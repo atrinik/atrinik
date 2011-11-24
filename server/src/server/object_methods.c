659e79138def7d109fe1b5a0cf82ce4813cb040f
Alex Tokar <admin@atokar.net>
2011-11-24 20:43:47 +0100

    Document.
    Former-commit-id: 10f3f85649bc25d1770ae7c0a23b4967834315c6

--- server/src/server/object_methods.c
+++ server/src/server/object_methods.c
@@ -33,10 +33,17 @@
  * Registered method handlers. */
 object_methods object_type_methods[OBJECT_TYPE_MAX];
 
+/**
+ * The base object type methods; all object types inherit from this. */
 object_methods object_methods_base;
 
+/**
+ * Recursion counter for object_move_on(). */
 static int object_move_on_recursion_depth = 0;
 
+/**
+ * Initialize one object methods structure.
+ * @param methods What to initialize. */
 static void object_methods_init_one(object_methods *methods)
 {
 	memset(methods, 0, sizeof(*methods));
