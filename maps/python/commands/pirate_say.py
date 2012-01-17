f8d853fbe83b2b130ddaa31787f04681c4ffa529
Alex Tokar <admin@atokar.net>
2012-01-17 05:41:22 +0100

    Simplify /pirate_say and /pirate_shout.
    Former-commit-id: 01cbfd334faa48e7aed7a016540bd3c1c5be9261

--- maps/python/commands/pirate_say.py
+++ maps/python/commands/pirate_say.py
@@ -2,13 +2,8 @@
 ## The /pirate_say command.
 
 from Pirate import english2pirate
-from Common import player_sanitize_input
 
-message = player_sanitize_input(msg)
-
-if not message:
+if not msg:
 	activator.Write("No message given.", COLOR_RED)
 else:
-	new_message = english2pirate(message)
-	Logger("CHAT", "Pirate say: {} >{}< >{}<".format(activator.name, message, new_message))
-	activator.Communicate(new_message)
+	activator.Controller().ExecuteCommand("/say " + english2pirate(msg))
--- maps/python/commands/pirate_shout.py
+++ maps/python/commands/pirate_shout.py
@@ -2,13 +2,8 @@
 ## The /pirate_shout command.
 
 from Pirate import english2pirate
-from Common import player_sanitize_input
 
-message = player_sanitize_input(msg)
-
-if not message:
+if not msg:
 	activator.Write("No message given.", COLOR_RED)
 else:
-	new_message = english2pirate(message)
-	Logger("CHAT", "Pirate shout: {} >{}< >{}<".format(activator.name, message, new_message))
-	activator.Write("{} shouts: {}".format(activator.name, new_message), COLOR_ORANGE, NDI_ALL | NDI_PLAYER | NDI_SHOUT)
+	activator.Controller().ExecuteCommand("/shout " + english2pirate(msg))
