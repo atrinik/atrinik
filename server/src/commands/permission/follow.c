17ecfb957e7f492f4fa40524044b38841ce35892
Alex Tokar <admin@atokar.net>
2012-01-20 19:07:06 +0100

    Implement /follow.
    Former-commit-id: 23a3006ed748073a948b4f74ace6e4760657bcf3

--- server/src/commands/permission/follow.c
+++ server/src/commands/permission/follow.c
@@ -34,4 +34,29 @@
 /** @copydoc command_func */
 void command_follow(object *op, const char *command, char *params)
 {
+	player *pl;
+
+	if (!params)
+	{
+		if (*CONTR(op)->followed_player != '\0')
+		{
+			draw_info_format(COLOR_WHITE, op, "You stop following %s.", CONTR(op)->followed_player);
+			CONTR(op)->followed_player[0] = '\0';
+		}
+
+		return;
+	}
+
+	pl = find_player(params);
+
+	if (!pl)
+	{
+		draw_info(COLOR_WHITE, op, "No such player.");
+		return;
+	}
+
+	strncpy(CONTR(op)->followed_player, params, sizeof(CONTR(op)->followed_player) - 1);
+	CONTR(op)->followed_player[sizeof(CONTR(op)->followed_player) - 1] = '\0';
+
+	draw_info_format(COLOR_GREEN, op, "Following %s.", params);
 }
