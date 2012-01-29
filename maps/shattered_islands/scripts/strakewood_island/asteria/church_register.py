f93cb315399477e662b838a90c9b628303a0fa98
emperorbma <emperorbma@suddenlink.net>
2012-01-29 13:51:32 -0600

    remove [deity specific] stuff.
    Former-commit-id: 7e3ef7155b9d5683674ca2b731d3de50bee41cdd

--- maps/shattered_islands/scripts/strakewood_island/asteria/church_register.py
+++ maps/shattered_islands/scripts/strakewood_island/asteria/church_register.py
@@ -1,7 +1,6 @@
 ## @file
 ## Script for Cathedral Registration attendants in the Catherdral of Holy Wisdom.
 
-from Atrinik import Gender
 from Interface import Interface
 
 inf = Interface(activator, me)
@@ -13,18 +12,7 @@ def main():
 		inf.add_link("Never mind.  Bye.", action = "close")
 
 	elif msg == "entry":
-		if activator.GetGod() == "Tabernacle":
-			if activator.GetGender() == Gender.MALE:
-				T = "brother"
-			elif activator.GetGender() == Gender.FEMALE:
-				T = "sister"
-			else:
-				T = "err... friend"
-			inf.add_msg("Go right on in {}, and may the blessings of the Tabernacle be with you.".format(T))
-		elif activator.GetGod() == "Rashindel":
-			inf.add_msg("What do you want here, devil-worshipper?  You may go in, but only so that the priests may soon drive that demonic drivel out of your head.")
-		else:
-			inf.add_msg("I see you are not of our faith.  Enter in peace and behold the great and powerful majesty of the Tabernacle.")
+		inf.add_msg("Enter in peace and behold the great and powerful majesty of the Tabernacle.")
 
 main()
 inf.finish()
