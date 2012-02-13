9d59e6e5a15a6117a8f60e7dee56df145b76ef90
Alex Tokar <admin@atokar.net>
2012-02-13 17:40:27 +0100

    Create objects one-by-one when using /create <num> <arch> instead of setting nrof directly.
    Former-commit-id: 7ea8cb7e3f6a2853bf59e0632acbe6e78df67ed6

--- maps/python/commands/create.py
+++ maps/python/commands/create.py
@@ -16,48 +16,58 @@ def main():
 
 	(num, archname, artname, attribs) = match.groups()
 
-	obj = CreateObject(archname)
-	obj.f_identified = True
-
-	if not obj:
-		return
-
-	if num:
-		obj.nrof = int(num)
-
-	if artname:
-		obj.Artificate(artname)
-
-	if attribs:
-		for (attrib, val) in re.findall(r'(\w+) ("[^"]+"|[^ ]+)', attribs):
-			if val.startswith('"') and val.endswith('"'):
-				val = val[1:-1]
-
-			try:
-				val = int(val)
-			except ValueError:
-				try:
-					val = float(val)
-				except ValueError:
-					pass
-
-			if val == "None":
-				val = None
-
-			if hasattr(obj, attrib):
-				setattr(obj, attrib, val)
-			elif hasattr(obj, "f_" + attrib):
-				setattr(obj, "f_" + attrib, True if val else False)
-			else:
-				obj.WriteKey(attrib, str(val))
-
-	if obj.f_monster:
-		activator.map.Insert(obj, activator.x, activator.y)
-		obj.Fix()
-
-		if obj.randomitems:
-			obj.CreateTreasure(obj.randomitems, obj.level)
+	if not num:
+		num = 1
 	else:
-		obj.InsertInto(activator)
+		num = int(num)
+
+	for i in range(num):
+		try:
+			obj = CreateObject(archname)
+		except AtrinikError as err:
+			activator.Write(str(err), COLOR_RED)
+			break
+
+		obj.f_identified = True
+
+		if artname:
+			try:
+				obj.Artificate(artname)
+			except AtrinikError as err:
+				obj.Destroy()
+				activator.Write(str(err), COLOR_RED)
+				break
+
+		if attribs:
+			for (attrib, val) in re.findall(r'(\w+) ("[^"]+"|[^ ]+)', attribs):
+				if val.startswith('"') and val.endswith('"'):
+					val = val[1:-1]
+
+				try:
+					val = int(val)
+				except ValueError:
+					try:
+						val = float(val)
+					except ValueError:
+						pass
+
+				if val == "None":
+					val = None
+
+				if hasattr(obj, attrib):
+					setattr(obj, attrib, val)
+				elif hasattr(obj, "f_" + attrib):
+					setattr(obj, "f_" + attrib, True if val else False)
+				else:
+					obj.WriteKey(attrib, str(val))
+
+		if obj.f_monster:
+			activator.map.Insert(obj, activator.x, activator.y)
+			obj.Fix()
+
+			if obj.randomitems:
+				obj.CreateTreasure(obj.randomitems, obj.level)
+		else:
+			obj.InsertInto(activator)
 
 main()
