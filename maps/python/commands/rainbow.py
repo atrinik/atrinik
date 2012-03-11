4d1d109076146d39bb9376f2cea048829d0c2f34
Alex Tokar <admin@atokar.net>
2012-03-11 19:07:27 +0100

    Add /rainbow command.
    Former-commit-id: d78aa93508639f8b9532cd50a9b833df8d237f88

---
+++ maps/python/commands/rainbow.py
@@ -0,0 +1,19 @@
+## @file
+## The /rainbow command.
+
+import colorsys, random
+
+def main():
+	if not msg:
+		return
+
+	newmsg = ""
+
+	for c in msg:
+		(h, s, v) = (random.randint(0, 255) / 255, random.randint(0, 192) / 255, random.randint(168, 255) / 255)
+		(r, g, b) = colorsys.hsv_to_rgb(h, s, v)
+		newmsg += "<c=#%2X%2X%2X>%s</c>" % (255 * r, 255 * g, 255 * b, c)
+
+	activator.map.DrawInfo(activator.x, activator.y, newmsg, COLOR_WHITE, CHAT_TYPE_PUBLIC, activator.name)
+
+main()
