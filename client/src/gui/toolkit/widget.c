f318ab0791a692380469e698d69a746a2a9473f9
Alex Tokar <admin@atokar.net>
2012-03-13 01:24:07 +0100

    Allow using markup in labels.
    Former-commit-id: 266579e4f8c27fc5736aaa7dc39aa038632c8b00

--- client/src/gui/toolkit/widget.c
+++ client/src/gui/toolkit/widget.c
@@ -2095,8 +2095,8 @@ widgetdata *add_label(const char *text, int font, const char *color)
 	label->font = font;
 	label->color = color;
 
-	resize_widget(widget, RESIZE_RIGHT, text_get_width(font, text, 0));
-	resize_widget(widget, RESIZE_BOTTOM, text_get_height(font, text, 0) + 3);
+	resize_widget(widget, RESIZE_RIGHT, text_get_width(font, text, TEXT_MARKUP));
+	resize_widget(widget, RESIZE_BOTTOM, text_get_height(font, text, TEXT_MARKUP) + 3);
 
 	return widget;
 }
