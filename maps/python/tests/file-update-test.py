0921c1d0487d4f37217d76f333cd32604978540a
Alex Tokar <admin@atokar.net>
2012-07-29 02:45:53 +0200

    Add a test for file update packet.
    Former-commit-id: d600bc61b14f39c2e5149950a4b5110d941a068a

---
+++ maps/python/tests/file-update-test.py
@@ -0,0 +1,14 @@
+## @file
+## Tests the ability to download PNGs from the web (or stored locally, it matter little),
+## and send them in file update packet to the client for use as textures. This means it's
+# possible to use them with the <icon> tag, for example.
+
+import os, zlib
+from urllib.request import urlopen
+
+def send_file_update(pl, url):
+	basename = os.path.basename(url)
+	f = urlopen(imgurl)
+	s = f.read()
+	f.close()
+	pl.SendPacket(2, "sIx", "textures/" + basename, len(s), zlib.compress(s))
