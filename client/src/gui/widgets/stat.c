fdb8b2c7c577b797067954bf780801f49d11480f
Alex Tokar <admin@atokar.net>
2012-09-17 19:25:32 +0100

    Use new show_percentage attribute in stat spheres.
    Former-commit-id: fc930a864bb9231986b85ba8425a497ef7c5ce8d

--- client/src/gui/widgets/stat.c
+++ client/src/gui/widgets/stat.c
@@ -30,6 +30,12 @@
 
 #include <global.h>
 
+/**
+ * Possible display modes of the stat widget. */
+static const char *const display_modes[] = {
+	"Sphere", "Bar", "Text"
+};
+
 /** @copydoc widgetdata::draw_func */
 static void widget_draw(widgetdata *widget)
 {
@@ -75,7 +81,7 @@ static void widget_draw(widgetdata *widget)
 		else if (strcmp(tmp->texture, "sphere") == 0)
 		{
 			text_show_format(widget->surface, FONT_ARIAL11, WIDGET_BORDER_SIZE, WIDGET_BORDER_SIZE, COLOR_WHITE, TEXT_MARKUP, NULL, "<icon=stat_sphere_back %d %d 1>", widget->w - WIDGET_BORDER_SIZE * 2, widget->h - WIDGET_BORDER_SIZE * 2);
-			text_show_format(widget->surface, FONT_ARIAL11, WIDGET_BORDER_SIZE, WIDGET_BORDER_SIZE, COLOR_WHITE, TEXT_MARKUP, NULL, "<icon=stat_sphere_%s %d %d 1>", widget->id, widget->w - WIDGET_BORDER_SIZE * 2, widget->h - WIDGET_BORDER_SIZE * 2);
+			text_show_format(widget->surface, FONT_ARIAL11, WIDGET_BORDER_SIZE, WIDGET_BORDER_SIZE, COLOR_WHITE, TEXT_MARKUP, NULL, "<icon=stat_sphere_%s %d %d 1 0 %f>", widget->id, widget->w - WIDGET_BORDER_SIZE * 2, widget->h - WIDGET_BORDER_SIZE * 2, 4.0 + ((double) curr / (double) max));
 			text_show_format(widget->surface, FONT_ARIAL11, WIDGET_BORDER_SIZE, WIDGET_BORDER_SIZE, COLOR_WHITE, TEXT_MARKUP, NULL, "<icon=stat_sphere %d %d 1>", widget->w - WIDGET_BORDER_SIZE * 2, widget->h - WIDGET_BORDER_SIZE * 2);
 		}
 		else
@@ -151,6 +157,59 @@ static void widget_save(widgetdata *widget, FILE *fp, const char *padding)
 	fprintf(fp, "%stexture = %s\n", padding, tmp->texture);
 }
 
+static void menu_stat_display_change(widgetdata *widget, widgetdata *menuitem, SDL_Event *event)
+{
+	widget_stat_struct *tmp;
+	widgetdata *tmp2;
+	_widget_label *label;
+
+	tmp = (widget_stat_struct *) widget->subwidget;
+
+	for (tmp2 = menuitem->inv; tmp2; tmp2 = tmp2->next)
+	{
+		if (tmp2->type == LABEL_ID)
+		{
+			label = LABEL(tmp2);
+			free(tmp->texture);
+			tmp->texture = strdup(label->text);
+			string_tolower(tmp->texture);
+			WIDGET_REDRAW(widget);
+
+			break;
+		}
+	}
+}
+
+static void menu_stat_display(widgetdata *widget, widgetdata *menuitem, SDL_Event *event)
+{
+	widget_stat_struct *tmp;
+	widgetdata *submenu;
+	size_t i;
+
+	tmp = (widget_stat_struct *) widget->subwidget;
+	submenu = MENU(menuitem->env)->submenu;
+
+	for (i = 0; i < arraysize(display_modes); i++)
+	{
+		add_menuitem(submenu, display_modes[i], &menu_stat_display_change, MENU_CHECKBOX, strcasecmp(tmp->texture, display_modes[i]) == 0);
+	}
+}
+
+/** @copydoc widgetdata::menu_handle_func */
+static int widget_menu_handle(widgetdata *widget, SDL_Event *event)
+{
+	widgetdata *menu;
+
+	menu = create_menu(event->motion.x, event->motion.y, widget);
+
+	add_menuitem(menu, "Move Widget", &menu_move_widget, MENU_NORMAL, 0);
+	add_menuitem(menu, "Display  >", &menu_stat_display, MENU_SUBMENU, 0);
+
+	menu_finalize(menu);
+
+	return 1;
+}
+
 /**
  * Initialize one stat widget. */
 void widget_stat_init(widgetdata *widget)
@@ -163,5 +222,6 @@ void widget_stat_init(widgetdata *widget)
 	widget->deinit_func = widget_deinit;
 	widget->load_func = widget_load;
 	widget->save_func = widget_save;
+	widget->menu_handle_func = widget_menu_handle;
 	widget->subwidget = tmp;
 }
