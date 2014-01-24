4647a915c71f490669dcf42df6066db577c3a939
Alex Tokar <admin@atokar.net>
2014-01-24 17:00:47 -0500

    Simplify logic. There is no need to check if saw exists or not before removing it, as that block of code will only ever run once - afterwards, the player is teleported and the quest is completed.
    Former-commit-id: 18998e295ea2f9136ad6f8fddf162e0153f51063

--- maps/shattered_islands/scripts/deserted_tutorial_island/sam_goodberry.py
+++ maps/shattered_islands/scripts/deserted_tutorial_island/sam_goodberry.py
@@ -134,15 +134,11 @@ def main():
 
             elif msg == "alright":
                 saw = activator.FindObject(INVENTORY_CONTAINERS, "sam_goodberry_saw")
+                saw.Remove()
+
                 inf.set_title("Sailing the Ocean")
                 inf.set_icon(activator.arch.clone.face[0])
-
-                if saw:
-                    inf.add_msg("You return the saw to {} and help him repair the boat.".format(me.name), COLOR_YELLOW)
-                    saw.Remove()
-                else:
-                    inf.add_msg("You help {} repair the boat.".format(me.name), COLOR_YELLOW)
-
+                inf.add_msg("You return the saw to {} and help him repair the boat.".format(me.name), COLOR_YELLOW)
                 inf.add_msg("After patching up the boat and setting everything in order, you set sail.", COLOR_YELLOW)
                 inf.add_msg("You feel tired after all that hard work, perhaps you should speak to {}...".format(me.name), COLOR_YELLOW)
                 activator.TeleportTo("/shattered_islands/world_af01", 4, 6)
