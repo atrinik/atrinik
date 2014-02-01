56cf703c3d3cca23d43e53d813be93f6e8de770f
Alex Tokar <admin@atokar.net>
2014-02-01 09:39:39 -0500

    Add script for running uncrustify.
    Former-commit-id: af8e47afc88ccd79b315ad2c35c081dceace17c1

---
+++ tools/uncrustify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+GIT_PATH=`which git`
+UNCRUSTIFY_PATH=`which uncrustify`
+
+if [ -z $GIT_PATH ]; then
+    echo "Git not found, ensure it's installed"
+    exit 1
+fi
+
+if [ -z $UNCRUSTIFY_PATH ]; then
+    echo "Uncrustify not found, ensure it's installed"
+    exit 1
+fi
+
+REPO_ROOT=`git rev-parse --show-toplevel`
+
+
+find "$REPO_ROOT" -name "*.[ch]" -and -not -path "*CMakeFiles*" -exec \
+    "$UNCRUSTIFY_PATH" --no-backup -c "$REPO_ROOT/uncrustify.cfg" {} \;
