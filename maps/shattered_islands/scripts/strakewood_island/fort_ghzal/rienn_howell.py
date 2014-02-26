39b9aa5fec4b3c649deda44130618f8410f2558d
Alex Tokar <admin@atokar.net>
2014-02-26 19:05:38 +0000

    Use InterfaceBuilderQuest in "Two Lovers Doomed" quest.

--- maps/shattered_islands/scripts/strakewood_island/fort_ghzal/rienn_howell.py
+++ maps/shattered_islands/scripts/strakewood_island/fort_ghzal/rienn_howell.py
@@ -1,28 +1,37 @@
 ## @file
 ## Script for Rienn Howell in Fort Ghzal.
 
-from Interface import Interface
+from Interface import InterfaceBuilderQuest
 from QuestManager import QuestManagerMulti
 from Quests import TwoLoversDoomed as quest
 
-inf = Interface(activator, me)
+class InterfaceDialog(InterfaceBuilderQuest):
+    """
+    Default dialog; regardless of whether the quest has been completed or not,
+    Rienn just stares in the direction of the ruins.
+    """
+
+    def dialog_hello(self):
+        self.add_msg("{npc.name} doesn't seem to notice you and continues to stare in the direction of the ruins north...", color = COLOR_YELLOW)
+
+class InterfaceDialog_need_complete_deliver_tortwalds_letter(InterfaceDialog):
+    """
+    Dialog when the player has Tortwald's letter, and needs to deliver it to
+    Rienn.
+    """
+
+    def dialog_hello(self):
+        InterfaceDialog.dialog_hello(self)
+        self.add_link("Here, take this letter from your husband...", dest = "takeletter")
+
+    def dialog_takeletter(self):
+        self.add_msg("Oh! He has been gone for a long time now...")
+        self.add_msg("Thank you, adventurer... Please, take this letter and deliver it to him...")
+        self.add_objects(me.FindObject(archname = "two_lovers_doomed_letter2"))
+
+        self.qm.start("deliver_rienns_letter")
+        self.qm.complete("deliver_tortwalds_letter")
+
 qm = QuestManagerMulti(activator, quest)
-
-def main():
-    if qm.need_complete("deliver tortwalds letter"):
-        if msg == "hello":
-            inf.add_msg("{} doesn't seem to notice you and continues to stare in the direction of the ruins north...".format(me.name), COLOR_YELLOW)
-            inf.add_link("Here, take this letter from your husband...", dest = "takeletter")
-
-        elif msg == "takeletter":
-            inf.add_msg("Oh! He has been gone for a long time now...")
-            inf.add_msg("Thank you, adventurer... Please, take this letter and deliver it to him...")
-            inf.add_objects(me.FindObject(archname = "two_lovers_doomed_letter2"))
-            qm.start("deliver rienns letter")
-            qm.complete("deliver tortwalds letter")
-
-    else:
-        inf.add_msg("{} doesn't seem to notice you and continues to stare in the direction of the ruins north...".format(me.name), COLOR_YELLOW)
-
-main()
-inf.finish()
+ib = InterfaceBuilderQuest(activator, me)
+ib.finish(locals(), qm, msg)
--- maps/shattered_islands/strakewood_island/underground_city/scripts/tortwald_howell.py
+++ maps/shattered_islands/strakewood_island/underground_city/scripts/tortwald_howell.py
@@ -1,64 +1,79 @@
 ## @file
 ## Script for Tortwald Howell, a guard imprisoned in Underground City.
 
-from Interface import Interface
+from Interface import InterfaceBuilderQuest
 from QuestManager import QuestManagerMulti
 from Quests import TwoLoversDoomed as quest
 
-inf = Interface(activator, me)
+class InterfaceDialog_need_start_deliver_tortwalds_letter(InterfaceBuilderQuest):
+    """
+    Dialog for starting Tortwald's letter delivery quest.
+    """
+
+    def dialog_hello(self):
+        self.add_msg("*sob*... Hello there...")
+        self.add_link("Who are you?", dest = "who")
+
+    def dialog_who(self):
+        self.add_msg("I'm {npc.name}, a guard from Fort Ghzal...")
+        self.add_link("What are you doing here?", dest = "doing")
+
+    def dialog_doing(self):
+        self.add_msg("I was captured by the creatures living in this terrible place... I don't know what they want to do with me...")
+        self.add_link("Can I help you?", dest = "help")
+
+    def dialog_help(self):
+        self.add_msg("No... I'm too weak to escape... And these bars are impervious to everything I have tried... But... Would you, please, deliver a letter to my wife in Fort Ghzal for me...?")
+        self.add_link("Sure.", dest = "sure")
+        self.add_link("No, too busy.", dest = "no")
+
+    def dialog_no(self):
+        self.add_msg("*sobs*... Please?")
+        self.add_link("Alright then.", dest = "sure")
+        self.add_link("I said no! I don't have time for your sniveling.", action = "close")
+
+    def dialog_sure(self):
+        self.add_msg("Thank you... Here's the letter. Please, deliver it to my wife, Rienn Howell in Fort Ghzal...")
+        self.add_objects(me.FindObject(archname = "two_lovers_doomed_letter1"))
+
+        self.qm.start("deliver_tortwalds_letter")
+
+class InterfaceDialog_need_complete_deliver_tortwalds_letter(InterfaceBuilderQuest):
+    """
+    Dialog to remind the player about delivering the letter.
+    """
+
+    def dialog_hello(self):
+        self.add_msg("*sobs*... Have you delivered the letter yet?")
+        self.add_link("Working on it.", dest = "working")
+
+    def dialog_working(self):
+        self.add_msg("Please, deliver it to my wife, Rienn Howell in Fort Ghzal...")
+
+class InterfaceDialog_need_complete_deliver_rienns_letter(InterfaceBuilderQuest):
+    """
+    Dialog when the player has delivered the letter, and has Rienn's to deliver.
+    """
+
+    def dialog_hello(self):
+        self.add_msg("*sobs*... Have you delivered the letter yet?")
+        self.add_link("Yes, take this letter from your wife.", dest = "takeletter")
+
+    def dialog_takeletter(self):
+        self.add_msg("You have a letter for me from my dear Rienn? Oh... thank you...")
+        self.add_msg("Here, take this key... it supposedly opens something around here, but before I could figure it out, I was captured... and... thank you again.")
+        self.add_objects(me.FindObject(archname = "uc_ii_skull_key"))
+
+        self.qm.complete("deliver_rienns_letter")
+
+class InterfaceDialog_completed(InterfaceBuilderQuest):
+    """
+    Quest has been completed.
+    """
+
+    def dialog_hello(self):
+        self.add_msg("Thank you...")
+
 qm = QuestManagerMulti(activator, quest)
-
-def main():
-    if not qm.started("deliver tortwalds letter"):
-        if msg == "hello":
-            inf.add_msg("*sob*... Hello there...")
-            inf.add_link("Who are you?", dest = "who")
-
-        elif msg == "who":
-            inf.add_msg("I'm {}, a guard from Fort Ghzal...".format(me.name))
-            inf.add_link("What are you doing here?", dest = "doing")
-
-        elif msg == "doing":
-            inf.add_msg("I was captured by the creatures living in this terrible place... I don't know what they want to do with me...")
-            inf.add_link("Can I help you?", dest = "help")
-
-        elif msg == "help":
-            inf.add_msg("No... I'm too weak to escape... And these bars are impervious to everything I have tried... But... Would you, please, deliver a letter to my wife in Fort Ghzal for me...?")
-            inf.add_link("Sure.", dest = "sure")
-            inf.add_link("No, too busy.", dest = "no")
-
-        elif msg == "no":
-            inf.add_msg("*sobs*... Please?")
-            inf.add_link("Alright then.", dest = "sure")
-            inf.add_link("I said no! I don't have time for your sniveling.", action = "close")
-
-        elif msg == "sure":
-            inf.add_msg("Thank you... Here's the letter. Please, deliver it to my wife, Rienn Howell in Fort Ghzal...")
-            inf.add_objects(me.FindObject(archname = "two_lovers_doomed_letter1"))
-            qm.start("deliver tortwalds letter")
-
-    elif not qm.completed("deliver tortwalds letter"):
-        if msg == "hello":
-            inf.add_msg("*sobs*... Have you delivered the letter yet?")
-            inf.add_link("Working on it.", dest = "working")
-
-        elif msg == "working":
-            inf.add_msg("Please, deliver it to my wife, Rienn Howell in Fort Ghzal...")
-
-    elif qm.need_complete("deliver rienns letter"):
-        if msg == "hello":
-            inf.add_msg("*sobs*... Have you delivered the letter yet?")
-            inf.add_link("Yes, take this letter from your wife.", dest = "takeletter")
-
-        elif msg == "takeletter":
-            inf.add_msg("You have a letter for me from my dear Rienn? Oh... thank you...")
-            inf.add_msg("Here, take this key... it supposedly opens something around here, but before I could figure it out, I was captured... and... thank you again.")
-            inf.add_objects(me.FindObject(archname = "uc_ii_skull_key"))
-            qm.complete("deliver rienns letter")
-
-    else:
-        if msg == "hello":
-            inf.add_msg("Thank you...")
-
-main()
-inf.finish()
+ib = InterfaceBuilderQuest(activator, me)
+ib.finish(locals(), qm, msg)
