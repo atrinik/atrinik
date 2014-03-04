392d7988ce03b2ea34aa34fe417fc6427e79c760
Alex Tokar <admin@atokar.net>
2014-03-04 17:45:46 +0000

    Mass-replace <yellow>...</yellow> with [yellow]...[/yellow].

--- maps/python/generic/guard_drunk.py
+++ maps/python/generic/guard_drunk.py
@@ -13,7 +13,7 @@ def main():
             "Get away from my *hic* beer *hic*!",
             "I can see you! *hic* Don' make me come af'er you! *hic* ... Can ye find my sword? I think I losst it!",
             "Bartender, 'nother beer here!",
-            "<yellow>The " + me.name + " breaks into a song...</yellow>\n\nI am a guard aaaand I am havin' a beer. Drinky drinky guard, drinky drinky guard.",
+            "[yellow]The " + me.name + " breaks into a song...[/yellow]\n\nI am a guard aaaand I am havin' a beer. Drinky drinky guard, drinky drinky guard.",
             "*HIC*",
             "I'm *hic!* watchin' you! I bets ye think I'm *hic* off duty but ye be wrong! I'm here guardin' my beer an' no-one iss gonna *hic!* take it from me!",
             "It's not fair! *hic!* I gets paid low wages! But I *hic* are super guard! We don' needs so many *hic* guards aroun' here! I can handles this whole *hic* here place meself! Noth'n can *hic* beat me! Hahahaha*hic!*!",
