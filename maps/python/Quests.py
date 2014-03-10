0d8829e90eb2ab6e90e8570dec15ea909c981f94
Alex Tokar <admin@atokar.net>
2014-03-10 19:51:36 +0000

    Use XML interface for Construction of Telescope quest.

--- maps/python/Quests.py
+++ maps/python/Quests.py
@@ -54,41 +54,6 @@ LostMemories = {
     },
 }
 
-ConstructionOfTelescope = {
-    "quest_name": "Construction of Telescope",
-    "type": QUEST_TYPE_MULTI,
-    "parts": {
-        "get shard": {
-            "message": "Albar from the Morliana Research Center asked you to go to Brynknot and find a shard from the Great Blue Crystal. He suspects an alchemist or mage might have found such a shard.",
-            "arch_name": "blue_crystal_fragment",
-            "item_name": "blue crystal fragment",
-            "type": QUEST_TYPE_KILL_ITEM,
-        },
-        "ask about flash": {
-            "message": "Albar was very pleased - and confused - with the fragment and the report you received from Jonaslen the mage in Brynknot. He has asked you to go back to Brynknot and ask Jonaslen whether there were reports of a flash in the sky just before the earthquake.",
-            "type": QUEST_TYPE_SPECIAL,
-        },
-        "report about flash": {
-            "message": "Jonaslen the mage from Brynknot has told you there were reports about a flash in the sky just before the earthquake from many of the townsfolk, including the guards on duty that day. He also thinks the crystal fell from the sky, which would explain the earthquake and the hole.",
-            "type": QUEST_TYPE_SPECIAL,
-        },
-        "get clear crystal": {
-            "message": "Albar has asked you to find a clear crystal for his telescope. He thinks Morg'eean the kobold trader south of Asteria might have some for sale in his little shop.",
-            "type": QUEST_TYPE_SPECIAL,
-        },
-        "get wood": {
-            "message": "After delivering the clear crystal to Albar, he has asked to find him wood from the ancient tree Silmedsen who should be south of the Asteria Swamp.",
-            "arch_name": "silmedsen_branches",
-            "item_name": "Silmedsen's branches",
-            "type": QUEST_TYPE_KILL_ITEM,
-        },
-        "get morliana water": {
-            "message": "The ancient tree Silmedsen south of Asteria Swamp has asked you to fill an empty bottle - which he has given you - with water that surrounds the Great Blue Crystal in Morliana.",
-            "type": QUEST_TYPE_SPECIAL,
-        },
-    },
-}
-
 RescuingLynren = {
     "quest_name": "Rescuing Lynren",
     "type": QUEST_TYPE_KILL_ITEM,
