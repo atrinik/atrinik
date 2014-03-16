425d09d86b59e6585e416d825d65f4a9f24f3ff7
Alex Tokar <admin@atokar.net>
2014-03-16 08:08:58 +0000

    Use XML interface for Lost Memories quest.

--- maps/python/Quests.py
+++ maps/python/Quests.py
@@ -3,15 +3,3 @@
 
 from Atrinik import *
 from collections import OrderedDict
-
-LostMemories = {
-    "quest_name": "Lost Memories",
-    "type": QUEST_TYPE_MULTI,
-    "message": "You have reached Incuna, and have started the attempt to get back your lost memories...",
-    "parts": {
-        "speak priest": {
-            "message": "Sam Goodberry has suggested that you go speak to the local church priest, who may be able to help you.",
-            "type": QUEST_TYPE_SPECIAL,
-        },
-    },
-}
