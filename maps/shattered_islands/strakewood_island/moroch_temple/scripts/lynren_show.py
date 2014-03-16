e198dea561d141913788d8be313b5de368d1722d
Alex Tokar <admin@atokar.net>
2014-03-16 07:45:59 +0000

    Use XML interface for Rescuing Lynren quest.

--- maps/shattered_islands/strakewood_island/moroch_temple/scripts/lynren_show.py
+++ maps/shattered_islands/strakewood_island/moroch_temple/scripts/lynren_show.py
@@ -2,13 +2,8 @@
 ## Handles the 'ask show' event for Lynren.
 
 from QuestManager import QuestManager
-from Quests import RescuingLynren as quest
+from InterfaceQuests import rescuing_lynren
 
-qm = QuestManager(activator, quest)
-
-def main():
-    # If we completed the quest, hide the NPC.
-    if qm.completed():
-        SetReturnValue(1)
-
-main()
+# If we completed the quest, hide the NPC.
+if QuestManager(activator, rescuing_lynren).completed():
+    SetReturnValue(1)
