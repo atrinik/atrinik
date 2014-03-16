7a542d2b9c47152d1001bcad5eb8dfc72d9f767f
Alex Tokar <admin@atokar.net>
2014-03-16 08:12:11 +0000

    Update script.

--- maps/shattered_islands/strakewood_island/brynknot/sewers/scripts/beholder_portal.py
+++ maps/shattered_islands/strakewood_island/brynknot/sewers/scripts/beholder_portal.py
@@ -2,10 +2,8 @@
 ## Script for the quest room of the Brynknot Sewers Maze.
 
 from QuestManager import QuestManagerMulti
-from Quests import LlwyfenPortal as quest
+from InterfaceQuests import portal_of_llwyfen
 
-qm = QuestManagerMulti(activator, quest)
-
-if not qm.finished("kill boss"):
+if not QuestManagerMulti(activator, portal_of_llwyfen).finished("kill_boss"):
     activator.TeleportTo(me.map.GetPath(me.slaying + "_nyhelobo", True), me.hp, me.sp)
     SetReturnValue(1)
