8bd166b9942cf5a9b25cadd3557a668540987d92
Alex Tokar <admin@atokar.net>
2014-11-05 19:19:14 +0000

    Change variable size.

--- client/src/include/player.h
+++ client/src/include/player.h
@@ -93,7 +93,7 @@ typedef struct Stat_struct
     uint16 flags;
 
     /** Protections. */
-    sint16 protection[20];
+    sint8 protection[20];
 
     /** Ranged weapon damage. */
     sint16 ranged_dam;
