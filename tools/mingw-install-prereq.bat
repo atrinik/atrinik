bcd579fd452eafe80d47eca7f9e16f7a139b2946
Alex Tokar <admin@atokar.net>
2014-11-11 21:53:38 +0000

    Simplify script.

--- tools/mingw-install-prereq.bat
+++ tools/mingw-install-prereq.bat
@@ -1,11 +1,3 @@
-@echo off
-setlocal ENABLEEXTENSIONS
-set KEY_NAME=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
-set VALUE_NAME=Lib
-for /F "usebackq tokens=3" %%A IN (`reg query "%KEY_NAME%" /v "%VALUE_NAME%" 2^>nul ^| find "%VALUE_NAME%"`) do (
-  set MINGW_PATH=%%A
-)
-
-%MINGW_PATH%\..\..\bin\mingw-get.exe install mingw-developer-toolkit-bin mingw32-base-bin mingw32-binutils-bin mingw32-gcc-dev mingw32-gdb-bin mingw32-libgcc-dll mingw32-libltdl-dev mingw32-libpthreadgc-dll mingw32-libstdc++-dll mingw32-libz-dev mingw32-make-bin mingw32-pthreads-w32-dev mingw32-w32api-dev msys-flex-bin msys-libcrypt-dev msys-make-bin
+mingw-get install mingw-developer-toolkit-bin mingw32-base-bin mingw32-binutils-bin mingw32-gcc-dev mingw32-gdb-bin mingw32-libgcc-dll mingw32-libltdl-dev mingw32-libpthreadgc-dll mingw32-libstdc++-dll mingw32-libz-dev mingw32-make-bin mingw32-pthreads-w32-dev mingw32-w32api-dev msys-flex-bin msys-libcrypt-dev msys-make-bin
 
 pause
