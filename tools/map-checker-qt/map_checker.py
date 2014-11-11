92bd9ee24c133bd70fd18476c5ffa0a22156451b
Alex Tokar <admin@atokar.net>
2014-11-11 18:11:29 +0100

    Fix #64: Map Checker (QT) loses issues found in resources after re-scan

--- tools/map-checker-qt/map_checker.py
+++ tools/map-checker-qt/map_checker.py
@@ -28,7 +28,7 @@ class MapChecker:
     Implements the map checker class, which handles things such as
     calling the map parser, actually checking for errors, etc.
     '''
-    
+
     definitionFilesData = OrderedDict([
                                        [
                                         "archetype",
@@ -52,10 +52,10 @@ class MapChecker:
                                         }
                                         ],
                                        ])
-    
+
     def __init__(self, config):
         self.config = config
-        
+
         # Create scanners, datasets, parsers, etc.
         self.scanner = ScannerMap(config)
         self.archetypes = ArchObjectCollection("archetype")
@@ -73,54 +73,54 @@ class MapChecker:
         self.global_objects = {
                                system.constants.game.types.beacon: [],
                                }
-        
+
         for collection in self.collections:
             self.collection_parser(collection).setCollection(collection)
-        
+
         self.archetypes.addLinkedCollection(self.artifacts)
-        
+
         for name in dir(self):
             if hasattr(getattr(self, name), "set_map_checker"):
                 getattr(self, name).set_map_checker(self)
-                
+
         self.checker_map.set_map_checker(self)
-        
+
         self.info_add_fnc = None
         self._thread = None
-        
+
         self.queue = queue.Queue()
-        
+
         self._scan_running = False
         self._scan_status = ""
         self._scan_progress = 0
-        
+
         self.path = os.path.dirname(os.path.realpath(__file__))
-        
+
     @property
     def collections(self):
         '''Returns all the available object collections.'''
         return sorted([self.__dict__[obj] for obj in self.__dict__ if isinstance(self.__dict__[obj], AbstractObjectCollection)], key = lambda x: list(self.definitionFilesData.keys()).index(x.name))
-    
+
     def collection_parser(self, collection):
         '''Returns a parser object for the specified collection.'''
         return self.__dict__["parser_" + collection.name]
-    
+
     def collection_checker(self, collection):
         '''Returns a checker object for the specified collection.'''
-        
+
         try:
             return self.__dict__["checker_" + collection.name]
         except KeyError:
             return None
-    
+
     def getDefinitionsPath(self, name):
         '''Returns absolute path to the specified definitions file.'''
         return os.path.join(self.config.get("General", self.definitionFilesData[name]["option"]), self.definitionFilesData[name]["filename"])
-    
+
     def getMapsPath(self):
         '''Returns absolute path to the maps directory.'''
         return self.config.get("General", "path_dir_maps")
-        
+
     def _scan(self, path):
         '''
         Internal function for actually performing the scan. Used by scan,
@@ -128,125 +128,129 @@ class MapChecker:
         _scan_status, _scan_progress, etc. Puts info about errors into
         the queue object
         '''
-        
+
         if not path:
             path = self.getMapsPath()
-            
+
         for key in self.global_objects:
             self.global_objects[key] = []
-        
+
         self._scan_progress = 0
-        
+
         # First scan for possible map files.
         self._scan_status = "Gathering files..."
         files = self.scanner.scan(path)
-        
+
         # Now filter out non-map files by reading the header of all
         # found files.
         self._scan_status = "Gathering map files..."
         maps = self.scanner.filter_map_files(files)
-        
+
         # Parse file definitions.
         for collection in self.collections:
             if not self._thread_running:
                 return
-            
+
             path = self.getDefinitionsPath(collection.name)
-                
+            checker = self.collection_checker(collection)
+
             if collection.needReload(path):
                 self._scan_status = "Parsing {} definitions...".format(collection.name)
-                
+
                 with open(path, "r") as f:
                     self.collection_parser(collection).parse(f)
                     collection.setLastRead(path)
-                    
-                checker = self.collection_checker(collection)
-                
+
+                for error in self.collection_parser(collection).errors:
+                    self.queue.put(error)
+
                 if checker:
                     self._scan_status = "Checking {} definitions...".format(collection.name)
                     checker.setPath(path)
-                    
+
                     for obj in collection:
                         checker.check(collection[obj])
-                        
+
                         for error in checker.errors:
-                            self.queue.put(error) 
-            
-            for error in self.collection_parser(collection).errors:
-                self.queue.put(error)
-        
+                            self.queue.put(error)
+                            self.collection_parser(collection).errors.append(
+                                error)
+            else:
+                for error in self.collection_parser(collection).errors:
+                    self.queue.put(error)
+
         # Now loop through the maps.
         for i, file in enumerate(maps):
             if not self._thread_running:
                 return
-            
+
             # Update scan progress.
             self._scan_progress = (i + 1) / len(maps)
-            
+
             self._scan_status = "Parsing {}...".format(file)
-            
+
             # Parse the map file.
             with open(file, "r") as f:
                 m = self.parser_map.parse(f)
-                
+
             #with open(file + ".tmp", "w", newline="\n") as f:
             #    self.saver_map.save(m, f)
-            
+
             self._scan_status = "Checking {}...".format(file)
-            
+
             self.checker_map.check(m)
-            
+
             for error in self.checker_map.errors:
                 self.queue.put(error)
-        
+
     def scan(self, path = None, threading = True):
         '''Perform a new scan for errors.'''
         if self._thread and self._thread.is_alive():
             return
-        
+
         self._thread_running = True
-            
+
         if threading:
             self._thread = Thread(target = self._scan, args = (path, ))
             self._thread.start()
         else:
             self._scan(path)
-    
+
     def scan_stop(self):
         '''Stop current scan, if any.'''
         if self._thread and self._thread.is_alive():
             self._thread_running = False
             self._scan_status = "Stopping..."
-            
+
     def scan_is_running(self):
         '''Check if there is currently a scan running.'''
         if self._thread:
             return self._thread.is_alive()
-        
+
         return False
-    
+
     def scan_get_progress(self):
         '''Get progress of the current scan.'''
         return self._scan_progress
-    
+
     def scan_get_status(self):
         '''Get status of the current scan.'''
         return self._scan_status
-            
+
     def exit(self):
-        '''Called when the application exits. Stops the scan, if any.''' 
+        '''Called when the application exits. Stops the scan, if any.'''
         self.scan_stop()
 
 def main():
     from PyQt5.QtWidgets import QApplication
-    
+
     config = Config()
-    
+
     # Create a MapChecker class.
     map_checker = MapChecker(config)
     # Load the program's configuration.
     config.load(map_checker.path)
-    
+
     # Try to parse our command line options.
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hc", ["help", "cli"])
@@ -254,17 +258,17 @@ def main():
         # Invalid option, show the error and exit.
         print(err)
         sys.exit(2)
-        
+
     cli = False
-    
+
     # Parse options.
     for o, a in opts:
         if o in ("-h", "--help"):
             ##usage()
             sys.exit()
         elif o in ("-c", "--cli"):
-            cli = True 
-    
+            cli = True
+
     if not cli:
         # Create a GUI window using Qt.
         app = QApplication(sys.argv)
@@ -273,15 +277,15 @@ def main():
         window.setMapChecker(map_checker)
         window.setExitFunction(map_checker.exit)
         window.show()
-        
+
         ret = app.exec_()
     else:
-        map_checker.scan(threading = False)    
+        map_checker.scan(threading = False)
         ret = 0
-        
+
     # Save configuration on exit.
     config.save()
     sys.exit(ret)
-    
+
 if __name__ == "__main__":
     main()
