7e49bc0a304ea2d7ee811753ff355531326a0f3b
Alex Tokar <admin@atokar.net>
2014-11-13 14:08:29 +0100

    Catch SIGABRT and throw Windows-style exception, attempt to write traceback to a file if possible.

--- server/src/toolkit/signals.c
+++ server/src/toolkit/signals.c
@@ -57,7 +57,8 @@ static const int register_signals[] = {
 #endif
     SIGINT,
     SIGTERM,
-    SIGSEGV
+    SIGSEGV,
+    SIGABRT
 };
 
 /**
@@ -65,6 +66,14 @@ static const int register_signals[] = {
  * @param signum ID of the signal being intercepted. */
 static void signal_handler(int signum)
 {
+    if (signum == SIGABRT) {
+#ifdef WIN32
+        RaiseException(STATUS_ACCESS_VIOLATION, 0, 0, 0);
+#else
+        return;
+#endif
+    }
+        
     /* SIGSEGV, so abort instead of exiting normally. */
     if (signum == SIGSEGV) {
         abort();
@@ -75,111 +84,129 @@ static void signal_handler(int signum)
 
 #ifdef WIN32
 
-void windows_print_stacktrace(CONTEXT *context)
-{
-    STACKFRAME frame;
-    int i;
-
-    SymInitialize(GetCurrentProcess(), 0, 1);
-
-    memset(&frame, 0, sizeof(frame));
-    frame.AddrPC.Offset = context->Eip;
-    frame.AddrPC.Mode = AddrModeFlat;
-    frame.AddrStack.Offset = context->Esp;
-    frame.AddrStack.Mode = AddrModeFlat;
-    frame.AddrFrame.Offset = context->Ebp;
-    frame.AddrFrame.Mode = AddrModeFlat;
-
-    i = 0;
-
-    while (StackWalk(IMAGE_FILE_MACHINE_I386, GetCurrentProcess(),
-            GetCurrentThread(), &frame, context, 0, SymFunctionTableAccess,
-            SymGetModuleBase, 0)) {
-        fprintf(stderr, "%d: %p\n", i, (void *) frame.AddrPC.Offset);
-        i++;
-    }
-
-    SymCleanup(GetCurrentProcess());
-}
-
 LONG WINAPI windows_exception_handler(EXCEPTION_POINTERS *ExceptionInfo)
 {
+    struct tm *tm;
+    static time_t t = 0;
+    char path[HUGE_BUF], date[MAX_BUF];
+    FILE *fp;
+
+    if (t == 0) {
+        t = time(NULL);
+    }
+    
+    tm = localtime(&t);
+
+    strftime(VS(date), "%Y_%m_%d_%H-%M-%S", tm);
+    snprintf(VS(path), "traceback_%s.txt", date);
+    fp = fopen(path, "a");
+    
+    if (fp == NULL) {
+        fp = stderr;
+    }
+    
     switch (ExceptionInfo->ExceptionRecord->ExceptionCode) {
     case EXCEPTION_ACCESS_VIOLATION:
-        fputs("Error: EXCEPTION_ACCESS_VIOLATION\n", stderr);
+        fputs("Error: EXCEPTION_ACCESS_VIOLATION\n", fp);
         break;
     case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
-        fputs("Error: EXCEPTION_ARRAY_BOUNDS_EXCEEDED\n", stderr);
+        fputs("Error: EXCEPTION_ARRAY_BOUNDS_EXCEEDED\n", fp);
         break;
     case EXCEPTION_BREAKPOINT:
-        fputs("Error: EXCEPTION_BREAKPOINT\n", stderr);
+        fputs("Error: EXCEPTION_BREAKPOINT\n", fp);
         break;
     case EXCEPTION_DATATYPE_MISALIGNMENT:
-        fputs("Error: EXCEPTION_DATATYPE_MISALIGNMENT\n", stderr);
+        fputs("Error: EXCEPTION_DATATYPE_MISALIGNMENT\n", fp);
         break;
     case EXCEPTION_FLT_DENORMAL_OPERAND:
-        fputs("Error: EXCEPTION_FLT_DENORMAL_OPERAND\n", stderr);
+        fputs("Error: EXCEPTION_FLT_DENORMAL_OPERAND\n", fp);
         break;
     case EXCEPTION_FLT_DIVIDE_BY_ZERO:
-        fputs("Error: EXCEPTION_FLT_DIVIDE_BY_ZERO\n", stderr);
+        fputs("Error: EXCEPTION_FLT_DIVIDE_BY_ZERO\n", fp);
         break;
     case EXCEPTION_FLT_INEXACT_RESULT:
-        fputs("Error: EXCEPTION_FLT_INEXACT_RESULT\n", stderr);
+        fputs("Error: EXCEPTION_FLT_INEXACT_RESULT\n", fp);
         break;
     case EXCEPTION_FLT_INVALID_OPERATION:
-        fputs("Error: EXCEPTION_FLT_INVALID_OPERATION\n", stderr);
+        fputs("Error: EXCEPTION_FLT_INVALID_OPERATION\n", fp);
         break;
     case EXCEPTION_FLT_OVERFLOW:
-        fputs("Error: EXCEPTION_FLT_OVERFLOW\n", stderr);
+        fputs("Error: EXCEPTION_FLT_OVERFLOW\n", fp);
         break;
     case EXCEPTION_FLT_STACK_CHECK:
-        fputs("Error: EXCEPTION_FLT_STACK_CHECK\n", stderr);
+        fputs("Error: EXCEPTION_FLT_STACK_CHECK\n", fp);
         break;
     case EXCEPTION_FLT_UNDERFLOW:
-        fputs("Error: EXCEPTION_FLT_UNDERFLOW\n", stderr);
+        fputs("Error: EXCEPTION_FLT_UNDERFLOW\n", fp);
         break;
     case EXCEPTION_ILLEGAL_INSTRUCTION:
-        fputs("Error: EXCEPTION_ILLEGAL_INSTRUCTION\n", stderr);
+        fputs("Error: EXCEPTION_ILLEGAL_INSTRUCTION\n", fp);
         break;
     case EXCEPTION_IN_PAGE_ERROR:
-        fputs("Error: EXCEPTION_IN_PAGE_ERROR\n", stderr);
+        fputs("Error: EXCEPTION_IN_PAGE_ERROR\n", fp);
         break;
     case EXCEPTION_INT_DIVIDE_BY_ZERO:
-        fputs("Error: EXCEPTION_INT_DIVIDE_BY_ZERO\n", stderr);
+        fputs("Error: EXCEPTION_INT_DIVIDE_BY_ZERO\n", fp);
         break;
     case EXCEPTION_INT_OVERFLOW:
-        fputs("Error: EXCEPTION_INT_OVERFLOW\n", stderr);
+        fputs("Error: EXCEPTION_INT_OVERFLOW\n", fp);
         break;
     case EXCEPTION_INVALID_DISPOSITION:
-        fputs("Error: EXCEPTION_INVALID_DISPOSITION\n", stderr);
+        fputs("Error: EXCEPTION_INVALID_DISPOSITION\n", fp);
         break;
     case EXCEPTION_NONCONTINUABLE_EXCEPTION:
-        fputs("Error: EXCEPTION_NONCONTINUABLE_EXCEPTION\n", stderr);
+        fputs("Error: EXCEPTION_NONCONTINUABLE_EXCEPTION\n", fp);
         break;
     case EXCEPTION_PRIV_INSTRUCTION:
-        fputs("Error: EXCEPTION_PRIV_INSTRUCTION\n", stderr);
+        fputs("Error: EXCEPTION_PRIV_INSTRUCTION\n", fp);
         break;
     case EXCEPTION_SINGLE_STEP:
-        fputs("Error: EXCEPTION_SINGLE_STEP\n", stderr);
+        fputs("Error: EXCEPTION_SINGLE_STEP\n", fp);
         break;
     case EXCEPTION_STACK_OVERFLOW:
-        fputs("Error: EXCEPTION_STACK_OVERFLOW\n", stderr);
+        fputs("Error: EXCEPTION_STACK_OVERFLOW\n", fp);
         break;
     default:
-        fputs("Error: Unrecognized Exception\n", stderr);
+        fputs("Error: Unrecognized Exception\n", fp);
         break;
     }
 
-    fputs("Stack trace:\n", stderr);
+    fputs("Stack trace:\n", fp);
 
     if (EXCEPTION_STACK_OVERFLOW !=
             ExceptionInfo->ExceptionRecord->ExceptionCode) {
-        windows_print_stacktrace(ExceptionInfo->ContextRecord);
+        STACKFRAME frame;
+        int i;
+
+        SymInitialize(GetCurrentProcess(), 0, 1);
+
+        memset(&frame, 0, sizeof(frame));
+        frame.AddrPC.Offset = ExceptionInfo->ContextRecord->Eip;
+        frame.AddrPC.Mode = AddrModeFlat;
+        frame.AddrStack.Offset = ExceptionInfo->ContextRecord->Esp;
+        frame.AddrStack.Mode = AddrModeFlat;
+        frame.AddrFrame.Offset = ExceptionInfo->ContextRecord->Ebp;
+        frame.AddrFrame.Mode = AddrModeFlat;
+
+        i = 0;
+
+        while (StackWalk(IMAGE_FILE_MACHINE_I386, GetCurrentProcess(),
+                GetCurrentThread(), &frame, ExceptionInfo->ContextRecord, 0,
+                SymFunctionTableAccess, SymGetModuleBase, 0)) {
+            fprintf(fp, "%d: %p\n", i, (void *) frame.AddrPC.Offset);
+            i++;
+        }
+
+        SymCleanup(GetCurrentProcess());
     } else {
-        fprintf(stderr, "%p\n", (void *) ExceptionInfo->ContextRecord->Eip);
+        fprintf(fp, "%p\n", (void *) ExceptionInfo->ContextRecord->Eip);
     }
 
-    fflush(stderr);
+    if (fp == stderr) {
+        fflush(stderr);
+    } else {
+        fclose(fp);
+    }
 
     return EXCEPTION_EXECUTE_HANDLER;
 }
