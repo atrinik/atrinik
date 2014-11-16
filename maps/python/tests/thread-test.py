4b1a7e6ae12d8aad9f431f5a6a3e0ef191bf33fe
Alex Tokar <admin@atokar.net>
2014-11-16 17:50:29 +0000

    Use Eval() in thread-test.py script.

--- maps/python/tests/thread-test.py
+++ maps/python/tests/thread-test.py
@@ -1,39 +1,25 @@
-def main2():
-    import threading
-    from Common import find_obj
+def timer1(objs):
+    for obj in objs:
+        if not obj:
+            try:
+                objs.remove(obj)
+            except:
+                pass
+        else:
+            print(obj)
 
-    sack = find_obj(activator, name = "sack")
+    for i in range(2):
+        obj = CreateObject("coppercoin")
+        objs.append(obj)
 
-    def timer(objs):
-        for obj in objs:
-            if not obj:
-                try:
-                    objs.remove(obj)
-                except:
-                    pass
-            else:
-                print(obj)
+    Eval("timer1(objs)")
 
-        for i in range(2):
-            obj = CreateObject("coppercoin")
-            objs.append(obj)
+def timer2():
+    pl.DrawInfo("hi 1!")
+    pl.DrawInfo("hi 2!")
+    pl.DrawInfo("hi 3!")
+    pl.DrawInfo("hi 4!")
+    Eval("timer2()", 0.005)
 
-        t = threading.Timer(0.005, timer, [objs])
-        t.start()
-
-    t = threading.Timer(0.005, timer, [list()])
-    t.start()
-
-def main2():
-    import threading
-
-    def timer():
-        pl.DrawInfo("hi 1!")
-        pl.DrawInfo("hi 2!")
-        pl.DrawInfo("hi 3!")
-        pl.DrawInfo("hi 4!")
-        t = threading.Timer(0.005, timer)
-        t.start()
-
-    t = threading.Timer(0.005, timer)
-    t.start()
+Eval("timer1(list())", 0.005)
+Eval("timer2()", 0.005)
