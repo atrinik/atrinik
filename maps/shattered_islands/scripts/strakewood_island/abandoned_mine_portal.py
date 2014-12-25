50d66948327d7868102d6c7f78e9b102fce58af7
Alex Tokar <admin@atokar.net>
2014-12-25 16:30:12 +0000

    Redraw Moroch Temple portal when adjusting its graphical attributes.

--- maps/shattered_islands/scripts/strakewood_island/abandoned_mine_portal.py
+++ maps/shattered_islands/scripts/strakewood_island/abandoned_mine_portal.py
@@ -9,6 +9,7 @@ me.zoom_y -= 5
 # Adjust X and Y align.
 me.align += 1
 me.z += 1
+me.map.Redraw(me.x, me.y, me.layer, me.sub_layer)
 
 # Shrinked to a very small size? Then set the is_used_up flag so it'll
 # get removed the next tick.
