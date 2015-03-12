5f5e7daf51d639a4f72791426fe3fb2d975232ef
Alex Tokar <admin@atokar.net>
2015-03-12 14:45:22 +0100

    Two new test cases for applyable lights, add comments to existing test cases.

--- server/src/tests/unit/types/check_light_apply.c
+++ server/src/tests/unit/types/check_light_apply.c
@@ -27,80 +27,170 @@
 #include <check_proto.h>
 #include <stdarg.h>
 
+/*
+ * Player applies a torch on the ground. Ensure the torch is lit and not
+ * applied.
+ */
 START_TEST(test_light_apply_apply_1)
 {
+    mapstruct *map;
     object *pl, *torch;
 
-    pl = player_get_dummy();
-    fail_if(pl == NULL, "No player object.");
+    check_setup_env_pl(&map, &pl);
 
-    torch = get_archetype("torch");
+    torch = insert_ob_in_map(get_archetype("torch"), map, NULL, 0);
     player_apply(pl, torch, 0, 0);
-    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch was applied.");
-    fail_if(torch->glow_radius == 0, "Torch was not lit.");
+    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch is applied.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
     fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != NULL,
             "Light object in player's equipment array is set.");
-
-    object_destroy(torch);
 }
 END_TEST
 
+/*
+ * Player applies a lit torch on the ground Ensure the torch is extinguished and
+ * not applied.
+ */
 START_TEST(test_light_apply_apply_2)
 {
+    mapstruct *map;
     object *pl, *torch;
 
-    pl = player_get_dummy();
-    fail_if(pl == NULL, "No player object.");
+    check_setup_env_pl(&map, &pl);
 
-    torch = get_archetype("torch");
+    torch = insert_ob_in_map(get_archetype("torch"), map, NULL, 0);
     manual_apply(torch, torch, 0);
     player_apply(pl, torch, 0, 0);
-    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch was applied.");
-    fail_if(torch->glow_radius != 0, "Torch was not extinguished.");
+    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch is applied.");
+    fail_if(torch->glow_radius != 0, "Torch is not extinguished.");
     fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != NULL,
             "Light object in player's equipment array is set.");
-
-    object_destroy(torch);
 }
 END_TEST
 
+/*
+ * Player applies a torch in his inventory. Ensure it's lit and applied.
+ */
 START_TEST(test_light_apply_apply_3)
 {
+    mapstruct *map;
     object *pl, *torch;
 
-    pl = player_get_dummy();
-    fail_if(pl == NULL, "No player object.");
+    check_setup_env_pl(&map, &pl);
 
-    torch = get_archetype("torch");
-    insert_ob_in_ob(torch, pl);
+    torch = insert_ob_in_ob(get_archetype("torch"), pl);
     player_apply(pl, torch, 0, 0);
-    fail_if(!QUERY_FLAG(torch, FLAG_APPLIED), "Torch was not applied.");
-    fail_if(torch->glow_radius == 0, "Torch was not lit.");
+    fail_if(!QUERY_FLAG(torch, FLAG_APPLIED), "Torch is not applied.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
     fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != torch,
-            "Torch was not assigned to player's equipment array.");
+            "Torch is not assigned to player's equipment array.");
 }
 END_TEST
 
+/*
+ * Player applies a lit torch in his inventory. Ensure it was extinguished and
+ * not applied.
+ */
 START_TEST(test_light_apply_apply_4)
 {
+    mapstruct *map;
     object *pl, *torch;
 
-    pl = player_get_dummy();
-    fail_if(pl == NULL, "No player object.");
+    check_setup_env_pl(&map, &pl);
 
-    torch = get_archetype("torch");
+    torch = insert_ob_in_map(get_archetype("torch"), map, NULL, 0);
     manual_apply(torch, torch, 0);
-    insert_ob_in_ob(torch, pl);
-    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch was applied.");
-    fail_if(torch->glow_radius == 0, "Torch was not lit.");
+    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch is applied.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
+    object_remove(torch, 0);
+    torch = insert_ob_in_ob(torch, pl);
+    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch is applied.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
     fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != NULL,
-            "Torch was not assigned to player's equipment array.");
+            "Torch is assigned to player's equipment array.");
 
     player_apply(pl, torch, 0, 0);
-    fail_if(!QUERY_FLAG(torch, FLAG_APPLIED), "Torch was not applied.");
-    fail_if(torch->glow_radius == 0, "Torch was not lit.");
+
+    fail_if(!QUERY_FLAG(torch, FLAG_APPLIED), "Torch is not applied.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
     fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != torch,
-            "Torch was not assigned to player's equipment array.");
+            "Torch is not assigned to player's equipment array.");
+}
+END_TEST
+
+/*
+ * Player applies a torch in his inventory, then a torch on the ground. Ensure
+ * both torches are lit, but only the one in inventory is applied.
+ */
+START_TEST(test_light_apply_apply_5)
+{
+    mapstruct *map;
+    object *pl, *torch, *torch2;
+
+    check_setup_env_pl(&map, &pl);
+
+    torch = insert_ob_in_ob(get_archetype("torch"), pl);
+    fail_if(torch == NULL, "Could not insert torch in player.");
+    torch2 = insert_ob_in_map(get_archetype("torch"), map, NULL, 0);
+    fail_if(torch2 == NULL, "Could not insert torch in map.");
+
+    player_apply(pl, torch, 0, 0);
+    fail_if(!QUERY_FLAG(torch, FLAG_APPLIED), "Torch is not applied.");
+    fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != torch,
+            "Torch is not assigned to player's equipment array.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
+    fail_if(QUERY_FLAG(torch2, FLAG_APPLIED), "Torch2 is applied.");
+    fail_if(torch2->glow_radius != 0, "Torch2 is lit.");
+
+    player_apply(pl, torch2, 0, 0);
+    fail_if(QUERY_FLAG(torch2, FLAG_APPLIED), "Torch2 is applied.");
+    fail_if(torch2->glow_radius == 0, "Torch2 is not lit.");
+
+    fail_if(!QUERY_FLAG(torch, FLAG_APPLIED), "Torch is not applied.");
+    fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != torch,
+            "Torch is not assigned to player's equipment array.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
+}
+END_TEST
+
+/*
+ * Player applies stacked torches, ensure a new torch is created and lit and
+ * the original is not lit. Then apply the original and ensure it's lit and
+ * applied, and that the old one remains lit but not applied.
+ */
+START_TEST(test_light_apply_apply_6)
+{
+    mapstruct *map;
+    object *pl, *torch, *torch2;
+
+    check_setup_env_pl(&map, &pl);
+
+    torch = get_archetype("torch");
+    torch->nrof = 2;
+    torch = insert_ob_in_ob(torch, pl);
+    fail_if(torch == NULL, "Could not insert torch in player.");
+
+    player_apply(pl, torch, 0, 0);
+    torch2 = torch;
+    torch = CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT];
+
+    fail_if(torch == torch2, "New torch was not created");
+    fail_if(torch == NULL, "Player has no light object in equipment array.");
+    fail_if(!QUERY_FLAG(torch, FLAG_APPLIED), "Torch is not applied.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
+    fail_if(torch->nrof != 1, "Torch nrof is not 1.");
+
+    fail_if(QUERY_FLAG(torch2, FLAG_APPLIED), "Torch2 is applied.");
+    fail_if(torch2->glow_radius != 0, "Torch2 is lit.");
+
+    player_apply(pl, torch2, 0, 0);
+    fail_if(!QUERY_FLAG(torch2, FLAG_APPLIED), "Torch2 is not applied.");
+    fail_if(torch2->glow_radius == 0, "Torch2 is not lit.");
+    fail_if(CONTR(pl)->equipment[PLAYER_EQUIP_LIGHT] != torch2,
+            "Torch2 is not assigned to player's equipment array.");
+
+    fail_if(QUERY_FLAG(torch, FLAG_APPLIED), "Torch is applied.");
+    fail_if(torch->glow_radius == 0, "Torch is not lit.");
 }
 END_TEST
 
@@ -116,6 +206,8 @@ static Suite *light_apply_suite(void)
     tcase_add_test(tc_core, test_light_apply_apply_2);
     tcase_add_test(tc_core, test_light_apply_apply_3);
     tcase_add_test(tc_core, test_light_apply_apply_4);
+    tcase_add_test(tc_core, test_light_apply_apply_5);
+    tcase_add_test(tc_core, test_light_apply_apply_6);
 
     return s;
 }
