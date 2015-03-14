c9ba0a1453d9452ef487b50d746ea44db227fff7
Alex Tokar <admin@atokar.net>
2015-03-14 15:37:10 +0000

    Avoid unnecessarily changing generated UI files if only the timestamp comment changes.

--- tools/map-checker-qt/dev/run.sh
+++ tools/map-checker-qt/dev/run.sh
@@ -4,5 +4,20 @@ cd $(dirname $0)
 
 for f in ../ui/*.ui
 do
-    pyuic5 $f -o $(echo $f | sed 's/\.ui$/.py/')
+    f_new=$(echo $f | sed 's/\.ui$/.py/')
+
+    if [ ! -f $f_new ]; then
+        pyuic5 $f -o $f_new
+    else
+        f_new_tmp=$f_new.tmp
+        pyuic5 $f -o $f_new_tmp
+
+        diff=`grep -Fxvf $f_new $f_new_tmp | grep -ve "^#"`
+
+        if [ -n "$diff" ]; then
+            mv $f_new_tmp $f_new
+        else
+            rm $f_new_tmp
+        fi
+    fi
 done
