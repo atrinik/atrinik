e1d731b434b58410ae4e025d9d51b2b94cd78d35
Alex Tokar <admin@atokar.net>
2015-03-28 13:48:52 +0000

    Implement /tpregion.

---
+++ maps/python/commands/tpregion.py
@@ -0,0 +1,25 @@
+## @file
+## Implements the /tpregion command.
+
+def main():
+    region_name = WhatIsMessage()
+
+    if not region_name:
+        activator.Controller().DrawInfo("Usage: /tpregion <region>",
+                                        color = COLOR_WHITE)
+        return
+
+    region = GetFirst("region")
+
+    while region is not None:
+        if region.name.startswith(region_name) and region.map_first:
+            try:
+                activator.TeleportTo(region.map_first)
+            except AtrinikError as e:
+                activator.Controller().DrawInfo(str(e), color = COLOR_RED)
+
+            break
+
+        region = region.next
+
+main()
