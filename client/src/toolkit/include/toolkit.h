433ce4d6b7b05c953fa5d6cd671fa99d7afd7416
Alex Tokar <admin@atokar.net>
2015-05-18 13:07:51 +0200

    Fix typo.

--- client/src/toolkit/include/toolkit.h
+++ client/src/toolkit/include/toolkit.h
@@ -267,7 +267,7 @@ typedef struct toolkit_dependency {
 
 #define HARD_ASSERT(cond) assert(cond)
 
-#define _FLOATING_EQUAL(_a_, _b, _epsilon) (fabs((_a) - (_b)) < (_epsilon))
+#define _FLOATING_EQUAL(_a, _b, _epsilon) (fabs((_a) - (_b)) < (_epsilon))
 #define FLT_EQUAL(_a, _b) _FLOATING_EQUAL(_a, _b, FLT_EPSILON)
 #define DBL_EQUAL(_a, _b) _FLOATING_EQUAL(_a, _b, DBL_EPSILON)
 #define LDBL_EQUAL(_a, _b) _FLOATING_EQUAL(_a, _b, LDBL_EPSILON)
