25222d71c7cc9d9189126b93e87f76835a969ea5
Alex Tokar <admin@atokar.net>
2015-05-31 18:04:58 +0100

    Make license check script aware of sound resources.

--- arch/license_check.py
+++ arch/license_check.py
@@ -11,12 +11,12 @@ class colors:
 # Print usage.
 def usage():
     print("\n" + colors.bold + colors.underscore + "Use:" + colors.end + colors.end)
-    print("\nAtrinik images license checker application.\n")
+    print("\nAtrinik resources license checker application.\n")
     print(colors.bold + colors.underscore + "Options:" + colors.end + colors.end)
     print("\n\t-h, --help:\n\t\tDisplay this help.")
-    print("\n\t-v, --verbose:\n\t\tPrint all images without known license.")
+    print("\n\t-v, --verbose:\n\t\tPrint all resources without known license.")
     print("\n\t-d " + colors.underscore + "directory" + colors.end + ", --directory=" + colors.underscore + "directory" + colors.end + ":\n\t\tSpecify directory where to start checking licenses (recursively). Default is '.'.")
-    print("\n\t-c " + colors.underscore + "contributor" + colors.end + ", --contributor=" + colors.underscore + "GPL" + colors.end + ":\n\t\tOptional contributor to list all images of. This can be a part of the contributor's name, the license it is using, etc.")
+    print("\n\t-c " + colors.underscore + "contributor" + colors.end + ", --contributor=" + colors.underscore + "GPL" + colors.end + ":\n\t\tOptional contributor to list all resources of. This can be a part of the contributor's name, the license it is using, etc.")
     print("\n\t--text-only:\n\t\tDo not use escape sequences for coloring the output.")
 
 # Try to parse our command line options.
@@ -55,24 +55,34 @@ if not os.path.exists(path) or not os.path.isdir(path):
 
 licensed = {}
 unlicensed = []
-image_info = {}
+resource_info = {}
 
-# Recursively scans directory for images under the same license.
+def is_resource(s):
+    for ext in (".png", ".s3m", ".ogg", ".mid", ".xm", ".mod"):
+        if s.endswith(ext):
+            if os.path.basename(os.path.dirname(s)) == "durations":
+                return False
+
+            return True
+
+    return False
+
+# Recursively scans directory for resources under the same license.
 def dir_recurse(path, license, extra):
     for entry in os.listdir(path):
         entry = os.path.join(dirpath, to_remove, entry)
 
-        # Image, add it to the list.
-        if entry.endswith(".png"):
+        # Resource, add it to the list.
+        if is_resource(entry):
             licensed[license].append(entry)
 
             if extra:
-                image_info[entry] = extra
+                resource_info[entry] = extra
         # Go on recursively.
         elif os.path.isdir(entry):
             dir_recurse(entry, license, extra)
 
-# Scan for images and license files.
+# Scan for resources and license files.
 for (dirpath, dirnames, filenames) in os.walk(path):
     # Parse LICENSE file in this directory.
     if "LICENSE" in filenames:
@@ -86,7 +96,7 @@ for (dirpath, dirnames, filenames) in os.walk(path):
 
                 if not license in licensed:
                     licensed[license] = []
-            # Image or directory name that is covered by the license.
+            # Resource or directory name that is covered by the license.
             elif line[:1].isspace() and line[1:]:
                 try:
                     parts = line.strip().split()
@@ -97,23 +107,23 @@ for (dirpath, dirnames, filenames) in os.walk(path):
 
                 extra = None
 
-                # There is more data about the image(s).
+                # There is more data about the resources(s).
                 if len(parts) > 1:
                     extra = " ".join(parts[1:])
 
                 to_remove_path = os.path.join(dirpath, to_remove)
 
                 # Remove it from dirnames to walk through recursively in the outermost loop
-                # but scan the directory to add the image names.
+                # but scan the directory to add the resource names.
                 if to_remove in dirnames:
                     dirnames.remove(to_remove)
                     dir_recurse(to_remove_path, license, extra)
-                # Simple image name.
+                # Simple resource name.
                 elif to_remove in filenames:
                     licensed[license].append(to_remove_path)
 
                     if extra:
-                        image_info[to_remove_path] = extra
+                        resource_info[to_remove_path] = extra
 
                     filenames.remove(to_remove)
                 # Invalid entry, drop a warning.
@@ -130,20 +140,20 @@ for (dirpath, dirnames, filenames) in os.walk(path):
     for entry in filenames:
         entry = os.path.join(dirpath, entry)
 
-        if entry.endswith(".png"):
+        if is_resource(entry):
             unlicensed.append(entry)
 
-# Show list of unlicensed images in verbose mode.
+# Show list of unlicensed resources in verbose mode.
 if verbose:
-    print(colors.underscore + "List of unlicensed images:" + colors.end)
+    print(colors.underscore + "List of unlicensed resources:" + colors.end)
     unlicensed.sort()
 
     for entry in unlicensed:
         print("\t" + entry)
 
 if not contributor:
-    print("\n" + colors.underscore + "Image contributors:" + colors.end)
-    # Store length of licensed images.
+    print("\n" + colors.underscore + "Resource contributors:" + colors.end)
+    # Store length of licensed resources.
     licensed_len = 0
 
     # Sort the licenses and output their staits.
@@ -161,7 +171,7 @@ if not contributor:
     # Show percentage.
     if total:
         print("\tPercent: {0}{1}%{2}".format(colors.bold, licensed_len * 100 / total, colors.end))
-# Look for images by specific contributor only.
+# Look for resources by specific contributor only.
 else:
     license = None
 
@@ -176,8 +186,8 @@ else:
         print(colors.bold + contributor + colors.end + " is not a known contributor.")
         sys.exit(2)
 
-    print("\n" + colors.underscore + "Images by " + license + colors.end + ":")
+    print("\n" + colors.underscore + "Resources by " + license + colors.end + ":")
 
-    # Dump out the images.
-    for image in licensed[license]:
-        print("\t" + image + (" " + image_info[image] if image in image_info else ""))
+    # Dump out the resources.
+    for resource in licensed[license]:
+        print("\t" + resource + (" " + resource_info[resource] if resource in resource_info else ""))
