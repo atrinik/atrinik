6c3acb81676b522992d4d254615ead324b0d33c4
Alex Tokar <admin@atokar.net>
2015-06-02 21:13:16 +0100

    Add Buyer class.

--- maps/python/Seller.py
+++ maps/python/Seller.py
@@ -7,11 +7,34 @@ import json
 import random
 import re
 
-class Seller(InterfaceBuilder):
+class Merchant(InterfaceBuilder):
+    _goods = None
+    max_goods = 100
+
+    @property
+    def goods(self):
+        raise NotImplementedError("not implemented")
+
+    def find_goods(self, name):
+        for obj in self.goods:
+            if obj.GetName() == name:
+                return obj
+
+        return None
+
+    def generate_item_links(self, dest, item, nrof):
+        name = item.GetName()
+
+        for x in [1, 5, 10, 25, 50, 100, 500, 1000]:
+            if x > self.max_goods or (nrof and x > nrof):
+                break
+
+            self.add_link(int2english(x).capitalize(), dest = dest + "{} {}".format(x, name))
+
+class Seller(Merchant):
     """Used for NPCs that sell something to the player."""
 
     max_goods = 100
-    _goods = None
 
     @property
     def goods(self):
@@ -57,13 +80,6 @@ class Seller(InterfaceBuilder):
 
         return self._goods
 
-    def find_goods(self, name):
-        for obj in self.goods:
-            if obj.GetName() == name:
-                return obj
-
-        return None
-
     def can_buy_goods(self, obj):
         fnc = getattr(self, "subdialog_can_buy_" + obj.arch.name, None)
 
@@ -91,13 +107,7 @@ class Seller(InterfaceBuilder):
             self.add_link(name.capitalize(), dest = "buy " + name)
 
     def subdialog_stockitem(self):
-        name = self.buy_item.GetName()
-
-        for x in [1, 5, 10, 25, 50, 100, 500, 1000]:
-            if x > self.max_goods or (self.buy_item_stock and x > self.buy_item_stock):
-                break
-
-            self.add_link(int2english(x).capitalize(), dest = "buy {} {}".format(x, name))
+        self.generate_item_links("buy ", self.buy_item, self.buy_item_stock)
 
     def subdialog_goods(self):
         if not self.goods:
@@ -188,3 +198,84 @@ class Seller(InterfaceBuilder):
                     obj.WriteKey("stock_nrof", str(obj_stock - num))
         else:
             getattr(self, "subdialog_buy_" + obj.arch.name, self.subdialog_buyitem)()
+
+class Buyer(Merchant):
+    @property
+    def goods(self):
+        if self._goods is not None:
+            return self._goods
+
+        event = WhatIsEvent()
+        self._goods = []
+
+        for obj in sorted(event.inv, key = lambda obj: obj.GetName()):
+            self._goods.append(obj)
+
+        return self._goods
+
+    def dialog_hello(self):
+        self.add_msg("Welcome, dear customer!")
+        self.add_msg("I would be interested in the following items.")
+        self.subdialog_goods()
+
+    def subdialog_goods(self):
+        for obj in self.goods:
+            name = obj.GetName()
+            self.add_link(name.capitalize(), dest = "sell " + name)
+
+    def subdialog_noitem(self):
+        self.add_msg("Ah, excellent!")
+        self.subdialog_sell()
+        self.add_msg("Oh, you don't have any of those!")
+
+    def subdialog_item(self):
+        self.generate_item_links("sell ", self.sell_item, self.sell_item_nrof)
+
+    def subdialog_sell(self):
+        self.add_msg_icon(self.sell_item.face[0], self.sell_item.GetName() + " for " + CostString(self.sell_item.GetCost(self.sell_item, COST_TRUE)))
+
+    def subdialog_sellitem(self):
+        self.add_msg("Ah, excellent!")
+        self.subdialog_sell()
+        self.add_msg("How many do you want to sell?")
+        self.subdialog_item()
+
+    def subdialog_sold(self):
+        self.add_msg_icon(self.sell_item.face[0], self.sell_item.GetName())
+
+    def subdialog_solditem(self):
+        self.subdialog_sold()
+        self.add_msg("Pleasure doing business with you!")
+
+    def dialog(self, msg):
+        match = re.match(r"sell (\d+)?\s*(.*)", msg)
+
+        if not match:
+            return
+
+        num, name = match.groups()
+        obj = self.find_goods(name)
+
+        if not obj:
+            return
+
+        item = self._activator.FindObject(archname=obj.arch.name, name=obj.name)
+        self.sell_item = obj
+
+        if not item:
+            getattr(self, "subdialog_noitem_" + obj.arch.name, self.subdialog_noitem)()
+            return
+
+        self.sell_item_nrof = item.nrof
+
+        if num != None:
+            num = max(1, min(int(num), self.max_goods, item.nrof))
+            cost = obj.GetCost(obj, COST_TRUE) * num
+
+            item = item.Decrease(num)
+            self._activator.Controller().InsertCoins(cost)
+
+            self.add_msg("You receive {}.".format(CostString(cost)), color = COLOR_YELLOW)
+            getattr(self, "subdialog_sold_" + obj.arch.name, self.subdialog_solditem)()
+        else:
+            getattr(self, "subdialog_sell_" + obj.arch.name, self.subdialog_sellitem)()
