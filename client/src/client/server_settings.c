874567b538e9740773728d16120221182426a315
Alex Tokar <admin@atokar.net>
2015-06-30 18:33:27 +0100

    Rewrite server settings file parsing.

--- client/src/client/server_settings.c
+++ client/src/client/server_settings.c
@@ -26,138 +26,160 @@
  * @file
  * Server settings.
  *
- * @author Alex Tokar */
+ * @author Alex Tokar
+ */
 
 #include <global.h>
 #include <toolkit_string.h>
 
-/** Server settings. */
+/** The server settings. */
 server_settings *s_settings = NULL;
 
 /**
- * Initialize the server settings from the srv file. */
+ * Initialize the server settings from the srv file.
+ */
 void server_settings_init(void)
 {
-    FILE *fp;
-    char buf[HUGE_BUF * 4], *cp;
-    int line = 0;
-    char_struct *cur_char = NULL;
-    size_t text_id = 0, i;
-
-    fp = server_file_open_name(SERVER_FILE_SETTINGS);
-
-    if (!fp) {
+    FILE *fp = server_file_open_name(SERVER_FILE_SETTINGS);
+    if (fp == NULL) {
         return;
     }
 
     server_settings_deinit();
     s_settings = ecalloc(1, sizeof(server_settings));
 
-    while (fgets(buf, sizeof(buf) - 1, fp)) {
-        line++;
+    char_struct *cur_char = NULL;
+    size_t text_id = 0;
 
-        if (*buf == '#') {
+    char buf[HUGE_BUF * 4];
+    uint64_t linenum = 0;
+
+    while (fgets(VS(buf), fp) != NULL) {
+        linenum++;
+
+        char *cp = string_skip_whitespace(buf), *end = strchr(cp, '\n');
+        if (end != NULL) {
+            *end = '\0';
+        }
+
+        char *cps[2];
+        if (string_split(cp, cps, arraysize(cps), ' ') < 1) {
             continue;
         }
 
-        cp = strrchr(buf, '\n');
+        char *key = cps[0], *value = cps[1], *error_str;
 
-        /* Eliminate newline. */
-        if (cp) {
-            *cp = '\0';
-        }
-
-        if (*buf == '\0') {
-            continue;
-        }
-
-        /* Parse the command. Unknown commands will be silently ignored. */
-        if (!strncmp(buf, "char ", 5)) {
-            s_settings->characters = ereallocz(s_settings->characters, sizeof(*s_settings->characters) * s_settings->num_characters, sizeof(*s_settings->characters) * (s_settings->num_characters + 1));
-            cur_char = &s_settings->characters[s_settings->num_characters];
-            cur_char->name = estrdup(buf + 5);
-        } else if (!strncmp(buf, "gender ", 7)) {
-            char gender[MAX_BUF], arch[MAX_BUF], face[MAX_BUF];
-            int gender_id;
-
-            if (sscanf(buf + 7, "%s %s %s", gender, arch, face) == 3) {
-                gender_id = gender_to_id(gender);
-                cur_char->gender_archetypes[gender_id] = estrdup(arch);
-                cur_char->gender_faces[gender_id] = estrdup(face);
+        if (strcmp(key, "char") == 0) {
+            if (cur_char != NULL) {
+                error_str = "superfluous char attribute";
+                goto error;
             }
-        } else if (!strncmp(buf, "desc ", 5)) {
-            cur_char->desc = estrdup(buf + 5);
-        } else if (!strcmp(buf, "end")) {
+
+            s_settings->characters = ereallocz(s_settings->characters,
+                    sizeof(*s_settings->characters) *
+                    s_settings->num_characters,
+                    sizeof(*s_settings->characters) *
+                    (s_settings->num_characters + 1));
+            cur_char = &s_settings->characters[s_settings->num_characters];
             s_settings->num_characters++;
-        } else if (!strncmp(buf, "level ", 6)) {
-            uint32_t lev;
+            cur_char->name = estrdup(value);
+        } else if (string_isempty(value)) {
+            if (strcmp(key, "end") == 0) {
+                if (cur_char == NULL) {
+                    error_str = "superfluous end attribute";
+                    goto error;
+                }
 
-            s_settings->max_level = atoi(buf + 6);
-            s_settings->level_exp = emalloc(sizeof(*s_settings->level_exp) * (s_settings->max_level + 2));
+                cur_char = NULL;
+            }
+        } else if (cur_char != NULL) {
+            if (strcmp(key, "gender") == 0) {
+                char *cps2[3];
 
-            for (lev = 0; lev <= s_settings->max_level; lev++) {
-                if (!fgets(buf, sizeof(buf) - 1, fp)) {
+                if (string_split(value, cps2, arraysize(cps2), ' ') == 3) {
+                    int gender_id = gender_to_id(cps2[0]);
+                    if (gender_id != -1) {
+                        cur_char->gender_archetypes[gender_id] =
+                                estrdup(cps2[1]);
+                        cur_char->gender_faces[gender_id] = estrdup(cps2[2]);
+                    }
+                }
+            } else if (strcmp(key, "desc") == 0) {
+                cur_char->desc = estrdup(value);
+            }
+        } else if (strcmp(key, "level") == 0) {
+            s_settings->max_level = atoi(value);
+            s_settings->level_exp = emalloc(sizeof(*s_settings->level_exp) *
+                    (s_settings->max_level + 2));
+
+            for (uint32_t lev = 0; lev <= s_settings->max_level; lev++) {
+                if (fgets(VS(buf), fp) == NULL) {
                     break;
                 }
 
                 s_settings->level_exp[lev] = strtoull(buf, NULL, 16);
             }
 
-            s_settings->level_exp[lev] = 0;
-        } else if (!strncmp(buf, "text ", 5)) {
-            if (text_id < SERVER_TEXT_MAX) {
-                size_t j = 0;
+            s_settings->level_exp[s_settings->max_level + 1] = 0;
+        } else if (strcmp(key, "text") == 0) {
+            if (text_id == SERVER_TEXT_MAX) {
+                error_str = "reached maximum amount of text entries";
+                goto error;
+            }
 
-                s_settings->text[text_id] = estrdup(buf + 5);
-                string_newline_to_literal(s_settings->text[text_id]);
+            s_settings->text[text_id] = estrdup(value);
+            string_newline_to_literal(s_settings->text[text_id]);
 
+            if (text_id == SERVER_TEXT_PROTECTION_GROUPS ||
+                    text_id == SERVER_TEXT_PROTECTION_LETTERS ||
+                    text_id == SERVER_TEXT_PROTECTION_FULL ||
+                    text_id == SERVER_TEXT_SPELL_PATHS) {
+                char **dst;
+                size_t max, arraymax;
                 if (text_id == SERVER_TEXT_PROTECTION_GROUPS) {
-                    cp = strtok(s_settings->text[text_id], " ");
-
-                    while (cp) {
-                        snprintf(s_settings->protection_groups[j],
-                                sizeof(s_settings->protection_groups[j]), "%s",
-                                cp);
-                        j++;
-                        cp = strtok(NULL, " ");
-                    }
+                    dst = s_settings->protection_groups;
+                    max = sizeof(*s_settings->protection_groups);
+                    arraymax = arraysize(s_settings->protection_groups);
                 } else if (text_id == SERVER_TEXT_PROTECTION_LETTERS) {
-                    cp = strtok(s_settings->text[text_id], " ");
-
-                    while (cp) {
-                        strncpy(s_settings->protection_letters[j], cp, sizeof(*s_settings->protection_letters) - 1);
-                        s_settings->protection_letters[j][sizeof(*s_settings->protection_letters) - 1] = '\0';
-                        j++;
-                        cp = strtok(NULL, " ");
-                    }
+                    dst = s_settings->protection_letters;
+                    max = sizeof(*s_settings->protection_letters);
+                    arraymax = arraysize(s_settings->protection_letters);
                 } else if (text_id == SERVER_TEXT_PROTECTION_FULL) {
-                    cp = strtok(s_settings->text[text_id], " ");
-
-                    while (cp) {
-                        strncpy(s_settings->protection_full[j], cp, sizeof(*s_settings->protection_full) - 1);
-                        s_settings->protection_full[j][sizeof(*s_settings->protection_full) - 1] = '\0';
-                        j++;
-                        cp = strtok(NULL, " ");
-                    }
+                    dst = s_settings->protection_full;
+                    max = sizeof(*s_settings->protection_full);
+                    arraymax = arraysize(s_settings->protection_full);
                 } else if (text_id == SERVER_TEXT_SPELL_PATHS) {
-                    cp = strtok(s_settings->text[text_id], " ");
-
-                    while (cp) {
-                        strncpy(s_settings->spell_paths[j], cp, sizeof(*s_settings->spell_paths) - 1);
-                        s_settings->spell_paths[j][sizeof(*s_settings->spell_paths) - 1] = '\0';
-                        j++;
-                        cp = strtok(NULL, " ");
-                    }
+                    dst = s_settings->spell_paths;
+                    max = sizeof(*s_settings->spell_paths);
+                    arraymax = arraysize(s_settings->spell_paths);
+                } else {
+                    HARD_ASSERT(false);
                 }
 
-                text_id++;
-            } else {
-                LOG(BUG, "Error in settings file, more text entries than allowed on line %d.", line);
+                size_t i = 0, pos = 0;
+                while (string_get_word(s_settings->text[text_id], &pos, ' ',
+                        VS(buf), 0)) {
+                    if (i == arraymax) {
+                        error_str = "reached maximum array size";
+                        goto error;
+                    }
+
+                    dst[i++] = estrdup(buf);
+                }
             }
+
+            text_id++;
         }
+
+        continue;
+
+error:
+        LOG(ERROR, "Error parsing %s, line %" PRIu64 ", %s: %s %s",
+                SERVER_FILE_SETTINGS, linenum, error_str, key,
+                value != NULL ? value : "");
     }
 
-    for (i = text_id; i < SERVER_TEXT_MAX; i++) {
+    for (size_t i = text_id; i < SERVER_TEXT_MAX; i++) {
         s_settings->text[i] = estrdup("???");
     }
 
@@ -165,34 +187,72 @@ void server_settings_init(void)
 }
 
 /**
- * Deinitialize the server settings. */
+ * Deinitialize the server settings.
+ */
 void server_settings_deinit(void)
 {
-    size_t i, gender;
-
-    if (!s_settings) {
+    if (s_settings == NULL) {
         return;
     }
 
-    efree(s_settings->level_exp);
+    if (s_settings->level_exp != NULL) {
+        efree(s_settings->level_exp);
+    }
 
-    for (i = 0; i < s_settings->num_characters; i++) {
-        efree(s_settings->characters[i].name);
-        efree(s_settings->characters[i].desc);
+    for (size_t i = 0; i < s_settings->num_characters; i++) {
+        if (s_settings->characters[i].name != NULL) {
+            efree(s_settings->characters[i].name);
+        }
 
-        for (gender = 0; gender < GENDER_MAX; gender++) {
-            if (s_settings->characters[i].gender_archetypes[gender]) {
+        if (s_settings->characters[i].desc != NULL) {
+            efree(s_settings->characters[i].desc);
+        }
+
+        for (size_t gender = 0; gender < GENDER_MAX; gender++) {
+            if (s_settings->characters[i].gender_archetypes[gender] != NULL) {
                 efree(s_settings->characters[i].gender_archetypes[gender]);
+            }
+
+            if (s_settings->characters[i].gender_faces[gender] != NULL) {
                 efree(s_settings->characters[i].gender_faces[gender]);
             }
         }
     }
 
-    for (i = 0; i < SERVER_TEXT_MAX; i++) {
-        efree(s_settings->text[i]);
+    if (s_settings->characters != NULL) {
+        efree(s_settings->characters);
     }
 
-    efree(s_settings->characters);
+    for (size_t i = 0; i < SERVER_TEXT_MAX; i++) {
+        if (s_settings->text[i] != NULL) {
+            efree(s_settings->text[i]);
+        }
+    }
+
+    for (size_t i = 0; i < arraysize(s_settings->protection_groups); i++) {
+        if (s_settings->protection_groups[i] != NULL) {
+            efree(s_settings->protection_groups[i]);
+        }
+    }
+
+    for (size_t i = 0; i < arraysize(s_settings->protection_letters); i++) {
+        if (s_settings->protection_letters[i] != NULL) {
+            efree(s_settings->protection_letters[i]);
+        }
+    }
+
+    for (size_t i = 0; i < arraysize(s_settings->protection_full); i++) {
+        if (s_settings->protection_full[i] != NULL) {
+            efree(s_settings->protection_full[i]);
+        }
+    }
+
+    for (size_t i = 0; i < arraysize(s_settings->spell_paths); i++) {
+        if (s_settings->spell_paths[i] != NULL) {
+            efree(s_settings->spell_paths[i]);
+        }
+    }
+
     efree(s_settings);
     s_settings = NULL;
 }
