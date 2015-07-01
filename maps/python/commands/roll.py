42dd8293a81f256308fb07aabf988992831e50db
Alex Tokar <admin@atokar.net>
2015-07-01 12:16:55 +0200

    Cleanup /roll command implementation.

--- maps/python/commands/roll.py
+++ maps/python/commands/roll.py
@@ -1,5 +1,6 @@
-## @file
-## Implements the /roll command.
+"""
+Implements the /roll command.
+"""
 
 from Atrinik import *
 from random import randrange
@@ -11,8 +12,16 @@ MAX_num = 10
 MIN_sides = 1
 MAX_sides = 1000
 
+
 # Parse parameters.
 def parse_params():
+    """
+    Parse command parameters, eg, '1d5'.
+    :return: Tuple containing how many times to roll, and how many sides it
+             should have.
+    :rtype: tuple
+    """
+
     try:
         if not msg:
             return None
@@ -22,23 +31,31 @@ def parse_params():
     # Split it.
     l = msg.split("d")
 
-    # Do validation. Are params in '<x>d<y>' format, and are both x and y digits?
+    # Do validation. Are params in '<x>d<y>' format, and are both x and y
+    # digits?
     if len(l) < 2 or not l[0].isdigit() or not l[1].isdigit():
         return None
 
     # Return how many times to roll the die, and how many sides it should have.
-    return max(MIN_num, min(MAX_num, int(l[0]))), max(MIN_sides, min(MAX_sides, int(l[1])))
+    return (max(MIN_num, min(MAX_num, int(l[0]))),
+            max(MIN_sides, min(MAX_sides, int(l[1]))))
+
 
 def main():
     parse = parse_params()
 
-    if not parse:
-        pl.DrawInfo(markup_escape("Usage: /roll <times>d<sides>"), color = COLOR_RED)
+    if parse is None:
+        pl.DrawInfo(markup_escape("Usage: /roll <times>d<sides>"),
+                    color=COLOR_RED)
         return
 
-    (num, sides) = parse
+    num, sides = parse
 
     # Tell everyone about the roll.
-    activator.map.DrawInfo(activator.x, activator.y, "{} rolls a magical die ({}d{}) and gets: {}.".format(activator.name, num, sides, ", ".join(str(randrange(sides) + 1) for die in range(num))), COLOR_ORANGE)
+    message = "{} rolls a magical die ({}d{}) and gets: {}.".format(
+        activator.name, num, sides,
+        ", ".join(str(randrange(sides) + 1) for _ in range(num))
+    )
+    activator.map.DrawInfo(activator.x, activator.y, message, COLOR_ORANGE)
 
 main()
