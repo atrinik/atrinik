f81c0f5ca74bdf14a6ce8d865dc382307b17bf22
Alex Tokar <admin@atokar.net>
2015-07-21 15:56:46 +0200

    Move stringbuffer prototypes to stringbuffer.h.

--- server/src/toolkit/include/stringbuffer.h
+++ server/src/toolkit/include/stringbuffer.h
@@ -24,13 +24,37 @@
 
 /**
  * @file
- * Header file for stringbuffer.c. */
+ * Header file for stringbuffer.c.
+ */
 
 #ifndef STRINGBUFFER_H
 #define STRINGBUFFER_H
 
 /**
- * The string buffer state. */
+ * The string buffer state.
+ */
 typedef struct StringBuffer_struct StringBuffer;
+
+/* Prototypes */
+void toolkit_stringbuffer_init(void);
+void toolkit_stringbuffer_deinit(void);
+StringBuffer *stringbuffer_new(void);
+void stringbuffer_free(StringBuffer *sb);
+char *stringbuffer_finish(StringBuffer *sb);
+const char *stringbuffer_finish_shared(StringBuffer *sb);
+void stringbuffer_append_string_len(StringBuffer *sb, const char *str,
+        size_t len);
+void stringbuffer_append_string(StringBuffer *sb, const char *str);
+void stringbuffer_append_printf(StringBuffer *sb, const char *format,
+        ...) __attribute__((format(printf, 2, 3)));
+void stringbuffer_append_stringbuffer(StringBuffer *sb,
+        const StringBuffer *sb2);
+void stringbuffer_append_char(StringBuffer *sb, const char c);
+const char *stringbuffer_data(StringBuffer *sb);
+size_t stringbuffer_length(StringBuffer *sb);
+void stringbuffer_seek(StringBuffer *sb, const size_t pos);
+ssize_t stringbuffer_index(StringBuffer *sb, char c);
+ssize_t stringbuffer_rindex(StringBuffer *sb, char c);
+char *stringbuffer_sub(StringBuffer *sb, ssize_t start, ssize_t end);
 
 #endif
