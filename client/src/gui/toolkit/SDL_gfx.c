fe31bd4c817bca7cf70096cec51cebf6e7d6103e
Alex Tokar <admin@atokar.net>
2015-12-31 20:34:56 +0000

    Simplify code.

--- client/src/gui/toolkit/SDL_gfx.c
+++ client/src/gui/toolkit/SDL_gfx.c
@@ -2128,9 +2128,7 @@ static int _clipLine(SDL_Surface * dst, Sint16 * x, Sint16 * y, Sint16 * x2, Sin
                 swaptmp = *y2;
                 *y2 = *y;
                 *y = swaptmp;
-                swaptmp = code2;
-                code2 = code1;
-                code1 = swaptmp;
+                code1 = code2;
             }
             if (*x2 != *x) {
                 m = (*y2 - *y) / (float) (*x2 - *x);
