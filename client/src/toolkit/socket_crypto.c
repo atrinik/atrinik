232c037f480e67d2a49bd2e7c1dc313929219ef4
Alex Tokar <admin@atokar.net>
2016-06-29 16:22:50 +0200

    Fix logic error.

--- client/src/toolkit/socket_crypto.c
+++ client/src/toolkit/socket_crypto.c
@@ -582,7 +582,7 @@ socket_crypto_load_trusted (void)
             continue;
         }
 
-        if (sk_X509_push(crypto_trusted_certs, cert) != 1) {
+        if (sk_X509_push(crypto_trusted_certs, cert) < 1) {
             LOG(ERROR, "sk_X509_push() failed: %s",
                 ERR_error_string(ERR_get_error(), NULL));
             X509_free(cert);
