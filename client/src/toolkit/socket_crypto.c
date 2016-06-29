05fc80c2c83e3acc1fac335d6c13ae49a6ec5ac3
Alex Tokar <admin@atokar.net>
2016-06-29 16:22:57 +0200

    Fix memory leak.

--- client/src/toolkit/socket_crypto.c
+++ client/src/toolkit/socket_crypto.c
@@ -578,9 +578,12 @@ socket_crypto_load_trusted (void)
 
         X509 *cert = crypto_read_pem_x509(contents);
         if (cert == NULL) {
+            efree(contents);
             /* Logging already done */
             continue;
         }
+
+        efree(contents);
 
         if (sk_X509_push(crypto_trusted_certs, cert) < 1) {
             LOG(ERROR, "sk_X509_push() failed: %s",
